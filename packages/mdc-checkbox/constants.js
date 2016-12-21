f9cc09265254321247586e9c63d37ab47e94e6c6
Peter Johason <peter@peterjohanson.com>
2016-12-20 22:30:20 -0500

    fix(checkbox): Make DOM access lazy to avoid SSR problems.

--- packages/mdc-checkbox/constants.js
+++ packages/mdc-checkbox/constants.js
@@ -29,15 +29,20 @@ export const cssClasses = {
   ANIM_INDETERMINATE_UNCHECKED: `${ANIM}-indeterminate-unchecked`,
 };
 
+export function dom() {
+  return {
+    ANIM_END_EVENT_NAME: (() => {
+      const el = document.createElement('div');
+      // NOTE: We can immediately assume that the prefix is 'webkit' in browsers that don't
+      // support unprefixed animations since the only browsers up to two major versions back that
+      // don't support unprefixed names are mobile Safari and Android native browser, both of
+      // which use the 'webkit' prefix.
+      return 'animation' in el.style ? 'animationend' : 'webkitAnimationEnd';
+    })(),
+  };
+}
+
 export const strings = {
-  ANIM_END_EVENT_NAME: (() => {
-    const el = document.createElement('div');
-    // NOTE: We can immediately assume that the prefix is 'webkit' in browsers that don't
-    // support unprefixed animations since the only browsers up to two major versions back that
-    // don't support unprefixed names are mobile Safari and Android native browser, both of
-    // which use the 'webkit' prefix.
-    return 'animation' in el.style ? 'animationend' : 'webkitAnimationEnd';
-  })(),
   NATIVE_CONTROL_SELECTOR: `.${ROOT}__native-control`,
   TRANSITION_STATE_INIT: 'init',
   TRANSITION_STATE_CHECKED: 'checked',
