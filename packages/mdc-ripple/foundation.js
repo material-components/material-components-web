128e5168bb90e6bee0e90c19ac989fe560653cc8
cristobalchao@google.com <cristobalchao@google.com>
2017-02-09 12:58:16 -0500

    feat(ripple): Implement improved graceful degradation for ripple
    Resolves #189

--- packages/mdc-ripple/foundation.js
+++ packages/mdc-ripple/foundation.js
@@ -288,7 +288,6 @@ export default class MDCRippleFoundation extends MDCFoundation {
     let approxCurScale = 0;
     if (msElapsed > FG_TRANSFORM_DELAY_MS) {
       const percentComplete = Math.min((msElapsed - FG_TRANSFORM_DELAY_MS) / this.xfDuration_, 1);
-      // approxCurScale = percentComplete * this.fgScale_;
       approxCurScale = percentComplete;
     }
 
