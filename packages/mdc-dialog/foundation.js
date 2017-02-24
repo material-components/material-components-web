4dea53378b01ed6e4761c40e16287bdf3ecaa64e
Alex Sheehan <sheehana@google.com>
2017-02-24 13:01:26 -0500

    WIP

--- packages/mdc-dialog/foundation.js
+++ packages/mdc-dialog/foundation.js
@@ -26,55 +26,34 @@ export default class MDCDialogFoundation extends MDCFoundation {
     return strings;
   }
 
-  static get defaultAdapter() {
-    return {};
-  }
-
 	static get isOpen() {
 		return this.isOpen_;
 	}
 
 	static get defaultAdapter() {
     return {
+      dialogEl: () => {},
+      hasClass: (/* className: string */) => {},
       addClass: (/* className: string */) => {},
       removeClass: (/* className: string */) => {},
-      hasClass: (/* className: string */) => {},
-      hasNavigation: () => false,
-      hasNavigationAutoSave: () => false,
 			registerInteractionHandler: (/* evt: string, handler: EventListener */) => {},
       deregisterInteractionHandler: (/* evt: string, handler: EventListener */) => {},
       registerDialogInteractionHandler: (/* evt: string, handler: EventListener */) => {},
       deregisterDialogInteractionHandler: (/* evt: string, handler: EventListener */) => {},
-      registerConfirmationInteractionHandler: (/* evt: string, handler: EventListener */) => {},
-      deregisterConfirmationInteractionHandler: (/* evt: string, handler: EventListener */) => {},
       registerDocumentKeydownHandler: (/* handler: EventListener */) => {},
       deregisterDocumentKeydownHandler: (/* handler: EventListener */) => {},
       registerAcceptHandler: () => {},
       deregisterAcceptHandler: () => {},
       registerCancelHandler: () => {},
       deregisterCancelHandler: () => {},
-      registerNavigationAcceptHandler: () => {},
-      deregisterNavigationAcceptHandler: () => {},
-      registerNavigationCancelHandler: () => {},
-      deregisterNavigationCancelHandler: () => {},
-      registerConfirmationAcceptHandler: () => {},
-      deregisterConfirmationAcceptHandler: () => {},
-      registerConfirmationCancelHandler: () => {},
-      deregisterConfirmationCancelHandler: () => {},
-      acceptAction: () => {},
-      cancelAction: () => {},
-      updateCssVariable: (/* value: string */) => {},
       getFocusableElements: () => /* NodeList */ {},
       saveElementTabState: (/* el: Element */) => {},
       restoreElementTabState: (/* el: Element */) => {},
       makeElementUntabbable: (/* el: Element */) => {},
       acceptButton: () => {},
       cancelButton: () => {},
-      navigationAcceptButton: () => {},
-      navigationCancelButton: () => {},
-      confirmationCancelButton: () => {},
-      dialogEl: () => {},
-      confirmationDialogEl: () => {},
+      acceptAction: () => {},
+      cancelAction: () => {},
     };
   }
 
@@ -89,12 +68,8 @@ export default class MDCDialogFoundation extends MDCFoundation {
     this.componentClickHandler_ = () => this.cancel();
     this.dialogClickHandler_ = (evt) => evt.stopPropagation();
 		this.dialogFocusHandler_ = (evt) => {return this.passFocus(evt);}
-		this.confirmationClickHandler_ = (evt) => evt.stopPropagation();
     this.acceptHandler_ = () => this.accept();
     this.cancelHandler_ = () => this.cancel();
-    this.confirmationHandler_ = () => this.openConfirmation();
-    this.confirmationAcceptHandler_ = () => this.confirmationAccept();
-    this.confirmationCancelHandler_ = () => this.closeConfirmation();
     this.documentKeydownHandler_ = (evt) => {
       if (evt.key && evt.key === 'Escape' || evt.keyCode === 27) {
         this.cancel();
@@ -112,30 +87,10 @@ export default class MDCDialogFoundation extends MDCFoundation {
     this.adapter_.registerInteractionHandler('click', this.componentClickHandler_);
     this.adapter_.registerDialogInteractionHandler('click', this.dialogClickHandler_);
     this.adapter_.registerAcceptHandler(this.acceptHandler_);
+    this.adapter_.registerCancelHandler(this.cancelHandler_);
+    
     this.defaultFocusElement_ = this.adapter_.cancelButton();
-
-    if (!this.adapter_.hasNavigationAutoSave()) {
-      this.adapter_.registerCancelHandler(this.cancelHandler_);
-    } 
  
-    if (this.adapter_.hasNavigation()) {
-      this.adapter_.registerNavigationAcceptHandler(this.acceptHandler_);
-    	this.defaultFocusElement_ = this.adapter_.navigationCancelButton();
-    }
-
-		if (this.adapter_.hasNavigationAutoSave() && !window.matchMedia("(min-width: 640px)").matches) {
-    	this.defaultFocusElement_ = this.adapter_.navigationAcceptButton();
-		} else if (this.adapter_.hasNavigationAutoSave() && window.matchMedia("(min-width: 640px)").matches) {
-    	this.defaultFocusElement_ = this.adapter_.acceptButton();
-		}
-
-    if (this.adapter_.hasNavigation() && !this.adapter_.hasNavigationAutoSave()) {
-      this.adapter_.registerNavigationCancelHandler(this.confirmationHandler_);
-      this.adapter_.registerConfirmationAcceptHandler(this.confirmationAcceptHandler_);
-      this.adapter_.registerConfirmationCancelHandler(this.confirmationCancelHandler_);
-      this.adapter_.registerConfirmationInteractionHandler('click', this.confirmationClickHandler_);
-    }
-
     if (this.adapter_.hasClass(ACTIVE)) {
       this.isOpen_ = true;
     } else {
@@ -147,19 +102,12 @@ export default class MDCDialogFoundation extends MDCFoundation {
   destroy() {
     this.adapter_.deregisterInteractionHandler(this.componentClickHandler_);
     this.adapter_.deregisterDialogInteractionHandler(this.dialogClickHandler_);
-    this.adapter_.deregisterConfirmationInteractionHandler(this.confirmationClickHandler_);
     this.adapter_.deregisterAcceptHandler(this.acceptHandler_);
     this.adapter_.deregisterCancelHandler(this.cancelHandler_);
-    this.adapter_.deregisterNavigationAcceptHandler(this.acceptHandler_);
-    this.adapter_.deregisterNavigationCancelHandler(this.confirmationHandler_);
-    this.adapter_.deregisterConfirmationAcceptHandler(this.confirmationAcceptHandler_);
-    this.adapter_.deregisterConfirmationCancelHandler(this.confirmationCancelHandler_);
   }
 
   open() {
     this.lastFocusEl_ = document.activeElement;
-		this.activeDialog_ = this.adapter_.dialogEl();
-    this.adapter_.updateCssVariable('');
     this.adapter_.registerDocumentKeydownHandler(this.documentKeydownHandler_);
     this.adapter_.addClass(MDCDialogFoundation.cssClasses.ACTIVE);
     this.unlockTab_();
@@ -173,56 +121,18 @@ export default class MDCDialogFoundation extends MDCFoundation {
   close() {
     this.adapter_.deregisterFocusTrappingHandler(this.dialogFocusHandler_);
     this.adapter_.deregisterDocumentKeydownHandler(this.documentKeydownHandler_);
-    this.adapter_.updateCssVariable('');
     this.adapter_.removeClass(MDCDialogFoundation.cssClasses.ACTIVE);
     this.lockTab_();
     this.enableScroll_();
     this.isOpen_ = false;
     this.lastFocusEl_.focus();
-    this.resetDefaultFocus();
   }
  
-  openConfirmation() {
-		this.activeDialog_ = this.adapter_.confirmationDialogEl();
-    this.defaultFocusElement_ = this.adapter_.confirmationCancelButton();
-    this.adapter_.addConfirmClass(MDCDialogFoundation.cssClasses.CONFIRMATION_ACTIVE);
-    this.adapter_.registerFocusTrappingHandler(this.dialogFocusHandler_);
- 		this.defaultFocusElement_.focus();
- }
-
-  closeConfirmation() {
-    this.adapter_.removeConfirmClass(MDCDialogFoundation.cssClasses.CONFIRMATION_ACTIVE)
-    this.adapter_.deregisterFocusTrappingHandler(this.dialogFocusHandler_);
-		//this.resetDefaultFocus();
-  }
-
 	passFocus(evt) {
-		if (!this.activeDialog_.contains(evt.target)) {
+		if (!this.adapter_.dialogEl.contains(evt.target)) {
       evt.stopPropagation();
     	this.defaultFocusElement_.focus();
     }
-	}
-
-  /**
-   * When a dialog closes, we must reset the default focus element since it is
-   * a different element depending on the type of dialog (confirmation/standard)
-   */
-	resetDefaultFocus() {
-    this.defaultFocusElement_ = this.adapter_.cancelButton();
-    
-    if (this.adapter_.hasNavigation()) {
-    	this.defaultFocusElement_ = this.adapter_.navigationCancelButton();
-    }
-
-		if (this.adapter_.hasNavigationAutoSave() && !window.matchMedia("(min-width: 640px)").matches) {
-    	this.defaultFocusElement_ = this.adapter_.navigationAcceptButton();
-		} else if (this.adapter_.hasNavigationAutoSave() && window.matchMedia("(min-width: 640px)").matches) {
-    	this.defaultFocusElement_ = this.adapter_.acceptButton();
-		}
-
-    //this.activeDialog_ = this.adapter_.dialogEl();
-    //this.defaultFocusElement_.focus();
-    //this.adapter_.registerFocusTrappingHandler(this.dialogFocusHandler_);
 	}
 
   accept() {
@@ -233,11 +143,6 @@ export default class MDCDialogFoundation extends MDCFoundation {
   cancel() {
     this.adapter_.cancelAction();
     this.close();
-  }
-
-  confirmationAccept() {
-    this.cancel();
-    this.closeConfirmation();
   }
 
   isOpen() {
--- packages/mdc-dialog/index.js
+++ packages/mdc-dialog/index.js
@@ -41,10 +41,6 @@ export class MDCDialog extends MDCComponent {
     return this.root_.querySelector(MDCDialogFoundation.strings.DIALOG_SURFACE_SELECTOR);
   }
 
-  get confirmationDialog_() {
-    return this.root_.querySelector(MDCDialogFoundation.strings.CONFIRMATION_DIALOG_SELECTOR);
-  }
-
   get acceptButton_() {
     return this.root_.querySelector(MDCDialogFoundation.strings.ACCEPT_SELECTOR);
   }
@@ -53,40 +49,16 @@ export class MDCDialog extends MDCComponent {
     return this.root_.querySelector(MDCDialogFoundation.strings.CANCEL_SELECTOR);
   }
 
-  get navigationAcceptButton_() {
-    return this.root_.querySelector(MDCDialogFoundation.strings.NAV_ACCEPT_SELECTOR);
-  }
-
-  get navigationCancelButton_() {
-    return this.root_.querySelector(MDCDialogFoundation.strings.NAV_CANCEL_SELECTOR);
-  }
-
-  get confirmationAcceptButton_() {
-    return this.root_.querySelector(MDCDialogFoundation.strings.CONFIRMATION_ACCEPT_SELECTOR);
-  }
-
-  get confirmationCancelButton_() {
-    return this.root_.querySelector(MDCDialogFoundation.strings.CONFIRMATION_CANCEL_SELECTOR);
-  }
-
   getDefaultFoundation() {
     const {FOCUSABLE_ELEMENTS, OPACITY_VAR_NAME, SCROLL_LOCK_TARGET} = MDCDialogFoundation.strings;
 
-    /**
-     * All Dialog types share the same props except for auto save dialogs.
-     * If it is auto save, we do not merge these adapter objects, otherwise
-     * we supplement our adapter with the standard dialog properties.
-     */
-    const adapter = {
+    return new MDCDialogFoundation({
+      dialogEl: () => this.root_,
+      hasClass: (className) => this.root_.classList.contains(className),
       addClass: (className) => this.root_.classList.add(className),
       removeClass: (className) => this.root_.classList.remove(className),
-      addConfirmClass: (classname) => this.confirmationDialog_.classList.add(classname),
-      removeConfirmClass: (classname) => this.confirmationDialog_.classList.remove(classname),
       addScrollLockClass: (className) => document.querySelector(SCROLL_LOCK_TARGET).classList.add(MDCDialogFoundation.cssClasses.SCROLL_LOCK),
       removeScrollLockClass: (className) => document.querySelector(SCROLL_LOCK_TARGET).classList.remove(MDCDialogFoundation.cssClasses.SCROLL_LOCK),
-      hasClass: (className) => this.root_.classList.contains(className),
-      hasNavigation: () => Boolean(this.navigationAcceptButton_),
-      hasNavigationAutoSave: () => (Boolean(this.navigationAcceptButton_) && !Boolean(this.navigationCancelButton_)),
       registerInteractionHandler: (evt, handler) =>
         this.root_.addEventListener(util.remapEvent(evt), handler, util.applyPassive()),
       deregisterInteractionHandler: (evt, handler) =>
@@ -95,62 +67,24 @@ export class MDCDialog extends MDCComponent {
         this.dialog_.addEventListener(util.remapEvent(evt), handler),
       deregisterDialogInteractionHandler: (evt, handler) =>
         this.dialog_.removeEventListener(util.remapEvent(evt), handler),
-      registerConfirmationInteractionHandler: (evt, handler) =>
-        this.confirmationDialog_ ? this.confirmationDialog_.addEventListener(util.remapEvent(evt), handler) : null,
-      deregisterConfirmationInteractionHandler: (evt, handler) =>
-        this.confirmationDialog_ ? this.confirmationDialog_.removeEventListener(util.remapEvent(evt), handler) : null,
       registerAcceptHandler: (handler) => this.acceptButton_.addEventListener('click', handler),
       deregisterAcceptHandler: (handler) => this.acceptButton_.removeEventListener('click', handler),
-      registerNavigationAcceptHandler: (handler) => 
-        this.navigationAcceptButton_.addEventListener('click', handler),
-      deregisterNavigationAcceptHandler: (handler) => 
-        this.navigationAcceptButton_.removeEventListener('click', handler),
-      acceptAction: (handler) => console.log('Accept'),
+      registerCancelHandler: (handler) => this.cancelButton_.addEventListener('click', handler),
+      deregisterCancelHandler: (handler) => this.cancelButton_.removeEventListener('click', handler),
       registerDocumentKeydownHandler: (handler) => document.addEventListener('keydown', handler),
       deregisterDocumentKeydownHandler: (handler) => document.removeEventListener('keydown', handler),
       getFocusableElements: () => this.dialog_.querySelectorAll(FOCUSABLE_ELEMENTS),
       saveElementTabState: (el) => util.saveElementTabState(el),
       restoreElementTabState: (el) => util.restoreElementTabState(el),
       makeElementUntabbable: (el) => el.setAttribute('tabindex', -1),
-      acceptButton: () => this.acceptButton_,
-      cancelButton: () => this.cancelButton_,
-      navigationAcceptButton: () => this.navigationAcceptButton_,
-      navigationCancelButton: () => this.navigationCancelButton_,
-      confirmationCancelButton: () => this.confirmationCancelButton_,
-      dialogEl: () => this.root_,
-      confirmationDialogEl: () => this.confirmationDialog_,
 			setBackgroundAriaAttribute: () => document.querySelector(SCROLL_LOCK_TARGET).setAttribute('aria-hidden', 'true'),
 			registerFocusTrappingHandler: (handler) => document.addEventListener('focus', handler, true),
 			deregisterFocusTrappingHandler: (handler) => document.addEventListener('focus', handler),
-    }
 
-    const stdDialogAdapterProps = {
-      registerCancelHandler: (handler) => this.cancelButton_.addEventListener('click', handler), deregisterCancelHandler: (handler) => this.cancelButton_.removeEventListener('click', handler),
-      registerConfirmationAcceptHandler: (handler) => 
-        this.confirmationAcceptButton_.addEventListener('click', handler),
-      deregisterConfirmationAcceptHandler: (handler) => 
-        this.confirmationAcceptButton_.removeEventListener('click', handler),
-      registerConfirmationCancelHandler: (handler) => 
-        this.confirmationCancelButton_.addEventListener('click', handler),
-      deregisterConfirmationCancelHandler: (handler) => 
-        this.confirmationCancelButton_.removeEventListener('click', handler),
-      registerNavigationCancelHandler: (handler) => 
-        this.navigationCancelButton_.addEventListener('click', handler),
-      deregisterNavigationCancelHandler: (handler) => 
-        this.navigationCancelButton_.removeEventListener('click', handler),
+      acceptButton: () => this.acceptButton_,
+      cancelButton: () => this.cancelButton_,
+      acceptAction: (handler) => console.log('Accept'),
       cancelAction: (handler) => console.log('Cancel'),
-    }
-
-    /**
-     * Auto save is the only type of dialog that does not have a cancel button. 
-     * Technically all it does is accept and close the dialog window.
-     * The user is responsible for handling any auto save functionality that
-     * happens within the context of the dialog
-     */
-    if (!adapter.hasNavigationAutoSave()) {
-      Object.assign(adapter, stdDialogAdapterProps);
-    }
-
-    return new MDCDialogFoundation(adapter);
+    });
   }
 }
