12bd03e90036cb80cbfbdd63a8d2fb17d978d20a
Travis Kaufman <traviskaufman@users.noreply.github.com>
2017-04-10 10:59:55 -0400

    fix(dialog): Handle focus trapping correctly (#491)
    - Remove all focus trapping logic within the dialog foundation,
      replacing it with adapter methods set up to handle focus trapping.
    - Add [focus-trap](https://npmjs.com/focus-trap) as a dependency and use
      it for implementing the adapter methods at the component level
    - Add a util method for easily creating properly configured focus-trap
      instances that framework components can use when implementing their
      adapters.
    - Remove use of aria-hidden and replace with visibility styles so that
      tab-focusing works correctly when dialog is hidden.
    - [techdebt] Remove all unnecessary adapter API methods and dead code
    - [techdebt] Make grammatical corrections in README
    - [techdebt] Remove unneeded passive event listener logic for dialog handlers
    - [demos] Fix ripples incorrectly appearing in buttons within the demos.
    
    Resolves #424
    Resolves #409
    Resolves #426
    
    NOTE: The additional issues resolved are by-products from switching to
    focus-trap.
    
    BREAKING CHANGE: There are a few changes that need to be taken into
    account for this commit:
    
    - Dialogs no longer require an `aria-hidden="true"` attribute.
    - Dialogs _do_ require a `style="visibility:hidden"` attribute for
      correct first render.
    - `trapFocusOnSurface` and `untrapFocusOnSurface` methods must be
      implemented for the adapter
    - `hasClass`, `setAttr`, `registerFocusTrappingHandler`,
      `deregisterFocusTrappingHandler`, `numFocusableTargets`,
      `setDialogFocusFirstTarget`, `setInitialFocus`,
      `getFocusableElements`, `saveElementTabState`,
      `restoreElementTabState`, `makeElementUntabbable`, `setBodyAttr`,
      `rmBodyAttr`, `getFocusedTarget`, and `setFocusedTarget` have all been
      removed from the adapter.
    - `applyPassive`, `saveElementTabState`, and `restoreElementTabState`
      have all been removed from `mdcDialog.util`.

--- packages/mdc-dialog/foundation.js
+++ packages/mdc-dialog/foundation.js
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-   */
+ */
 
 import {MDCFoundation} from '@material/base';
 import {cssClasses, strings} from './constants';
@@ -28,10 +28,9 @@ export default class MDCDialogFoundation extends MDCFoundation {
 
   static get defaultAdapter() {
     return {
-      hasClass: (/* className: string */) => {},
       addClass: (/* className: string */) => {},
       removeClass: (/* className: string */) => {},
-      setAttr: (/* attr: string, val: string */) => {},
+      setStyle: (/* propertyName: string, value: string */) => {},
       addBodyClass: (/* className: string */) => {},
       removeBodyClass: (/* className: string */) => {},
       eventTargetHasClass: (/* target: EventTarget, className: string */) => /* boolean */ false,
@@ -41,34 +40,19 @@ export default class MDCDialogFoundation extends MDCFoundation {
       deregisterSurfaceInteractionHandler: (/* evt: string, handler: EventListener */) => {},
       registerDocumentKeydownHandler: (/* handler: EventListener */) => {},
       deregisterDocumentKeydownHandler: (/* handler: EventListener */) => {},
-      registerFocusTrappingHandler: (/* handler: EventListener */) => {},
-      deregisterFocusTrappingHandler: (/* handler: EventListener */) => {},
-      numFocusableTargets: () => /* number */ 0,
-      setDialogFocusFirstTarget: () => {},
-      setInitialFocus: () => {},
-      getFocusableElements: (/* handler: EventListener */) => /* Array<Element> */ [],
-      saveElementTabState: (/* el: Element */) => {},
-      restoreElementTabState: (/* el: Element */) => {},
-      makeElementUntabbable: (/* el: Element */) => {},
-      setBodyAttr: (/* attr: string, val: string */) => {},
-      rmBodyAttr: (/* attr: string */) => {},
-      getFocusedTarget: () => {},
-      setFocusedTarget: (/* target: EventTarget */) => {},
       notifyAccept: () => {},
       notifyCancel: () => {},
+      trapFocusOnSurface: () => {},
+      untrapFocusOnSurface: () => {},
     };
   }
 
   constructor(adapter) {
     super(Object.assign(MDCDialogFoundation.defaultAdapter, adapter));
 
-    this.lastFocusedTarget_ = null;
-    this.currentFocusedElIndex_ = -1;
     this.isOpen_ = false;
-    this.isResettingToFirstFocusTarget_ = false;
     this.componentClickHandler_ = () => this.cancel(true);
     this.dialogClickHandler_ = (evt) => this.handleDialogClick_(evt);
-    this.focusHandler_ = (evt) => this.setFocus_(evt);
     this.documentKeydownHandler_ = (evt) => {
       if (evt.key && evt.key === 'Escape' || evt.keyCode === 27) {
         this.cancel(true);
@@ -77,42 +61,31 @@ export default class MDCDialogFoundation extends MDCFoundation {
   }
 
   destroy() {
-    // Ensure that dialog is cleaned up when destroyed
     this.close();
   }
 
   open() {
     this.isOpen_ = true;
-    this.lastFocusedTarget_ = this.adapter_.getFocusedTarget();
-    this.makeTabbable_();
+    this.disableScroll_();
+    this.adapter_.setStyle('visibility', 'visible');
+    this.adapter_.addClass(MDCDialogFoundation.cssClasses.OPEN);
+    this.adapter_.trapFocusOnSurface();
+
     this.adapter_.registerDocumentKeydownHandler(this.documentKeydownHandler_);
     this.adapter_.registerSurfaceInteractionHandler('click', this.dialogClickHandler_);
     this.adapter_.registerInteractionHandler('click', this.componentClickHandler_);
-    this.adapter_.setInitialFocus();
-    this.adapter_.registerFocusTrappingHandler(this.focusHandler_);
-    this.disableScroll_();
-    this.adapter_.setBodyAttr('aria-hidden', 'true');
-    this.adapter_.setAttr('aria-hidden', 'false');
-    this.adapter_.addClass(MDCDialogFoundation.cssClasses.OPEN);
-    this.currentFocusedElIndex_ = this.adapter_.numFocusableTargets() - 1;
   }
 
   close() {
     this.isOpen_ = false;
-    this.makeUntabbable_();
+    this.adapter_.untrapFocusOnSurface();
+    this.adapter_.removeClass(MDCDialogFoundation.cssClasses.OPEN);
+    this.adapter_.setStyle('visibility', 'hidden');
+    this.enableScroll_();
+
     this.adapter_.deregisterSurfaceInteractionHandler('click', this.dialogClickHandler_);
     this.adapter_.deregisterDocumentKeydownHandler(this.documentKeydownHandler_);
     this.adapter_.deregisterInteractionHandler('click', this.componentClickHandler_);
-    this.adapter_.deregisterFocusTrappingHandler(this.focusHandler_);
-    this.adapter_.removeClass(MDCDialogFoundation.cssClasses.OPEN);
-    this.enableScroll_();
-    this.adapter_.rmBodyAttr('aria-hidden');
-    this.adapter_.setAttr('aria-hidden', 'true');
-
-    if (this.lastFocusedTarget_) {
-      this.adapter_.setFocusedTarget(this.lastFocusedTarget_);
-    }
-    this.lastFocusedTarget_ = null;
   }
 
   isOpen() {
@@ -142,44 +115,6 @@ export default class MDCDialogFoundation extends MDCFoundation {
       this.accept(true);
     } else if (this.adapter_.eventTargetHasClass(target, cssClasses.CANCEL_BTN)) {
       this.cancel(true);
-    }
-  }
-
-  makeUntabbable_() {
-    const elements = this.adapter_.getFocusableElements();
-    if (elements) {
-      for (let i = 0; i < elements.length; i++) {
-        this.adapter_.saveElementTabState(elements[i]);
-        this.adapter_.makeElementUntabbable(elements[i]);
-      }
-    }
-  }
-
-  makeTabbable_() {
-    const elements = this.adapter_.getFocusableElements();
-    if (elements) {
-      for (let i = 0; i < elements.length; i++) {
-        this.adapter_.restoreElementTabState(elements[i]);
-      }
-    }
-  }
-
-  setFocus_(evt) {
-    if (!evt.relatedTarget) {
-      // Do not increment the focused el index when re-focusing on same element, e.g. switching windows
-      return;
-    }
-
-    if (this.isResettingToFirstFocusTarget_) {
-      return;
-    }
-
-    this.currentFocusedElIndex_ = (this.currentFocusedElIndex_ + 1) % this.adapter_.numFocusableTargets();
-
-    if (this.currentFocusedElIndex_ === 0) {
-      this.isResettingToFirstFocusTarget_ = true;
-      this.adapter_.setDialogFocusFirstTarget();
-      this.isResettingToFirstFocusTarget_ = false;
     }
   }
 
--- packages/mdc-dialog/util.js
+++ packages/mdc-dialog/util.js
@@ -13,43 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const TAB_DATA = 'data-mdc-tabindex';
-const TAB_DATA_HANDLED = 'data-mdc-tabindex-handled';
 
-let supportsPassive_;
+import createFocusTrap from 'focus-trap';
 
-// Determine whether the current browser supports passive event listeners, and if so, use them.
-export function applyPassive(globalObj = window, forceRefresh = false) {
-  if (supportsPassive_ === undefined || forceRefresh) {
-    let isSupported = false;
-    try {
-      globalObj.document.addEventListener('test', null, {get passive() {
-        isSupported = true;
-      }});
-    } catch (e) { }
-
-    supportsPassive_ = isSupported;
-  }
-
-  return supportsPassive_ ? {passive: true} : false;
-}
-
-export function saveElementTabState(el) {
-  if (el.hasAttribute('tabindex')) {
-    el.setAttribute(TAB_DATA, el.getAttribute('tabindex'));
-  }
-  el.setAttribute(TAB_DATA_HANDLED, true);
-}
-
-export function restoreElementTabState(el) {
-  // Only modify elements we've already handled, in case anything was dynamically added since we saved state.
-  if (el.hasAttribute(TAB_DATA_HANDLED)) {
-    if (el.hasAttribute(TAB_DATA)) {
-      el.setAttribute('tabindex', el.getAttribute(TAB_DATA));
-      el.removeAttribute(TAB_DATA);
-    } else {
-      el.removeAttribute('tabindex');
-    }
-    el.removeAttribute(TAB_DATA_HANDLED);
-  }
+export function createFocusTrapInstance(surfaceEl, acceptButtonEl, focusTrapFactory = createFocusTrap) {
+  return focusTrapFactory(surfaceEl, {
+    initialFocus: acceptButtonEl,
+    clickOutsideDeactivates: true,
+  });
 }
