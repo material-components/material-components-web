8624b0aedadd7309423e3209e8780a2196898426
lynnjepsen <lynnjepsen@google.com>
2017-09-29 15:53:16 -0700

    chore: Maintain license comment when transpiling for goog.module format

--- scripts/rewrite-decl-statements-for-closure-test.js
+++ scripts/rewrite-decl-statements-for-closure-test.js
@@ -138,7 +138,13 @@ function transform(srcFile, rootDir) {
       }
 
       const variableDeclarator = t.variableDeclarator(variableDeclaratorId, callExpression);
-      path.replaceWith(t.variableDeclaration('const', [variableDeclarator]));
+      const variableDeclaration = t.variableDeclaration('const', [variableDeclarator]);
+      if (path.node.comments && path.node.comments.length > 0) {
+        const commentValue = path.node.comments[0].value;
+        variableDeclaration.comments = variableDeclaration.comments || [];
+        variableDeclaration.comments.push({type: 'CommentBlock', value: commentValue});
+      }
+      path.replaceWith(variableDeclaration);
     },
   });
 
@@ -156,7 +162,7 @@ function transform(srcFile, rootDir) {
   const packageParts = relativePath.replace('mdc-', '').replace('.js', '').split('/');
   const packageStr = 'mdc.' + packageParts.join('.').replace('.index', '');
 
-  outputCode = 'goog.module(\'' + packageStr + '\')\n' + outputCode;
+  outputCode = 'goog.module(\'' + packageStr + '\');\n' + outputCode;
   fs.writeFileSync(srcFile, outputCode, 'utf8');
   console.log(`[rewrite] ${srcFile}`);
 }
