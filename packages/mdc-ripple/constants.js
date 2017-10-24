55061c6cbe25b0224d2bd729fa518fb174d16418
Aaron J Spetner <aaron.spetner@spetnik.com>
2017-10-24 05:42:49 -0500

    fix(ripple): add 'keyup' to UP_EVENTS and remove unnecessary activation
    - 'keyup' also causes the same issue as the mouse events
    - there is no need to rerun the activation code on 'touchstart'

--- packages/mdc-ripple/constants.js
+++ packages/mdc-ripple/constants.js
@@ -47,7 +47,7 @@ const events = {
   // 'Up' events needs to be at the window level, because if they
   // are at the component level they will not fire if the mouse
   // moves off of the element (or if the touch event has movement)
-  UP_EVENTS: ['mouseup', 'pointerup', 'touchend'],
+  UP_EVENTS: ['mouseup', 'pointerup', 'touchend', 'keyup'],
 };
 
 export {cssClasses, strings, numbers, events};
