b31fc4b50e3f19d767c1b95f35466d05023894b1
Andy Dvorak <advorak@google.com>
2017-10-26 16:13:17 +0200

    WIP: Pass through CLI args to docker

--- scripts/docker/run-gcloud-image.sh
+++ scripts/docker/run-gcloud-image.sh
@@ -21,4 +21,4 @@ set -e
 cd "`dirname ${BASH_SOURCE[0]}`"
 
 gcloud docker -- pull us.gcr.io/material-components-web/dev-server:latest
-docker run --interactive --tty -p 8080:8080 us.gcr.io/material-components-web/dev-server:latest
+docker run --interactive --tty -p 8080:8080 us.gcr.io/material-components-web/dev-server:latest -- "$@"
