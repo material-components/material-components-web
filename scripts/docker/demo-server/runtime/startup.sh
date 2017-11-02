07221631b207502add1d283d3dd83aee4c1a0333
Andy Dvorak <advorak@google.com>
2017-11-01 21:47:07 -0700

    WIP: Use explicit command instead of `npm run dev`
    Allows us to drop hot reloading (we don't need it)

--- scripts/docker/demo-server/runtime/startup.sh
+++ scripts/docker/demo-server/runtime/startup.sh
@@ -101,4 +101,5 @@ echo 'Installing node modules...'
 npm install
 
 echo 'Starting demo server...'
-npm run dev
+PATH="$PATH:$PWD/node_modules/.bin"
+MDC_ENV=development webpack-dev-server --progress --content-base demos --host 0.0.0.0
