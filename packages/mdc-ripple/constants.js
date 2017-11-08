242368ec1b12b9178c278f7254ad12f127edda2c
Kenneth G. Franqueiro <kfranqueiro@google.com>
2017-11-08 16:27:51 -0500

    WIP Update ripple constants tied to animation durations

--- packages/mdc-ripple/constants.js
+++ packages/mdc-ripple/constants.js
@@ -37,8 +37,8 @@ const strings = {
 const numbers = {
   PADDING: 10,
   INITIAL_ORIGIN_SCALE: 0.6,
-  DEACTIVATION_TIMEOUT_MS: 150,
-  FG_DEACTIVATION_MS: 83,
+  DEACTIVATION_TIMEOUT_MS: 225, // Corresponds to $mdc-ripple-translate-duration (i.e. activation animation duration)
+  FG_DEACTIVATION_MS: 150, // Corresponds to $mdc-ripple-fade-out-duration (i.e. deactivation animation duration)
 };
 
 export {cssClasses, strings, numbers};
