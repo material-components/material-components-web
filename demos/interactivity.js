79a89f98c035551598ca28182c5d8473c0b1cc28
Andy Dvorak <advorak@google.com>
2018-01-16 14:25:05 -0800

    WIP: Move theme swapping code into theme/index.js

--- demos/interactivity.js
+++ demos/interactivity.js
@@ -20,34 +20,20 @@ import * as pony from './ponyfill.js';
 import * as util from './util.js';
 
 const classes = {
-  MDC_LIST_ITEM: 'mdc-list-item',
   TOOLBAR_PROGRESS_BAR_ACTIVE: 'demo-toolbar-progress-bar--active',
-  THEME_MENU_ITEM_SELECTED: 'demo-theme-menu__list-item--selected',
-  COMPONENT_SECTION: 'demo-component-section',
-  COMPONENT_SECTION_FLASH: 'demo-component-section--flash',
-  COMPONENT_SECTION_PERMALINK: 'demo-component-section__permalink',
-  COMPONENT_SECTION_HEADING_FOCUS_WITHIN: 'demo-component-section__heading--focus-within',
 };
 
 const attributes = {
-  THEME_NAME: 'data-theme',
   HOT_SWAP: 'data-hot',
 };
 
 const ids = {
-  THEME_STYLESHEET: 'theme-stylesheet',
   TOOLBAR_PROGRESS_BAR: 'demo-toolbar-progress-bar',
   RTL_ACTION: 'rtl-action',
-  THEME_COLOR_MENU: 'theme-color-menu',
-  THEME_COLOR_MENU_ACTION: 'theme-color-action',
-};
-
-const events = {
-  MDC_SIMPLE_MENU_SELECTED: 'MDCSimpleMenu:selected',
 };
 
 /** @abstract */
-class InteractivityProvider {
+export class InteractivityProvider {
   constructor(root) {
     /** @protected {!Element|!Document} */
     this.root_ = root;
@@ -64,6 +50,7 @@ class InteractivityProvider {
   /**
    * @param {string} id
    * @param {!Element|!Document=} opt_root
+   * @return {?Element}
    * @protected
    */
   getElementById_(id, opt_root) {
@@ -72,8 +59,9 @@ class InteractivityProvider {
   }
 
   /**
-   * @param {string} id
+   * @param {string} selector
    * @param {!Element|!Document=} opt_root
+   * @return {!Array<!Element>}
    * @protected
    */
   querySelectorAll_(selector, opt_root) {
@@ -82,21 +70,26 @@ class InteractivityProvider {
   }
 }
 
-class ToolbarManager extends InteractivityProvider {
+export class ToolbarProvider extends InteractivityProvider {
   /** @param {!Element|!Document} root */
   static attachTo(root) {
-    const instance = new ToolbarManager(root);
+    const instance = new ToolbarProvider(root);
     instance.initialize();
     return instance;
   }
 
   /** @override */
   initialize() {
+    /** @type {?Element} */
     this.progressBarEl_ = this.getElementById_(ids.TOOLBAR_PROGRESS_BAR);
   }
 
   /** @param {boolean} isLoading */
   setIsLoading(isLoading) {
+    if (!this.progressBarEl_) {
+      return;
+    }
+
     if (isLoading) {
       this.progressBarEl_.classList.add(classes.TOOLBAR_PROGRESS_BAR_ACTIVE);
     } else {
@@ -105,70 +98,21 @@ class ToolbarManager extends InteractivityProvider {
   }
 }
 
-class Permalinker extends InteractivityProvider {
-  /** @param {!Element|!Document} root */
-  static attachTo(root) {
-    const instance = new Permalinker(root);
-    instance.initialize();
-    return instance;
+export class HotSwapper extends InteractivityProvider {
+  constructor(root) {
+    super(root);
+
+    /** @type {number} */
+    this.numPending_ = 0;
   }
 
-  /** @private {number} */
-  static get flashDurationMs_() {
-    return 200;
-  }
-
-  /** @override */
-  initialize() {
-    this.root_.addEventListener('focusin', (evt) => {
-      if (evt.target.classList.contains(classes.COMPONENT_SECTION_PERMALINK)) {
-        evt.target.parentElement.classList.add(classes.COMPONENT_SECTION_HEADING_FOCUS_WITHIN);
-      }
-    });
-
-    this.root_.addEventListener('focusout', (evt) => {
-      if (evt.target.classList.contains(classes.COMPONENT_SECTION_PERMALINK)) {
-        evt.target.parentElement.classList.remove(classes.COMPONENT_SECTION_HEADING_FOCUS_WITHIN);
-      }
-    });
-
-    this.window_.addEventListener('hashchange', () => this.flashCurrentSection_());
-    this.flashCurrentSection_();
-  }
-
-  // TODO(acdvorak): Replace page URL hash with current section on scroll
-  /** @private */
-  flashCurrentSection_() {
-    const id = location.href.split('#')[1];
-    if (!id) {
-      return;
-    }
-
-    const anchor = this.document_.getElementById(id);
-    if (!anchor) {
-      return;
-    }
-
-    const section = pony.closest(anchor, `.${classes.COMPONENT_SECTION}`);
-    if (!section) {
-      return;
-    }
-
-    section.classList.add(classes.COMPONENT_SECTION_FLASH);
-    setTimeout(() => {
-      section.classList.remove(classes.COMPONENT_SECTION_FLASH);
-    }, Permalinker.flashDurationMs_);
-  }
-}
-
-class Themer extends InteractivityProvider {
   /**
    * @param {!Element|!Document} root
-   * @param {!ToolbarManager} toolbarManager
+   * @param {!ToolbarProvider} toolbarProvider
    */
-  static attachTo(root, toolbarManager) {
-    const instance = new Themer(root);
-    instance.initialize(toolbarManager);
+  static attachTo(root, toolbarProvider) {
+    const instance = new HotSwapper(root);
+    instance.initialize(toolbarProvider);
     return instance;
   }
 
@@ -178,60 +122,46 @@ class Themer extends InteractivityProvider {
   }
 
   /**
-   * @param {!ToolbarManager} toolbarManager
+   * @param {!ToolbarProvider} toolbarProvider
    * @override
    */
-  initialize(toolbarManager) {
-    this.toolbarManager_ = toolbarManager;
-    this.themeMenuEl_ = this.getElementById_(ids.THEME_COLOR_MENU);
+  initialize(toolbarProvider) {
+    /** @type {!ToolbarProvider} */
+    this.toolbarProvider_ = toolbarProvider;
+
+    /** @type {?Element} */
+    this.rtlActionEl_ = this.getElementById_(ids.RTL_ACTION);
 
     this.registerRTLToggleHandler_();
-    this.registerThemeMenuOpenHandler_();
-    this.registerThemeMenuChangeHandler_();
     this.registerHotUpdateHandler_();
-    this.registerUrlChangeHandler_();
-    this.selectCurrentThemeInMenu_();
   }
 
   /** @private */
   registerRTLToggleHandler_() {
-    this.getElementById_(ids.RTL_ACTION).addEventListener('click', (evt) => this.toggleRTL_(evt));
-  }
-
-  /** @private */
-  registerThemeMenuOpenHandler_() {
-    const menu = new mdc.menu.MDCSimpleMenu(this.themeMenuEl_);
-    const actionEl = this.getElementById_(ids.THEME_COLOR_MENU_ACTION);
-    actionEl.addEventListener('click', () => menu.open = !menu.open);
-  }
-
-  /** @private */
-  registerThemeMenuChangeHandler_() {
-    this.themeMenuEl_.addEventListener(events.MDC_SIMPLE_MENU_SELECTED, (evt) => {
-      const safeNewTheme = getSafeDemoTheme(evt.detail.item.getAttribute(attributes.THEME_NAME));
-      this.swapTheme_(safeNewTheme);
-      this.pushHistoryState_(safeNewTheme);
-    });
+    if (!this.rtlActionEl_) {
+      return;
+    }
+    this.rtlActionEl_.addEventListener('click', (evt) => this.toggleRTL_(evt));
   }
 
   /** @private */
   registerHotUpdateHandler_() {
-    const hotSwapAllStylesheets = util.debounce(() => this.hotSwapAllStylesheets_(), Themer.hotUpdateWaitPeriodMs_);
+    const hotSwapAllStylesheets = util.debounce(() => this.hotSwapAllStylesheets_(), HotSwapper.hotUpdateWaitPeriodMs_);
     this.window_.addEventListener('message', (evt) => {
-      if (typeof evt.data === 'string' && evt.data.indexOf('webpackHotUpdate') === 0) {
+      if (this.isWebpackRecompileStart_(evt)) {
+        this.toolbarProvider_.setIsLoading(true);
+      } else if (this.isWebpackRecompileEnd_(evt)) {
         hotSwapAllStylesheets();
       }
     });
   }
 
-  /** @private */
-  registerUrlChangeHandler_() {
-    this.window_.addEventListener('popstate', (evt) => {
-      const getDefaultState = () => ({theme: getSafeDemoThemeFromUri()});
-      const unsafeNewTheme = (evt.state || getDefaultState()).theme;
-      const safeNewTheme = getSafeDemoTheme(unsafeNewTheme);
-      this.swapTheme_(safeNewTheme);
-    });
+  isWebpackRecompileStart_(evt) {
+    return Boolean(evt.data) && evt.data.type === 'webpackInvalid';
+  }
+
+  isWebpackRecompileEnd_(evt) {
+    return typeof evt.data === 'string' && evt.data.indexOf('webpackHotUpdate') === 0;
   }
 
   /** @private */
@@ -253,7 +183,7 @@ class Themer extends InteractivityProvider {
     });
   }
 
-  /** @private */
+  /** @protected */
   hotSwapStylesheet_(oldLink, newUri) {
     // Remove query string from old URI
     const oldUri = (oldLink.getAttribute('href') || '').replace(/[?].*$/, '');
@@ -277,7 +207,8 @@ class Themer extends InteractivityProvider {
 
     logHotSwap('swapping', '...');
 
-    this.toolbarManager_.setIsLoading(true);
+    this.toolbarProvider_.setIsLoading(true);
+    this.numPending_++;
 
     const newLink = oldLink.cloneNode(false);
     newLink.setAttribute('href', newUri);
@@ -293,60 +224,17 @@ class Themer extends InteractivityProvider {
         oldLink.parentNode.removeChild(oldLink);
       });
 
-      this.toolbarManager_.setIsLoading(false);
+      this.numPending_--;
+      if (this.numPending_ === 0) {
+        this.toolbarProvider_.setIsLoading(false);
+      }
     });
 
     oldLink.parentNode.insertBefore(newLink, oldLink);
-  }
-
-  /** @private */
-  pushHistoryState_(safeNewTheme) {
-    const hash = this.window_.location.hash || '';
-    const url = `?theme=${unwrapSafeDemoTheme(safeNewTheme) + hash}`;
-    const data = {theme: unwrapSafeDemoTheme(safeNewTheme)};
-    history.pushState(data, '', url);
-  }
-
-  /**
-   * @param {!SafeTheme} safeNewTheme
-   * @private
-   */
-  swapTheme_(safeNewTheme) {
-    const oldLink = this.getElementById_(ids.THEME_STYLESHEET);
-    const newUri = `/assets/theme/theme-${unwrapSafeDemoTheme(safeNewTheme)}.css`;
-    this.hotSwapStylesheet_(oldLink, newUri);
-    this.selectThemeInMenu_(safeNewTheme);
-  }
-
-  /** @private */
-  selectCurrentThemeInMenu_() {
-    this.selectThemeInMenu_(getSafeDemoThemeFromUri());
-  }
-
-  /**
-   * @param {!SafeTheme} safeNewTheme
-   * @private
-   */
-  selectThemeInMenu_(safeNewTheme) {
-    const selectedMenuItemClass = classes.THEME_MENU_ITEM_SELECTED;
-    const mdcListItemClass = classes.MDC_LIST_ITEM;
-    const themeAttr = attributes.THEME_NAME;
-    const menuActionId = ids.THEME_COLOR_MENU_ACTION;
-
-    const unwrappedSafeNewTheme = unwrapSafeDemoTheme(safeNewTheme);
-    const oldSelectedItems = dom.getAll(`.${selectedMenuItemClass}[${themeAttr}]`, this.themeMenuEl_);
-    const newSelectedItem =
-      this.themeMenuEl_.querySelector(`.${mdcListItemClass}[${themeAttr}="${unwrappedSafeNewTheme}"]`);
-
-    oldSelectedItems.forEach((colorIcon) => colorIcon.classList.remove(selectedMenuItemClass));
-    newSelectedItem.classList.add(selectedMenuItemClass);
-
-    this.getElementById_(menuActionId).setAttribute(themeAttr, unwrappedSafeNewTheme);
   }
 }
 
 /** @param {!Element|!Document} root */
 export function init(root) {
-  Permalinker.attachTo(root);
-  Themer.attachTo(root, ToolbarManager.attachTo(root));
+  HotSwapper.attachTo(root, ToolbarProvider.attachTo(root));
 }
--- demos/theme/index.js
+++ demos/theme/index.js
@@ -15,118 +15,307 @@
  * limitations under the License.
  */
 
+import {InteractivityProvider, ToolbarProvider, HotSwapper} from '../interactivity.js';
 import * as dom from '../dom.js';
 import * as util from '../util.js';
+import * as pony from '../ponyfill.js';
 
-(() => {
-  const initializers = {
-    button() {
-      dom.getAll('.mdc-button').forEach((button) => {
-        mdc.ripple.MDCRipple.attachTo(button);
+const classes = {
+  MDC_LIST_ITEM: 'mdc-list-item',
+  THEME_MENU_ITEM_SELECTED: 'demo-theme-menu__list-item--selected',
+  COMPONENT_SECTION: 'demo-component-section',
+  COMPONENT_SECTION_FLASH: 'demo-component-section--flash',
+  COMPONENT_SECTION_PERMALINK: 'demo-component-section__permalink',
+  COMPONENT_SECTION_HEADING_FOCUS_WITHIN: 'demo-component-section__heading--focus-within',
+};
+
+const attributes = {
+  THEME_NAME: 'data-theme',
+};
+
+const ids = {
+  THEME_STYLESHEET: 'theme-stylesheet',
+  THEME_COLOR_MENU: 'theme-color-menu',
+  THEME_COLOR_MENU_ACTION: 'theme-color-action',
+};
+
+const events = {
+  MDC_SIMPLE_MENU_SELECTED: 'MDCSimpleMenu:selected',
+};
+
+class Permalinker extends InteractivityProvider {
+  /** @param {!Element|!Document} root */
+  static attachTo(root) {
+    const instance = new Permalinker(root);
+    instance.initialize();
+    return instance;
+  }
+
+  /** @private {number} */
+  static get flashDurationMs_() {
+    return 200;
+  }
+
+  /** @override */
+  initialize() {
+    this.root_.addEventListener('focusin', (evt) => {
+      if (evt.target.classList.contains(classes.COMPONENT_SECTION_PERMALINK)) {
+        evt.target.parentElement.classList.add(classes.COMPONENT_SECTION_HEADING_FOCUS_WITHIN);
+      }
+    });
+
+    this.root_.addEventListener('focusout', (evt) => {
+      if (evt.target.classList.contains(classes.COMPONENT_SECTION_PERMALINK)) {
+        evt.target.parentElement.classList.remove(classes.COMPONENT_SECTION_HEADING_FOCUS_WITHIN);
+      }
+    });
+
+    this.window_.addEventListener('hashchange', () => this.flashCurrentSection_());
+    this.flashCurrentSection_();
+  }
+
+  // TODO(acdvorak): Replace page URL hash with current section on scroll
+  /** @private */
+  flashCurrentSection_() {
+    const id = location.href.split('#')[1];
+    if (!id) {
+      return;
+    }
+
+    const anchor = this.document_.getElementById(id);
+    if (!anchor) {
+      return;
+    }
+
+    const section = pony.closest(anchor, `.${classes.COMPONENT_SECTION}`);
+    if (!section) {
+      return;
+    }
+
+    section.classList.add(classes.COMPONENT_SECTION_FLASH);
+    setTimeout(() => {
+      section.classList.remove(classes.COMPONENT_SECTION_FLASH);
+    }, Permalinker.flashDurationMs_);
+  }
+}
+
+class ThemePicker extends HotSwapper {
+  /**
+   * @param {!Element|!Document} root
+   * @param {!ToolbarProvider} toolbarProvider
+   */
+  static attachTo(root, toolbarProvider) {
+    const instance = new ThemePicker(root);
+    instance.initialize(toolbarProvider);
+    return instance;
+  }
+
+  /**
+   * @param {!ToolbarProvider} toolbarProvider
+   * @override
+   */
+  initialize(toolbarProvider) {
+    /** @type {!ToolbarProvider} */
+    this.toolbarProvider_ = toolbarProvider;
+
+    /** @type {?Element} */
+    this.themeMenuEl_ = this.getElementById_(ids.THEME_COLOR_MENU);
+
+    this.registerThemeMenuOpenHandler_();
+    this.registerThemeMenuChangeHandler_();
+    this.registerUrlChangeHandler_();
+    this.selectCurrentThemeInMenu_();
+  }
+
+  /** @private */
+  registerThemeMenuOpenHandler_() {
+    const menu = new mdc.menu.MDCSimpleMenu(this.themeMenuEl_);
+    const actionEl = this.getElementById_(ids.THEME_COLOR_MENU_ACTION);
+    actionEl.addEventListener('click', () => menu.open = !menu.open);
+  }
+
+  /** @private */
+  registerThemeMenuChangeHandler_() {
+    this.themeMenuEl_.addEventListener(events.MDC_SIMPLE_MENU_SELECTED, (evt) => {
+      const safeNewTheme = getSafeDemoTheme(evt.detail.item.getAttribute(attributes.THEME_NAME));
+      this.swapTheme_(safeNewTheme);
+      this.pushHistoryState_(safeNewTheme);
+    });
+  }
+
+  /** @private */
+  registerUrlChangeHandler_() {
+    this.window_.addEventListener('popstate', (evt) => {
+      const getDefaultState = () => ({theme: getSafeDemoThemeFromUri()});
+      const unsafeNewTheme = (evt.state || getDefaultState()).theme;
+      const safeNewTheme = getSafeDemoTheme(unsafeNewTheme);
+      this.swapTheme_(safeNewTheme);
+    });
+  }
+
+  /** @private */
+  pushHistoryState_(safeNewTheme) {
+    const hash = this.window_.location.hash || '';
+    const url = `?theme=${unwrapSafeDemoTheme(safeNewTheme) + hash}`;
+    const data = {theme: unwrapSafeDemoTheme(safeNewTheme)};
+    history.pushState(data, '', url);
+  }
+
+  /**
+   * @param {!SafeTheme} safeNewTheme
+   * @private
+   */
+  swapTheme_(safeNewTheme) {
+    const oldLink = this.getElementById_(ids.THEME_STYLESHEET);
+    const newUri = `/assets/theme/theme-${unwrapSafeDemoTheme(safeNewTheme)}.css`;
+    this.hotSwapStylesheet_(oldLink, newUri);
+    this.selectThemeInMenu_(safeNewTheme);
+  }
+
+  /** @private */
+  selectCurrentThemeInMenu_() {
+    this.selectThemeInMenu_(getSafeDemoThemeFromUri());
+  }
+
+  /**
+   * @param {!SafeTheme} safeNewTheme
+   * @private
+   */
+  selectThemeInMenu_(safeNewTheme) {
+    const selectedMenuItemClass = classes.THEME_MENU_ITEM_SELECTED;
+    const mdcListItemClass = classes.MDC_LIST_ITEM;
+    const themeAttr = attributes.THEME_NAME;
+    const menuActionId = ids.THEME_COLOR_MENU_ACTION;
+
+    const unwrappedSafeNewTheme = unwrapSafeDemoTheme(safeNewTheme);
+    const oldSelectedItems = dom.getAll(`.${selectedMenuItemClass}[${themeAttr}]`, this.themeMenuEl_);
+    const newSelectedItem =
+      this.themeMenuEl_.querySelector(`.${mdcListItemClass}[${themeAttr}="${unwrappedSafeNewTheme}"]`);
+
+    oldSelectedItems.forEach((colorIcon) => colorIcon.classList.remove(selectedMenuItemClass));
+    newSelectedItem.classList.add(selectedMenuItemClass);
+
+    this.getElementById_(menuActionId).setAttribute(themeAttr, unwrappedSafeNewTheme);
+  }
+}
+
+const initializers = {
+  permalinker() {
+    const root = document;
+    Permalinker.attachTo(root);
+  },
+
+  themePicker() {
+    const root = document;
+    ThemePicker.attachTo(root, ToolbarProvider.attachTo(root));
+  },
+
+  button() {
+    dom.getAll('.mdc-button').forEach((button) => {
+      mdc.ripple.MDCRipple.attachTo(button);
+    });
+  },
+
+  checkbox() {
+    document.querySelector('#indeterminate-checkbox').indeterminate = true;
+
+    document.querySelector('#checkbox-toggle--indeterminate').addEventListener('click', () => {
+      const checkboxes = dom.getAll('.demo-checkbox-wrapper .mdc-checkbox__native-control');
+      checkboxes.forEach(function(checkbox) {
+        checkbox.indeterminate = !checkbox.indeterminate;
       });
-    },
+    });
 
-    checkbox() {
-      document.querySelector('#indeterminate-checkbox').indeterminate = true;
-
-      document.querySelector('#checkbox-toggle--indeterminate').addEventListener('click', () => {
-        const checkboxes = dom.getAll('.demo-checkbox-wrapper .mdc-checkbox__native-control');
-        checkboxes.forEach(function(checkbox) {
-          checkbox.indeterminate = !checkbox.indeterminate;
-        });
+    document.querySelector('#checkbox-toggle--align-end').addEventListener('click', () => {
+      const formFields = dom.getAll('.demo-checkbox-wrapper.mdc-form-field');
+      formFields.forEach(function(formField) {
+        formField.classList.toggle('mdc-form-field--align-end');
       });
+    });
+  },
 
-      document.querySelector('#checkbox-toggle--align-end').addEventListener('click', () => {
-        const formFields = dom.getAll('.demo-checkbox-wrapper.mdc-form-field');
-        formFields.forEach(function(formField) {
-          formField.classList.toggle('mdc-form-field--align-end');
-        });
+  drawer() {
+    const drawerEl = document.querySelector('.mdc-drawer--temporary');
+    const drawer = new mdc.drawer.MDCTemporaryDrawer(drawerEl);
+
+    dom.getAll('.demo-drawer-toggle').forEach((toggleElem) => {
+      toggleElem.addEventListener('click', () => {
+        drawer.open = true;
       });
-    },
+    });
 
-    drawer() {
-      const drawerEl = document.querySelector('.mdc-drawer--temporary');
-      const drawer = new mdc.drawer.MDCTemporaryDrawer(drawerEl);
+    drawerEl.addEventListener('MDCTemporaryDrawer:open', () => {
+      console.log('Received MDCTemporaryDrawer:open');
+    });
 
-      dom.getAll('.demo-drawer-toggle').forEach((toggleElem) => {
-        toggleElem.addEventListener('click', () => {
-          drawer.open = true;
-        });
-      });
+    drawerEl.addEventListener('MDCTemporaryDrawer:close', () => {
+      console.log('Received MDCTemporaryDrawer:close');
+    });
+  },
 
-      drawerEl.addEventListener('MDCTemporaryDrawer:open', () => {
-        console.log('Received MDCTemporaryDrawer:open');
-      });
+  fab() {
+    dom.getAll('.mdc-fab').forEach((fab) => {
+      mdc.ripple.MDCRipple.attachTo(fab);
+    });
+  },
 
-      drawerEl.addEventListener('MDCTemporaryDrawer:close', () => {
-        console.log('Received MDCTemporaryDrawer:close');
-      });
-    },
+  iconToggle() {
+    dom.getAll('.mdc-icon-toggle').forEach((iconToggleEl) => {
+      mdc.iconToggle.MDCIconToggle.attachTo(iconToggleEl);
+    });
+  },
 
-    fab() {
-      dom.getAll('.mdc-fab').forEach((fab) => {
-        mdc.ripple.MDCRipple.attachTo(fab);
-      });
-    },
+  linearProgress() {
+    dom.getAll('.mdc-linear-progress').forEach((progressEl) => {
+      const linearProgress = mdc.linearProgress.MDCLinearProgress.attachTo(progressEl);
+      linearProgress.progress = 0.5;
+      if (progressEl.dataset.buffer) {
+        linearProgress.buffer = 0.75;
+      }
+    });
+  },
 
-    iconToggle() {
-      dom.getAll('.mdc-icon-toggle').forEach((iconToggleEl) => {
-        mdc.iconToggle.MDCIconToggle.attachTo(iconToggleEl);
-      });
-    },
+  ripple() {
+    dom.getAll('.mdc-ripple-surface').forEach((surface) => {
+      mdc.ripple.MDCRipple.attachTo(surface);
+    });
+  },
 
-    linearProgress() {
-      dom.getAll('.mdc-linear-progress').forEach((progressEl) => {
-        const linearProgress = mdc.linearProgress.MDCLinearProgress.attachTo(progressEl);
-        linearProgress.progress = 0.5;
-        if (progressEl.dataset.buffer) {
-          linearProgress.buffer = 0.75;
-        }
-      });
-    },
+  select() {
+    dom.getAll('.mdc-select:not(select)').forEach((select) => {
+      mdc.select.MDCSelect.attachTo(select);
+    });
+  },
 
-    ripple() {
-      dom.getAll('.mdc-ripple-surface').forEach((surface) => {
-        mdc.ripple.MDCRipple.attachTo(surface);
-      });
-    },
+  slider() {
+    dom.getAll('.mdc-slider').forEach((slider) => {
+      mdc.slider.MDCSlider.attachTo(slider);
+    });
+  },
 
-    select() {
-      dom.getAll('.mdc-select:not(select)').forEach((select) => {
-        mdc.select.MDCSelect.attachTo(select);
-      });
-    },
+  tab() {
+    dom.getAll('.mdc-tab-bar').forEach((tabBar) => {
+      mdc.tabs.MDCTabBar.attachTo(tabBar);
+    });
+  },
 
-    slider() {
-      dom.getAll('.mdc-slider').forEach((slider) => {
-        mdc.slider.MDCSlider.attachTo(slider);
-      });
-    },
+  textfield() {
+    dom.getAll('.mdc-text-field').forEach((textField) => {
+      mdc.textField.MDCTextField.attachTo(textField);
+    });
+  },
 
-    tab() {
-      dom.getAll('.mdc-tab-bar').forEach((tabBar) => {
-        mdc.tabs.MDCTabBar.attachTo(tabBar);
-      });
-    },
+  radio() {
+    dom.getAll('.mdc-form-field.demo-radio-form-field').forEach((formField) => {
+      const formFieldInstance = new mdc.formField.MDCFormField(formField);
+      const radio = formField.querySelector('.mdc-radio');
+      if (radio) {
+        formFieldInstance.input = new mdc.radio.MDCRadio(radio);
+      }
+    });
+  },
+};
 
-    textfield() {
-      dom.getAll('.mdc-text-field').forEach((textField) => {
-        mdc.textField.MDCTextField.attachTo(textField);
-      });
-    },
-
-    radio() {
-      dom.getAll('.mdc-form-field.demo-radio-form-field').forEach((formField) => {
-        const formFieldInstance = new mdc.formField.MDCFormField(formField);
-        const radio = formField.querySelector('.mdc-radio');
-        if (radio) {
-          formFieldInstance.input = new mdc.radio.MDCRadio(radio);
-        }
-      });
-    },
-  };
-
-  demoReady(() => {
-    util.objectForEach(initializers, (initializer) => initializer());
-  });
-})();
+demoReady(() => {
+  util.objectForEach(initializers, (initializer) => initializer());
+});
