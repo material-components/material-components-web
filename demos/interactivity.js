ee38ec165d87f27e4c06048d3e69fbec18fb438e
Andy Dvorak <advorak@google.com>
2018-01-16 20:23:10 -0800

    WIP: Copy experimental/theme/demo branch at 7e588ce

---
+++ demos/interactivity.js
@@ -0,0 +1,277 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as dom from './dom.js';
+import * as pony from './ponyfill.js';
+import * as util from './util.js';
+
+const classes = {
+  TOOLBAR_PROGRESS_BAR_ACTIVE: 'demo-toolbar-progress-bar--active',
+};
+
+const attributes = {
+  HOT_SWAP: 'data-hot',
+};
+
+const ids = {
+  RTL_ACTION: 'rtl-action',
+  TOOLBAR_PROGRESS_BAR: 'demo-toolbar-progress-bar',
+};
+
+/** @abstract */
+export class InteractivityProvider {
+  constructor(root) {
+    /** @protected {!Element|!Document} */
+    this.root_ = root;
+
+    /** @protected {!Document} */
+    this.document_ = this.root_.ownerDocument || this.root_;
+
+    /** @protected {!Window} */
+    this.window_ = this.document_.defaultView || this.document_.parentWindow;
+  }
+
+  initialize() {}
+
+  /**
+   * @param {string} id
+   * @param {!Element|!Document=} opt_root
+   * @return {?Element}
+   * @protected
+   */
+  getElementById_(id, opt_root) {
+    const root = opt_root || this.root_;
+    return root.querySelector(`#${id}`);
+  }
+
+  /**
+   * @param {string} selector
+   * @param {!Element|!Document=} opt_root
+   * @return {!Array<!Element>}
+   * @protected
+   */
+  querySelectorAll_(selector, opt_root) {
+    const root = opt_root || this.root_;
+    return dom.getAll(selector, root);
+  }
+}
+
+export class ToolbarProvider extends InteractivityProvider {
+  /** @param {!Element|!Document} root */
+  static attachTo(root) {
+    const instance = new ToolbarProvider(root);
+    instance.initialize();
+    return instance;
+  }
+
+  /** @override */
+  initialize() {
+    /** @type {?Element} */
+    this.progressBarEl_ = this.getElementById_(ids.TOOLBAR_PROGRESS_BAR);
+  }
+
+  /** @param {boolean} isLoading */
+  setIsLoading(isLoading) {
+    if (!this.progressBarEl_) {
+      return;
+    }
+
+    if (isLoading) {
+      this.progressBarEl_.classList.add(classes.TOOLBAR_PROGRESS_BAR_ACTIVE);
+    } else {
+      this.progressBarEl_.classList.remove(classes.TOOLBAR_PROGRESS_BAR_ACTIVE);
+    }
+  }
+}
+
+export class HotSwapper extends InteractivityProvider {
+  constructor(root) {
+    super(root);
+
+    /** @type {number} */
+    this.numPending_ = 0;
+  }
+
+  /**
+   * @param {!Element|!Document} root
+   * @param {!ToolbarProvider} toolbarProvider
+   */
+  static attachTo(root, toolbarProvider) {
+    const instance = new HotSwapper(root);
+    instance.initialize(toolbarProvider);
+    return instance;
+  }
+
+  /** @private {number} */
+  static get hotUpdateWaitPeriodMs_() {
+    return 250;
+  }
+
+  /**
+   * @param {!ToolbarProvider} toolbarProvider
+   * @override
+   */
+  initialize(toolbarProvider) {
+    /** @type {!ToolbarProvider} */
+    this.toolbarProvider_ = toolbarProvider;
+
+    /** @type {?Element} */
+    this.rtlActionEl_ = this.getElementById_(ids.RTL_ACTION);
+
+    this.registerRTLToggleHandler_();
+    this.registerHotUpdateHandler_();
+  }
+
+  /** @private */
+  registerRTLToggleHandler_() {
+    if (!this.rtlActionEl_) {
+      return;
+    }
+    this.rtlActionEl_.addEventListener('click', (evt) => this.toggleRTL_(evt));
+  }
+
+  /** @private */
+  registerHotUpdateHandler_() {
+    const hotSwapAllStylesheets = util.debounce(() => this.hotSwapAllStylesheets_(), HotSwapper.hotUpdateWaitPeriodMs_);
+    this.window_.addEventListener('message', (evt) => {
+      if (this.isWebpackRecompileStart_(evt)) {
+        this.toolbarProvider_.setIsLoading(true);
+      } else if (this.isWebpackRecompileEnd_(evt)) {
+        hotSwapAllStylesheets();
+      }
+    });
+  }
+
+  /**
+   * @param {!Event} evt
+   * @return {boolean}
+   * @private
+   */
+  isWebpackRecompileStart_(evt) {
+    return Boolean(evt.data) && evt.data.type === 'webpackInvalid';
+  }
+
+  /**
+   * @param {!Event} evt
+   * @return {boolean}
+   * @private
+   */
+  isWebpackRecompileEnd_(evt) {
+    return typeof evt.data === 'string' && evt.data.indexOf('webpackHotUpdate') === 0;
+  }
+
+  /**
+   * @param {!Event} evt
+   * @private
+   */
+  toggleRTL_(evt) {
+    const el = this.document_.documentElement;
+    if (el.getAttribute('dir') === 'rtl') {
+      el.setAttribute('dir', 'ltr');
+      evt.target.innerHTML = 'format_align_left';
+    } else {
+      el.setAttribute('dir', 'rtl');
+      evt.target.innerHTML = 'format_align_right';
+    }
+  }
+
+  /** @private */
+  hotSwapAllStylesheets_() {
+    dom.getAll(`link[${attributes.HOT_SWAP}]`, this.document_.head).forEach((link) => {
+      this.hotSwapStylesheet_(link);
+    });
+  }
+
+  /**
+   * @param {!Element} oldLink
+   * @param {string|undefined=} newUri
+   * @protected
+   */
+  hotSwapStylesheet_(oldLink, newUri) {
+    // Remove query string from old URI
+    const oldUri = (oldLink.getAttribute('href') || '').replace(/[?].*$/, '');
+
+    // oldLink was probably detached from the DOM
+    if (!oldUri) {
+      return;
+    }
+
+    if (!newUri) {
+      newUri = oldUri;
+    }
+
+    // Force IE 11 and Edge to bypass the cache and request a fresh copy of the CSS.
+    newUri += `?timestamp=${Date.now()}`;
+
+    const logHotSwap = (verb, trailingPunctuation) => {
+      const swapMessage = `"${oldUri}"${newUri ? ` with "${newUri}"` : ''}`;
+      console.log(`Hot ${verb} stylesheet ${swapMessage}${trailingPunctuation}`);
+    };
+
+    logHotSwap('swapping', '...');
+
+    this.toolbarProvider_.setIsLoading(true);
+    this.numPending_++;
+
+    const newId = oldLink.id || `stylesheet-${Math.floor(Math.random() * Date.now())}`;
+    oldLink.id = newId;
+
+    const newLink = oldLink.cloneNode(false);
+    newLink.setAttribute('href', newUri);
+
+    // IE 11 and Edge fire the `load` event twice for `<link>` elements.
+    newLink.addEventListener('load', util.debounce(() => {
+      logHotSwap('swapped', '!');
+
+      setTimeout(() => this.purgeOldStylesheets_(newId));
+
+      this.numPending_--;
+      if (this.numPending_ === 0) {
+        this.toolbarProvider_.setIsLoading(false);
+      }
+    }, 50));
+
+    oldLink.parentNode.insertBefore(newLink, oldLink);
+  }
+
+  /**
+   * @param {string} newId
+   * @private
+   */
+  purgeOldStylesheets_(newId) {
+    let oldLinks;
+
+    // New links are inserted before old links in the DOM, so we skip the first matching ID because it is the newest.
+    const getOldLinks = () => this.querySelectorAll_(`link[id="${newId}"]`).slice(1);
+
+    while ((oldLinks = getOldLinks()).length > 0) {
+      oldLinks.forEach((oldLink) => {
+        // Link has already been detached from the DOM. I'm not sure what causes this to happen; I've only seen it in
+        // IE 11 and/or Edge so far, and only occasionally.
+        if (!oldLink.parentNode) {
+          return;
+        }
+        oldLink.parentNode.removeChild(oldLink);
+      });
+    }
+  }
+}
+
+/** @param {!Element|!Document} root */
+export function init(root) {
+  HotSwapper.attachTo(root, ToolbarProvider.attachTo(root));
+}
---
+++ demos/theme/index.js
@@ -0,0 +1,206 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {InteractivityProvider, ToolbarProvider, HotSwapper} from '../interactivity.js';
+import * as dom from '../dom.js';
+import * as pony from '../ponyfill.js';
+
+const classes = {
+  MDC_LIST_ITEM: 'mdc-list-item',
+  THEME_MENU_ITEM_SELECTED: 'demo-theme-menu__list-item--selected',
+  COMPONENT_SECTION: 'demo-component-section',
+  COMPONENT_SECTION_FLASH: 'demo-component-section--flash',
+  COMPONENT_SECTION_PERMALINK: 'demo-component-section__permalink',
+  COMPONENT_SECTION_HEADING_FOCUS_WITHIN: 'demo-component-section__heading--focus-within',
+};
+
+const attributes = {
+  THEME_NAME: 'data-theme',
+};
+
+const ids = {
+  THEME_STYLESHEET: 'theme-stylesheet',
+  THEME_COLOR_MENU: 'theme-color-menu',
+  THEME_COLOR_MENU_ACTION: 'theme-color-action',
+};
+
+const events = {
+  MDC_SIMPLE_MENU_SELECTED: 'MDCSimpleMenu:selected',
+};
+
+class Permalinker extends InteractivityProvider {
+  /** @param {!Element|!Document} root */
+  static attachTo(root) {
+    const instance = new Permalinker(root);
+    instance.initialize();
+    return instance;
+  }
+
+  /** @private {number} */
+  static get flashDurationMs_() {
+    return 200;
+  }
+
+  /** @override */
+  initialize() {
+    this.root_.addEventListener('focusin', (evt) => {
+      if (evt.target.classList.contains(classes.COMPONENT_SECTION_PERMALINK)) {
+        evt.target.parentElement.classList.add(classes.COMPONENT_SECTION_HEADING_FOCUS_WITHIN);
+      }
+    });
+
+    this.root_.addEventListener('focusout', (evt) => {
+      if (evt.target.classList.contains(classes.COMPONENT_SECTION_PERMALINK)) {
+        evt.target.parentElement.classList.remove(classes.COMPONENT_SECTION_HEADING_FOCUS_WITHIN);
+      }
+    });
+
+    this.window_.addEventListener('hashchange', () => this.flashCurrentSection_());
+    this.flashCurrentSection_();
+  }
+
+  // TODO(acdvorak): Replace page URL hash with current section on scroll
+  /** @private */
+  flashCurrentSection_() {
+    const id = location.href.split('#')[1];
+    if (!id) {
+      return;
+    }
+
+    const anchor = this.document_.getElementById(id);
+    if (!anchor) {
+      return;
+    }
+
+    const section = pony.closest(anchor, `.${classes.COMPONENT_SECTION}`);
+    if (!section) {
+      return;
+    }
+
+    section.classList.add(classes.COMPONENT_SECTION_FLASH);
+    setTimeout(() => {
+      section.classList.remove(classes.COMPONENT_SECTION_FLASH);
+    }, Permalinker.flashDurationMs_);
+  }
+}
+
+class ThemePicker extends HotSwapper {
+  /**
+   * @param {!Element|!Document} root
+   * @param {!ToolbarProvider} toolbarProvider
+   */
+  static attachTo(root, toolbarProvider) {
+    const instance = new ThemePicker(root);
+    instance.initialize(toolbarProvider);
+    return instance;
+  }
+
+  /**
+   * @param {!ToolbarProvider} toolbarProvider
+   * @override
+   */
+  initialize(toolbarProvider) {
+    /** @type {!ToolbarProvider} */
+    this.toolbarProvider_ = toolbarProvider;
+
+    /** @type {?Element} */
+    this.themeMenuEl_ = this.getElementById_(ids.THEME_COLOR_MENU);
+
+    this.registerThemeMenuOpenHandler_();
+    this.registerThemeMenuChangeHandler_();
+    this.registerUrlChangeHandler_();
+    this.selectCurrentThemeInMenu_();
+  }
+
+  /** @private */
+  registerThemeMenuOpenHandler_() {
+    const menu = new mdc.menu.MDCSimpleMenu(this.themeMenuEl_);
+    const actionEl = this.getElementById_(ids.THEME_COLOR_MENU_ACTION);
+    actionEl.addEventListener('click', () => menu.open = !menu.open);
+  }
+
+  /** @private */
+  registerThemeMenuChangeHandler_() {
+    this.themeMenuEl_.addEventListener(events.MDC_SIMPLE_MENU_SELECTED, (evt) => {
+      const safeNewTheme = getSafeDemoTheme(evt.detail.item.getAttribute(attributes.THEME_NAME));
+      this.swapTheme_(safeNewTheme);
+      this.pushHistoryState_(safeNewTheme);
+    });
+  }
+
+  /** @private */
+  registerUrlChangeHandler_() {
+    this.window_.addEventListener('popstate', (evt) => {
+      const getDefaultState = () => ({theme: getSafeDemoThemeFromUri()});
+      const unsafeNewTheme = (evt.state || getDefaultState()).theme;
+      const safeNewTheme = getSafeDemoTheme(unsafeNewTheme);
+      this.swapTheme_(safeNewTheme);
+    });
+  }
+
+  /** @private */
+  pushHistoryState_(safeNewTheme) {
+    const hash = this.window_.location.hash || '';
+    const url = `?theme=${unwrapSafeDemoTheme(safeNewTheme) + hash}`;
+    const data = {theme: unwrapSafeDemoTheme(safeNewTheme)};
+    history.pushState(data, '', url);
+  }
+
+  /**
+   * @param {!SafeTheme} safeNewTheme
+   * @private
+   */
+  swapTheme_(safeNewTheme) {
+    const oldLink = this.getElementById_(ids.THEME_STYLESHEET);
+    const newUri = `/assets/theme/theme-${unwrapSafeDemoTheme(safeNewTheme)}.css`;
+    this.hotSwapStylesheet_(oldLink, newUri);
+    this.selectThemeInMenu_(safeNewTheme);
+  }
+
+  /** @private */
+  selectCurrentThemeInMenu_() {
+    this.selectThemeInMenu_(getSafeDemoThemeFromUri());
+  }
+
+  /**
+   * @param {!SafeTheme} safeNewTheme
+   * @private
+   */
+  selectThemeInMenu_(safeNewTheme) {
+    const selectedMenuItemClass = classes.THEME_MENU_ITEM_SELECTED;
+    const mdcListItemClass = classes.MDC_LIST_ITEM;
+    const themeAttr = attributes.THEME_NAME;
+    const menuActionId = ids.THEME_COLOR_MENU_ACTION;
+
+    const unwrappedSafeNewTheme = unwrapSafeDemoTheme(safeNewTheme);
+    const oldSelectedItems = dom.getAll(`.${selectedMenuItemClass}[${themeAttr}]`, this.themeMenuEl_);
+    const newSelectedItem =
+      this.themeMenuEl_.querySelector(`.${mdcListItemClass}[${themeAttr}="${unwrappedSafeNewTheme}"]`);
+
+    oldSelectedItems.forEach((colorIcon) => colorIcon.classList.remove(selectedMenuItemClass));
+    newSelectedItem.classList.add(selectedMenuItemClass);
+
+    this.getElementById_(menuActionId).setAttribute(themeAttr, unwrappedSafeNewTheme);
+  }
+}
+
+demoReady(() => {
+  const root = document;
+
+  Permalinker.attachTo(root);
+  ThemePicker.attachTo(root, ToolbarProvider.attachTo(root));
+});
