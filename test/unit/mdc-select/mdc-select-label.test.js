9500d8d6ad963bf6c4afa170951d3b666d180a4a
Matt Goo <mattgoo@google.com>
2018-02-21 14:27:28 -0800

    fix(select): add tests for select label package

---
+++ test/unit/mdc-select/mdc-select-label.test.js
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {assert} from 'chai';
+import bel from 'bel';
+import domEvents from 'dom-events';
+import td from 'testdouble';
+
+import {MDCSelectLabel} from '../../../packages/mdc-select/label';
+import {strings} from '../../../packages/mdc-select/constants';
+
+function getFixture() {
+  return bel`
+    <div class="mdc-select__label">Label</div>
+  `;
+}
+
+function setupTest() {
+  const fixture = getFixture();
+  const component = new MDCSelectLabel(fixture);
+
+  return {component, fixture};
+}
+
+suite('MDCSelectLabel');
+
+test('attachTo returns a component instance', () => {
+  assert.isOk(MDCSelectLabel.attachTo(getFixture()) instanceof MDCSelectLabel);
+});
+
+test('#float should call styleFloat on foundation', () => {
+  const {component} = setupTest();
+  component.foundation_.styleFloat = td.function();
+  component.float();
+  td.verify(component.foundation_.styleFloat(td.matchers.anything()));
+});
+
+test('adapter#addClass adds a class to the root element', () => {
+  const {component, fixture} = setupTest();
+
+  component.getDefaultFoundation().adapter_.addClass('foo');
+  assert.isTrue(fixture.classList.contains('foo'));
+});
+
+
+test('adapter#removeClass adds a class to the root element', () => {
+  const {component, fixture} = setupTest();
+
+  fixture.classList.add('foo');
+  component.getDefaultFoundation().adapter_.removeClass('foo');
+  assert.isFalse(fixture.classList.contains('foo'));
+});
