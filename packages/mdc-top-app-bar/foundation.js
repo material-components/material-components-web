2d48ea50db80e4df0e98850e1e80c9b3bb307222
Will Ernest <34519388+williamernest@users.noreply.github.com>
2018-02-23 15:16:28 -0800

    chore(top-app-bar): Update copyright notices. (#2300)

--- packages/mdc-top-app-bar/foundation.js
+++ packages/mdc-top-app-bar/foundation.js
@@ -1,4 +1,5 @@
 /**
+ * @license
  * Copyright 2018 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import {strings} from './constants';
 
 import MDCTopAppBarAdapter from './adapter';
