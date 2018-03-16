c4f46ff3bd9d1a16eefb88542bdf0df5c2c92d15
Kenneth G. Franqueiro <kfranqueiro@google.com>
2018-03-16 11:33:37 -0400

    fix(text-field): Remove press ripple effect

--- test/unit/mdc-textfield/mdc-text-field.test.js
+++ test/unit/mdc-textfield/mdc-text-field.test.js
@@ -94,21 +94,6 @@ test('#constructor when given a `mdc-text-field--box` element instantiates a rip
   assert.equal(component.ripple.root, root);
 });
 
-test('#constructor when given a `mdc-text-field--outlined` element instantiates a ripple on the ' +
-     'outline element', () => {
-  const root = bel`
-    <div class="mdc-text-field mdc-text-field--outlined">
-      <input type="text" class="mdc-text-field__input" id="my-text-field">
-      <label class="mdc-floating-label" for="my-text-field">My Label</label>
-      <div class="mdc-notched-outline"></div>
-      <div class="mdc-notched-outline__idle"></div>
-    </div>
-  `;
-  const outline = root.querySelector('.mdc-notched-outline');
-  const component = new MDCTextField(root, undefined, (el) => new FakeRipple(el));
-  assert.equal(component.ripple.root, outline);
-});
-
 test('#constructor sets the ripple property to `null` when not given a `mdc-text-field--box` nor ' +
      'a `mdc-text-field--outlined` subelement', () => {
   const component = new MDCTextField(getFixture());
@@ -119,20 +104,6 @@ test('#constructor when given a `mdc-text-field--box` element, initializes a def
      'ripple factory given', () => {
   const root = getFixture();
   root.classList.add(cssClasses.BOX);
-  const component = new MDCTextField(root);
-  assert.instanceOf(component.ripple, MDCRipple);
-});
-
-test('#constructor when given a `mdc-text-field--outlined` element, initializes a default ripple when no ' +
-     'ripple factory given', () => {
-  const root = bel`
-    <div class="mdc-text-field mdc-text-field--outlined">
-      <input type="text" class="mdc-text-field__input" id="my-text-field">
-      <label class="mdc-floating-label" for="my-text-field">My Label</label>
-      <div class="mdc-notched-outline"></div>
-      <div class="mdc-notched-outline__idle"></div>
-    </div>
-  `;
   const component = new MDCTextField(root);
   assert.instanceOf(component.ripple, MDCRipple);
 });
