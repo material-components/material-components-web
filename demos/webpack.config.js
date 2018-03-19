material-components/material-components-web

commit 4c7f006dff0484096f70dbcd5bbb06b32c5f4a99

chore: Move demo Webpack config to separate file (#2325)
Fixes #1994

demos/webpack.config.js
@@ -1,0 +1,93 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const CssBundleFactory = require('../scripts/webpack/css-bundle-factory');
+const Environment = require('../scripts/build/environment');
+const Globber = require('../scripts/webpack/globber');
+const JsBundleFactory = require('../scripts/webpack/js-bundle-factory');
+const PathResolver = require('../scripts/build/path-resolver');
+const PluginFactory = require('../scripts/webpack/plugin-factory');
+
+const env = new Environment();
+env.setBabelEnv();
+
+const pathResolver = new PathResolver();
+const globber = new Globber({pathResolver});
+const pluginFactory = new PluginFactory({globber});
+const copyrightBannerPlugin = pluginFactory.createCopyrightBannerPlugin();
+const cssBundleFactory = new CssBundleFactory({env, pathResolver, globber, pluginFactory});
+const jsBundleFactory = new JsBundleFactory({env, pathResolver, globber, pluginFactory});
+
+const DEMO_BASE_DIR_ABSOLUTE_PATH = pathResolver.getAbsolutePath('/demos');
+
+const OUTPUT = {
+  httpDirAbsolutePath: '/assets/',
+};
+
+module.exports = [
+  mainJsCombined(),
+  demoCss(),
+  demoJs(),
+];
+
+// webpack-dev-server requires that these properties be set on the first bundle.
+// It ignores them on all other bundles.
+Object.assign(module.exports[0], {
+  devServer: {
+    contentBase: DEMO_BASE_DIR_ABSOLUTE_PATH,
+
+    // See https://github.com/webpack/webpack-dev-server/issues/882
+    // Because we only spin up dev servers temporarily, and all of our assets are publicly
+    // available on GitHub, we can safely disable this check.
+    disableHostCheck: true,
+  },
+});
+
+function mainJsCombined() {
+  return jsBundleFactory.createMainJsCombined({output: OUTPUT});
+}
+
+function demoJs() {
+  return jsBundleFactory.createCustomJs({
+    bundleName: 'demo-js',
+    chunks: {
+      'common': pathResolver.getAbsolutePath('./demos/common.js'),
+      'theme/index': pathResolver.getAbsolutePath('./demos/theme/index.js'),
+    },
+    output: Object.assign({}, OUTPUT, {
+      filename: '[name].js',
+      library: ['demo', '[name]'],
+    }),
+    plugins: [
+      copyrightBannerPlugin,
+    ],
+  });
+}
+
+function demoCss() {
+  return cssBundleFactory.createCustomCss({
+    bundleName: 'demo-css',
+    chunkGlobConfig: {
+      inputDirectory: '/demos',
+    },
+    output: OUTPUT,
+    plugins: [
+      copyrightBannerPlugin,
+    ],
+  });
+}

webpack.config.js
@@ -29,80 +29,16 @@ env.setBabelEnv();
 const pathResolver = new PathResolver();
 const globber = new Globber({pathResolver});
 const pluginFactory = new PluginFactory({globber});
-const copyrightBannerPlugin = pluginFactory.createCopyrightBannerPlugin();
 const cssBundleFactory = new CssBundleFactory({env, pathResolver, globber, pluginFactory});
 const jsBundleFactory = new JsBundleFactory({env, pathResolver, globber, pluginFactory});
 
 const OUTPUT = {
   fsDirAbsolutePath: pathResolver.getAbsolutePath('./build'),
-  httpDirAbsolutePath: '/assets/',
 };
 
-if (env.isDev()) {
-  module.exports = demo();
-} else {
-  module.exports = dist();
-}
-
-function dist() {
-  return [
-    mainJsCombined(),
-    mainJsALaCarte(),
-    mainCssCombined(),
-    mainCssALaCarte(),
-  ];
-}
-
-function demo() {
-  return [
-    mainJsCombined(),
-    demoCss(),
-    demoJs(),
-  ];
-}
-
-function mainJsCombined() {
-  return jsBundleFactory.createMainJsCombined({output: OUTPUT});
-}
-
-function mainJsALaCarte() {
-  return jsBundleFactory.createMainJsALaCarte({output: OUTPUT});
-}
-
-function mainCssCombined() {
-  return cssBundleFactory.createMainCssCombined({output: OUTPUT});
-}
-
-function mainCssALaCarte() {
-  return cssBundleFactory.createMainCssALaCarte({output: OUTPUT});
-}
-
-function demoJs() {
-  return jsBundleFactory.createCustomJs({
-    bundleName: 'demo-js',
-    chunks: {
-      'common': pathResolver.getAbsolutePath('./demos/common.js'),
-      'theme/index': pathResolver.getAbsolutePath('./demos/theme/index.js'),
-    },
-    output: Object.assign({}, OUTPUT, {
-      filename: '[name].js',
-      library: ['demo', '[name]'],
-    }),
-    plugins: [
-      copyrightBannerPlugin,
-    ],
-  });
-}
-
-function demoCss() {
-  return cssBundleFactory.createCustomCss({
-    bundleName: 'demo-css',
-    chunkGlobConfig: {
-      inputDirectory: '/demos',
-    },
-    output: OUTPUT,
-    plugins: [
-      copyrightBannerPlugin,
-    ],
-  });
-}
+module.exports = [
+  jsBundleFactory.createMainJsCombined({output: OUTPUT}),
+  jsBundleFactory.createMainJsALaCarte({output: OUTPUT}),
+  cssBundleFactory.createMainCssCombined({output: OUTPUT}),
+  cssBundleFactory.createMainCssALaCarte({output: OUTPUT}),
+];

scripts/webpack/css-bundle-factory.js
@@ -55,13 +55,22 @@ module.exports = class {
         filePathPattern = '**/*.scss',
       } = {},
       output: {
-        fsDirAbsolutePath,
-        httpDirAbsolutePath,
+        fsDirAbsolutePath = undefined, // Required for building the npm distribution and writing output files to disk
+        httpDirAbsolutePath = undefined, // Required for running the demo server
         filenamePattern = this.env_.isProd() ? '[name].min.css' : '[name].css',
       },
       plugins = [],
     }) {
     chunks = chunks || this.globber_.getChunks({inputDirectory, filePathPattern});
+
+    const fsCleanupPlugins = [];
+
+    if (fsDirAbsolutePath) {
+      fsCleanupPlugins.push(this.pluginFactory_.createCssCleanupPlugin({
+        cleanupDirRelativePath: fsDirAbsolutePath,
+      }));
+    }
+
     const cssExtractorPlugin = this.pluginFactory_.createCssExtractorPlugin(filenamePattern);
 
     return {
@@ -81,9 +90,7 @@ module.exports = class {
       },
       plugins: [
         cssExtractorPlugin,
-        this.pluginFactory_.createCssCleanupPlugin({
-          cleanupDirRelativePath: fsDirAbsolutePath,
-        }),
+        ...fsCleanupPlugins,
         ...plugins,
       ],
     };

scripts/webpack/js-bundle-factory.js
@@ -46,11 +46,11 @@ class JsBundleFactory {
       chunks,
       chunkGlobConfig: {
         inputDirectory = null,
-        filePathPattern = null,
+        filePathPattern = '**/*.js',
       } = {},
       output: {
-        fsDirAbsolutePath,
-        httpDirAbsolutePath,
+        fsDirAbsolutePath = undefined, // Required for building the npm distribution and writing output files to disk
+        httpDirAbsolutePath = undefined, // Required for running the demo server
         filenamePattern = this.env_.isProd() ? '[name].min.js' : '[name].js',
         library,
       },
@@ -67,12 +67,6 @@ class JsBundleFactory {
         filename: filenamePattern,
         libraryTarget: 'umd',
         library,
-      },
-      // See https://github.com/webpack/webpack-dev-server/issues/882
-      // Because we only spin up dev servers temporarily, and all of our assets are publicly
-      // available on GitHub, we can safely disable this check.
-      devServer: {
-        disableHostCheck: true,
       },
       devtool: 'source-map',
       module: {

test/build/webpack-module-exports.test.js
@@ -51,7 +51,7 @@ describe('webpack.config.js', () => {
   describe('MDC_ENV="development"', () => {
     it('module exports should match build-config-dev-env.golden.json', () => {
       const {generatedWebpackConfig, expectedWebpackConfig} = webpackConfigLoader.setupTest({
-        configPath: path.join(__dirname, '../../webpack.config.js'),
+        configPath: path.join(__dirname, '../../demos/webpack.config.js'),
         goldenPath: path.join(__dirname, './goldens/build-config-dev-env.golden.json'),
         mdcEnv: 'development',
       });

commit 21150c7a1c78a3c1be8f340acaaba44e11f4c5cf

fix: Compile demo CSS/JS during `npm run build` (#2437)
Fixes a regression introduced by #2325.

Supersedes #2433.

To verify:

1. Build and deploy to staging server:
    ```bash
    MDC_ENV=development npm run build && gcloud app deploy app.yaml --project google.com:mdc-web-dev --version $USER
    ```
2. View the staging server:
    ```
    http://$USER.material-components-web.appspot.com
    ```

demos/webpack.config.js
@@ -36,6 +36,7 @@ const jsBundleFactory = new JsBundleFactory({env, pathResolver, globber, pluginF
 const DEMO_BASE_DIR_ABSOLUTE_PATH = pathResolver.getAbsolutePath('/demos');
 
 const OUTPUT = {
+  fsDirAbsolutePath: pathResolver.getAbsolutePath('/build'),
   httpDirAbsolutePath: '/assets/',
 };
 