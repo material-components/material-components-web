5fa0260c06d17e44ae2df24b055996f7af4ee033
Patrick RoDee <prodee@google.com>
2018-03-20 15:36:53 -0700

    WIP starting

---
+++ packages/mdc-tab-indicator/foundation.js
@@ -0,0 +1,96 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* eslint no-unused-vars: [2, {"args": "none"}] */
+
+import MDCFoundation from '@material/base/foundation';
+import MDCTabIndicatorAdapter from './adapter';
+import {cssClasses} from './constants';
+
+/* eslint-disable no-unused-vars */
+import MDCTabDimensions from '@material/tab/dimensions';
+/* eslint-enable no-unused-vars */
+
+/**
+ * @extends {MDCFoundation<!MDCTabIndicatorAdapter>}
+ * @abstract
+ */
+class MDCTabIndicatorFoundation extends MDCFoundation {
+  /** @return enum {string} */
+  static get cssClasses() {
+    return cssClasses;
+  }
+
+  /**
+   * @see MDCTabIndicatorAdapter for typing information
+   * @return {!MDCTabIndicatorAdapter}
+   */
+  static get defaultAdapter() {
+    return /** @type {!MDCTabIndicatorAdapter} */ ({
+      registerEventHandler: () => {},
+      deregisterEventHandler: () => {},
+      addClass: () => {},
+      removeClass: () => {},
+      hasClass: () => {},
+      setRootStyleProperty: () => {},
+    });
+  }
+
+  /** @param {!MDCTabIndicatorAdapter} adapter */
+  constructor(adapter) {
+    super(Object.assign(MDCTabIndicatorAdapter.defaultAdapter, adapter));
+
+    /** @private {function(): undefined} */
+    this.handleTransitionEnd_ = () => this.handleTransitionEnd();
+  }
+
+  /**
+   * Handles the "transitionend" event
+   */
+  handleTransitionEnd() {
+    this.adapter_.deregisterEventHandler('transitionend', this.handleTransitionEnd_);
+    this.adapter_.removeClass(cssClasses.ANIMATING);
+  }
+
+  /**
+   * Returns whether the indicator should match the width of the tab content
+   * @return {boolean}
+   */
+  shouldMatchTextLabelWidth() {
+    return this.adapter_.hasClass(cssClasses.MATCH_TEXT_LABEL);
+  }
+
+
+  /**
+   * Animates the indicator's position
+   * @param {!MDCTabDimensions} tabDimensions
+   */
+  animatePosition(tabDimensions) {
+    this.adapter_.registerEventHandler('transitionend', this.handleTransitionEnd_);
+    this.adapter_.addClass(cssClasses.ANIMATING);
+    this.layout(tabDimensions);
+  }
+
+  /**
+   * Calculates the indicator size and applies it
+   * @param {!MDCTabDimensions} tabDimensions
+   * @abstract
+   */
+  layout(tabDimensions) {}
+}
+
+export default MDCTabIndicatorFoundation;
---
+++ packages/mdc-tab-indicator/index.js
@@ -0,0 +1,75 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import MDCComponent from '@material/base/component';
+
+import {cssClasses} from './constants';
+import MDCTabIndicatorAdapter from './adapter';
+import MDCTabWidthIndicatorFoundation from './tab-width-foundation';
+import MDCTextLabelWidthIndicatorFoundation from './text-label-width-foundation';
+
+/* eslint-disable no-unused-vars */
+import MDCTabDimensions from '@material/tab/dimensions';
+import MDCTabIndicatorFoundation from './foundation';
+/* eslint-enable no-unused-vars */
+
+/**
+ * @extends {MDCComponent<!MDCTabIndicatorFoundation>}
+ * @final
+ */
+class MDCTabIndicator extends MDCComponent {
+  /**
+   * @param {!Element} root
+   * @return {!MDCTabIndicator}
+   */
+  static attachTo(root) {
+    return new MDCTabIndicator(root);
+  }
+
+  /**
+   * @return {!MDCTabIndicatorFoundation}
+   */
+  getDefaultFoundation() {
+    const defaultAdapter = /** @type {!MDCTabIndicatorAdapter} */ (Object.assign({
+      registerEventHandler: (evtType, handler) => this.root_.addEventListener(evtType, handler),
+      deregisterEventHandler: (evtType, handler) => this.root_.removeEventListener(evtType, handler),
+      addClass: (className) => this.root_.classList.add(className),
+      removeClass: (className) => this.root_.classList.remove(className),
+      hasClass: (className) => this.root_.classList.contains(className),
+      setRootStyleProperty: (attr, value) => this.root_.style[attr] = value,
+    }));
+
+    if (defaultAdapter.hasClass(cssClasses.MATCH_TEXT_LABEL)) {
+      return new MDCTextLabelWidthIndicatorFoundation(defaultAdapter);
+    } else {
+      return new MDCTabWidthIndicatorFoundation(defaultAdapter);
+    }
+  }
+
+  /** @param {!MDCTabDimensions} tabDimensions */
+  animate(tabDimensions) {
+    this.foundation_.animatePosition(tabDimensions);
+  }
+
+  /** @param {!MDCTabDimensions} tabDimensions */
+  layout(tabDimensions) {
+    this.foundation_.layout(tabDimensions);
+  }
+}
+
+export {MDCTabIndicator, MDCTabWidthIndicatorFoundation, MDCTextLabelWidthIndicatorFoundation};
