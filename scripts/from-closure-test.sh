2494c94cd4df578cddf51cc12e37ea45cdf6c34f
Andrey Prigogin <aprigogin@google.com>
2018-03-29 08:03:35 -0700

    feat(slider): Single identifier exports rewriting exportDefaultDeclarat

---
+++ scripts/from-closure-test.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+##
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+set -e
+
+function log() {
+  echo -e "\033[36m[from-closure-test]\033[0m" "$@"
+}
+
+CLOSURE_TMP=.closure-tmp
+FROM_CLOSURE_TMP=.from-closure-tmp
+FROM_CLOSURE_PKGDIR=$FROM_CLOSURE_TMP/packages
+CLOSURIZED_PKGS=$(node -e "console.log(require('./package.json').closureWhitelist.join(' '))")
+
+if [ -z "$CLOSURIZED_PKGS" ]; then
+  echo "No closurized packages to test!"
+  exit 0
+fi
+
+log "Prepping whitelisted packages for JS compilation/rewrite"
+
+rm -fr $FROM_CLOSURE_TMP/**
+mkdir -p $FROM_CLOSURE_PKGDIR
+for pkg in $CLOSURIZED_PKGS; do
+  cp -r "$CLOSURE_TMP/packages/$pkg" $FROM_CLOSURE_PKGDIR
+done
+rm -fr $FROM_CLOSURE_PKGDIR/**/{node_modules,dist}
+
+log "Rewriting all goog.require statements to be imports" "$FROM_CLOSURE_PKGDIR"
+node scripts/rewrite-decl-statements-from-closure-test.js $FROM_CLOSURE_PKGDIR
+
+log "TODO: Need to npm test packages"
+echo ''
+
+set +e
+# for pkg in $CLOSURIZED_PKGS; do
+#   entry_point="goog:mdc.${pkg/mdc-/}"
+#   entry_point=${entry_point//-/}
+#   # Note that the jscomp_error flags turn all default warnings into errors, so that
+#   # closure exits with a non-zero status if any of them are caught.
+#   # Also note that we disable accessControls checks due to
+#   # https://github.com/google/closure-compiler/issues/2261
+#   CMD="java -jar node_modules/google-closure-compiler/compiler.jar \
+#   --externs closure_externs.js \
+#   --compilation_level ADVANCED \
+#   --js $(find $FROM_CLOSURE_PKGDIR -type f -name "*.js") \
+#   --language_out ECMASCRIPT5_STRICT \
+#   --dependency_mode STRICT \
+#   --module_resolution LEGACY \
+#   --js_module_root $FROM_CLOSURE_PKGDIR \
+#   --entry_point $entry_point \
+#   --checks_only \
+#   --jscomp_error checkTypes \
+#   --jscomp_error conformanceViolations \
+#   --jscomp_error functionParams \
+#   --jscomp_error globalThis \
+#   --jscomp_error invalidCasts \
+#   --jscomp_error misplacedTypeAnnotation \
+#   --jscomp_error nonStandardJsDocs \
+#   --jscomp_error suspiciousCode \
+#   --jscomp_error uselessCode \
+#   --jscomp_off accessControls
+#   "
+#   $CMD
+#
+#   if [ $? -eq 0 ]; then
+#     echo -e "\033[32;1mPASS\033[0m" "$pkg"
+#   else
+#     # Add extra space after compiler output
+#     echo ''
+#     echo -e "\033[31;1mFAIL\033[0m" "$pkg"
+#     echo "Failed with the following command:"
+#     echo $CMD
+#     exit 1
+#   fi
+# done
+set -e
+
+echo ''
+echo '✨  All tests pass! ✨'
---
+++ scripts/rewrite-decl-statements-from-closure-test.js
@@ -0,0 +1,234 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Rewrites JS to match a goog.module format. That means
+ *  * Add goog.module to the top of the source code
+ *  * Rewrite export {foo, bar} to exports = {foo, bar}
+ *  * Rewrite export default Foo, to exports = Foo
+ *  * Rewrite import Foo from './foo' to const Foo = goog.require('mdc.foo')
+ *  * Rewrite import {foo, bar} from './util' to const {foo, bar} = goog.require('mdc.util')
+ *
+ *
+ * This script rewrites import statements such that:
+ *
+ * ```js
+ * import [<SPECIFIERS> from] '@material/$PKG[/files...]';
+ * ```
+ * becomes
+ * ```js
+ * const [<SPECIFIERS>] = goog.require('mdc.$PKG.<RESOLVED_FILE_NAMESPACE>');
+ * ```
+ * The RESOLVED_FILE_NAMESPACE is a namespace matching the directory structure.
+ *
+ * This script also handles third-party dependencies, e.g.
+ *
+ * ```js
+ * import {thing1, thing2} from 'third-party-lib';
+ * ```
+ *
+ * becomes
+ *
+ * ```js
+ * const {thing1, thing2} = goog.require('goog:mdc.thirdparty.thirdPartyLib');
+ * ```
+ *
+ * and
+ *
+ * ```js
+ * import someDefaultExport from 'third-party-lib';
+ * ```
+ *
+ * becomes
+ *
+ * ```js
+ * const {someDefaultExport} = goog.require('goog:mdc.thirdparty.thirdPartyLib')
+ * ```
+ *
+ * This is so closure is able to properly build and check our files without breaking when it encounters
+ * node modules. Note that while closure does have a --module_resolution NODE flag
+ * (https://github.com/google/closure-compiler/wiki/JS-Modules#node-resolution-mode), it has inherent problems
+ * that prevents us from using it. See:
+ * - https://github.com/google/closure-compiler/issues/2386
+ *
+ * Note that for third-party modules, they must be defined in closure_externs.js. See that file for more info.
+ * Also note that this works on `export .... from ...` as well.
+ */
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const {default: traverse} = require('babel-traverse');
+const babylon = require('babylon');
+const camelCase = require('camel-case');
+const glob = require('glob');
+const recast = require('recast');
+const resolve = require('resolve');
+const t = require('babel-types');
+
+main(process.argv);
+
+function main(argv) {
+  if (argv.length < 3) {
+    console.error('Missing root directory path');
+    process.exit(1);
+  }
+
+  const rootDir = path.resolve(process.argv[2]);
+  const srcFiles = glob.sync(`${rootDir}/**/*.js`);
+  srcFiles.forEach((srcFile) => transform(srcFile, rootDir));
+}
+
+function transform(srcFile, rootDir) {
+  const src = fs.readFileSync(srcFile, 'utf8');
+  const ast = recast.parse(src, {
+    parser: {
+      parse: (code) => babylon.parse(code, {sourceType: 'script'}),
+    },
+  });
+
+  // traverse(ast, {
+  //   ExportNamedDeclaration(path) {
+  //     const properties = [];
+  //     path.node.specifiers.forEach((specifier) => {
+  //       properties.push(t.objectProperty(specifier.exported, specifier.exported, false, true, []));
+  //     });
+  //     const right = t.objectExpression(properties);
+  //     const expression = t.assignmentExpression('=', t.identifier('exports'), right);
+  //     path.replaceWith(t.expressionStatement(expression));
+  //   },
+  // });
+  //
+  traverse(ast, {
+    ExpressionStatement(path) {
+      //const expression = t.assignmentExpression('=', t.identifier('exports'), path.node.declaration);
+      //path.replaceWith(t.expressionStatement(expression));
+      if (path.node.expression.operator === "=" && path.node.expression.left.type == "Identifier" && path.node.expression.left.name == "exports") {
+        console.log(path.node.expression);
+
+        console.log("exports = " + path.node.expression.right.name);
+        console.log(path.node.expression);
+        console.log('^^^^AAAAA^^^^');
+        if (path.node.expression.right.type == "Identifier") {
+          // export default {right.name}
+          const declaration = t.exportDefaultDeclaration(t.identifier(path.node.expression.right.name));
+          console.log('++replacing:');
+          console.log(declaration);
+          path.replaceWith(declaration);
+        } else {
+          console.log(path.node.expression.right.properties);
+          console.log('___OBJECT___');
+        }
+      }
+      //console.log(path)
+    },
+  });
+
+  // traverse(ast, {
+  //   ImportDeclaration(path) {
+  //     const callee = t.memberExpression(t.identifier('goog'), t.identifier('require'), false);
+  //     const packageStr = rewriteDeclarationSource(path.node, srcFile, rootDir);
+  //     const callExpression = t.callExpression(callee, [t.stringLiteral(packageStr)]);
+  //
+  //     let variableDeclaratorId;
+  //     if (path.node.specifiers.length > 1 || (src.substr(path.node.specifiers[0].start - 1, 1) === '{')) {
+  //       const properties = [];
+  //       path.node.specifiers.forEach((specifier) => {
+  //         properties.push(t.objectProperty(specifier.local, specifier.local, false, true, []));
+  //       });
+  //       variableDeclaratorId = t.objectPattern(properties);
+  //     } else {
+  //       variableDeclaratorId = path.node.specifiers[0].local;
+  //     }
+  //
+  //     const variableDeclarator = t.variableDeclarator(variableDeclaratorId, callExpression);
+  //     const variableDeclaration = t.variableDeclaration('const', [variableDeclarator]);
+  //     // Preserve comments above import statements, since this is most likely the license comment.
+  //     if (path.node.comments && path.node.comments.length > 0) {
+  //       const commentValue = path.node.comments[0].value;
+  //       variableDeclaration.comments = variableDeclaration.comments || [];
+  //       variableDeclaration.comments.push({type: 'CommentBlock', value: commentValue});
+  //     }
+  //     path.replaceWith(variableDeclaration);
+  //   },
+  // });
+
+  let {code: outputCode} = recast.print(ast, {
+    objectCurlySpacing: false,
+    quote: 'single',
+    trailingComma: {
+      objects: true,
+      arrays: true,
+      parameters: false,
+    },
+  });
+
+  const relativePath = path.relative(rootDir, srcFile);
+  const packageParts = relativePath.replace('mdc-', '').replace(/-/g, '').replace('.js', '').split('/');
+  const packageStr = 'mdc.' + packageParts.join('.').replace('.index', '');
+
+  console.log("DEBUG\n" + relativePath + "\n" + packageParts + "\n" + packageStr)
+  //outputCode = 'goog.module(\'' + packageStr + '\');\n' + outputCode;
+  fs.writeFileSync(srcFile, outputCode, 'utf8');
+  console.log(`[rewrote] ${srcFile}`);
+}
+
+function rewriteDeclarationSource(node, srcFile, rootDir) {
+  let source = node.source.value;
+  const pathParts = source.split('/');
+  const isMDCImport = pathParts[0] === '@material';
+  if (isMDCImport) {
+    const modName = pathParts[1]; // @material/<modName>
+    const atMaterialReplacementPath = `${rootDir}/mdc-${modName}`;
+    const rewrittenSource = [atMaterialReplacementPath].concat(pathParts.slice(2)).join('/');
+    source = rewrittenSource;
+  }
+
+  return patchNodeForDeclarationSource(source, srcFile, rootDir, node);
+}
+
+function patchNodeForDeclarationSource(source, srcFile, rootDir, node) {
+  let resolvedSource = source;
+  // See: https://nodejs.org/api/modules.html#modules_all_together (step 3)
+  const wouldLoadAsFileOrDir = ['./', '/', '../'].some((s) => source.indexOf(s) === 0);
+  const isThirdPartyModule = !wouldLoadAsFileOrDir;
+  if (isThirdPartyModule) {
+    assert(source.indexOf('@material') < 0, '@material/* import sources should have already been rewritten');
+    patchDefaultImportIfNeeded(node);
+    resolvedSource = `goog:mdc.thirdparty.${camelCase(source)}`;
+  } else {
+    const normPath = path.normalize(path.dirname(srcFile), source);
+    const needsClosureModuleRootResolution = path.isAbsolute(source) || fs.statSync(normPath).isDirectory();
+    if (needsClosureModuleRootResolution) {
+      resolvedSource = path.relative(rootDir, resolve.sync(source, {
+        basedir: path.dirname(srcFile),
+      }));
+    }
+  }
+  const packageParts = resolvedSource.replace('mdc-', '').replace(/-/g, '').replace('.js', '').split('/');
+  const packageStr = 'mdc.' + packageParts.join('.').replace('.index', '');
+  return packageStr;
+}
+
+function patchDefaultImportIfNeeded(node) {
+  const defaultImportSpecifierIndex = node.specifiers.findIndex(t.isImportDefaultSpecifier);
+  if (defaultImportSpecifierIndex >= 0) {
+    const defaultImportSpecifier = node.specifiers[defaultImportSpecifierIndex];
+    const defaultPropImportSpecifier = t.importSpecifier(defaultImportSpecifier.local, t.identifier('default'));
+    node.specifiers[defaultImportSpecifierIndex] = defaultPropImportSpecifier;
+  }
+}
