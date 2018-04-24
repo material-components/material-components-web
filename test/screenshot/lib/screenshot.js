dee0b6088b7e19c7fbbb2a8de74d9546a51fdc28
Andrew C. Dvorak <acdvorak@gmail.com>
2018-04-24 13:51:42 -0700

    feat(infrastructure): Capture screenshots with CrossBrowserTesting.com (#2621)
    ### How to test
    
    ```bash
    export CBT_USERNAME='...'
    export CBT_AUTHKEY='...'
    export MDC_GCLOUD_SERVICE_ACCOUNT_KEY_FILE_PATH='...'
    
    npm run screenshot:build
    npm run screenshot:test
    ```
    
    ### What it does
    
    The above commands will:
    
    1. Upload compiled assets (HTML/CSS/JS) to Google Cloud Storage
    2. Capture screenshots of all HTML pages in Chrome and Edge
    3. Print out the public CBT screenshot URLs
    
    The entire process takes about 2 minutes on my MacBook.
    
    ### What it DOESN'T do
    
    This PR does not:
    
    1. Upload screenshot images to GCS
    2. Diff the images
    3. Store the results in a golden.json file
    
    Those things will be done in future PRs.
    
    ### Example output
    
    ```
    DONE uploading HTML/CSS/JS assets to Google Cloud Storage!
    
    
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/classes/baseline.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/classes/dense.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/container-fill-color.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/corner-radius.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/filled-accessible.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/horizontal-padding-baseline.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/horizontal-padding-dense.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/icon-color.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/ink-color.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/stroke-color.html
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/mixins/stroke-width.html
    
    
    22 of 22 screenshots finished (100% complete)
    
    
    SUCCESS!
    
    
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/classes/baseline.html
    
      - desktop > Windows 10 > Microsoft Edge 16:
          https://s3.amazonaws.com/media.crossbrowsertesting.com/users/389032/screenshots/chromeless/zadb2f33cb5c2060a52e.png
    
      - desktop > Windows 10 > Google Chrome 63 (64-bit):
          https://s3.amazonaws.com/media.crossbrowsertesting.com/users/389032/screenshots/chromeless/z8afa9b67820c90c12ed.png
    
    https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/04/20/23_53_21_569/6058daf77/assets/mdc-button/classes/dense.html
    
      - desktop > Windows 10 > Microsoft Edge 16:
          https://s3.amazonaws.com/media.crossbrowsertesting.com/users/389032/screenshots/chromeless/z26ecfae99d36cda09bf.png
    
      - desktop > Windows 10 > Google Chrome 63 (64-bit):
          https://s3.amazonaws.com/media.crossbrowsertesting.com/users/389032/screenshots/chromeless/z37a9992b7b3d567acc5.png
    ```

---
+++ test/screenshot/lib/screenshot.js
@@ -0,0 +1,192 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const request = require('request-promise-native');
+const Progress = require('./progress');
+
+const API_BASE_URL = 'https://crossbrowsertesting.com/api/v3';
+const API_USERNAME = process.env.CBT_USERNAME;
+const API_AUTHKEY = process.env.CBT_AUTHKEY;
+
+/** How long to wait between polling the API for status changes. */
+const API_POLL_INTERVAL_MS = 1000 * 5;
+
+/** How long to wait for a single URL to be captured in all browsers. */
+const API_MAX_WAIT_MS = 1000 * 60;
+
+/**
+ * List of browsers to use.
+ *
+ * API documentation for the name format can be found at:
+ * https://crossbrowsertesting.com/apidocs/v3/screenshots.html#!/default/post_screenshots
+ *
+ * TODO(acdvorak): Use CBT's API that returns the full list of browser names available:
+ * https://crossbrowsertesting.com/apidocs/v3/screenshots.html#!/default/get_screenshots_browsers
+ *
+ * @type {!Array<string>}
+ */
+const BROWSERS = [
+  'Chrome63x64',
+  'Edge16',
+];
+
+/** Map of URLs to `Progress` objects. */
+const progressMap = new Map();
+
+module.exports = {
+  capture,
+};
+
+async function capture(testPageUrls) {
+  return Promise.all(testPageUrls.map(captureOneUrl));
+}
+
+async function captureOneUrl(testPageUrl) {
+  logTestCaseProgress(testPageUrl, Progress.enqueued(BROWSERS.length));
+
+  return sendCaptureRequest(testPageUrl)
+    .then(
+      async (captureResponseBody) => handleCaptureResponse(testPageUrl, captureResponseBody),
+      async (err) => rejectWithError('captureOne', testPageUrl, err)
+    );
+}
+
+async function sendCaptureRequest(testPageUrl) {
+  const options = {
+    method: 'POST',
+    uri: apiUrl('/screenshots/'),
+    auth: {
+      username: API_USERNAME,
+      password: API_AUTHKEY,
+    },
+    body: {
+      url: testPageUrl,
+      browsers: BROWSERS.join(','),
+    },
+    json: true, // Automatically stringify the request body and parse the response body as JSON
+  };
+
+  return request(options)
+    .catch(async (err) => {
+      if (reachedParallelExecutionLimit(err)) {
+        // Wait a few seconds, then try again.
+        await sleep(API_POLL_INTERVAL_MS);
+        return sendCaptureRequest(testPageUrl);
+      }
+
+      return rejectWithError('sendCaptureRequest', testPageUrl, err);
+    });
+}
+
+async function handleCaptureResponse(testPageUrl, captureResponseBody) {
+  let infoResponseBody;
+  let infoProgress = computeTestCaseProgress(testPageUrl, captureResponseBody);
+
+  const startTime = Date.now();
+  const isStillRunning = () => infoProgress.running > 0;
+  const isTimedOut = () => (Date.now() - startTime) > API_MAX_WAIT_MS;
+
+  while (isStillRunning() && !isTimedOut()) {
+    // Wait a few seconds, then try again.
+    await sleep(API_POLL_INTERVAL_MS);
+    infoResponseBody = await fetchScreenshotInfo(captureResponseBody.screenshot_test_id);
+    infoProgress = computeTestCaseProgress(testPageUrl, infoResponseBody);
+    logTestCaseProgress(testPageUrl, infoProgress);
+  }
+
+  if (isStillRunning() && isTimedOut()) {
+    const err = new Error([
+      `Timed out after ${Date.now() - startTime} ms waiting for CBT`,
+      `to finish capturing a screenshot of "${testPageUrl}".`,
+    ].join('\n'));
+    return rejectWithError('handleCaptureResponse', testPageUrl, err);
+  }
+
+  if (infoResponseBody.statusCode === 524) {
+    const err = new Error([
+      `Timed out after ${Date.now() - startTime} ms waiting for CBT`,
+      `to finish capturing a screenshot of "${testPageUrl}".`,
+      'See https://support.cloudflare.com/hc/en-us/articles/200171926-Error-524',
+    ].join('\n'));
+    return rejectWithError('handleCaptureResponse', testPageUrl, err);
+  }
+
+  logTestCaseProgress(testPageUrl, infoProgress);
+
+  return Promise.resolve(infoResponseBody);
+}
+
+async function fetchScreenshotInfo(screenshotTestId) {
+  return request({
+    method: 'GET',
+    uri: apiUrl(`/screenshots/${screenshotTestId}`),
+    auth: {
+      username: API_USERNAME,
+      password: API_AUTHKEY,
+    },
+    json: true, // Automatically stringify the request body and parse the response body as JSON
+  });
+}
+
+function computeTestCaseProgress(testPageUrl, screenshotInfoResponseBody) {
+  const progress = Progress.none();
+  return screenshotInfoResponseBody.versions
+    .map((version) => new Progress(version.result_count)) // `result_count` has the same properties as `Progress`
+    .reduce((total, current) => progress.plus(current));
+}
+
+function computeTestSuiteProgress() {
+  return Array.from(progressMap.values()).reduce((total, current) => total.plus(current));
+}
+
+function reachedParallelExecutionLimit(requestError) {
+  try {
+    // The try/catch is necessary because some of these properties might not exist.
+    return requestError.response.body.message.indexOf('maximum number of parallel');
+  } catch (e) {
+    return false;
+  }
+}
+
+function rejectWithError(functionName, testPageUrl, err) {
+  console.error(`ERROR in ${functionName}("${testPageUrl}"):`);
+  console.error('');
+  console.error(err);
+  return Promise.reject(err);
+}
+
+function apiUrl(apiEndpointPath) {
+  return `${API_BASE_URL}${apiEndpointPath}`;
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), ms);
+  });
+}
+
+function logTestCaseProgress(testPageUrl, testPageProgress) {
+  progressMap.set(testPageUrl, testPageProgress);
+
+  const aggregateProgress = computeTestSuiteProgress();
+  const finished = aggregateProgress.finished;
+  const total = aggregateProgress.total;
+  const pct = Math.floor(aggregateProgress.percent);
+
+  process.stdout.write(`\r${finished} of ${total} screenshots finished (${pct}% complete)`);
+}
