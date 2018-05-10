134dbe542bf7abeaa4ea38711c84823ab44a2a58
Andrew C. Dvorak <acdvorak@gmail.com>
2018-05-09 20:28:50 -0700

    feat(infrastructure): More reliable auto-cropping of screenshot images (#2719)
    ### How to test
    
    ```bash
    export CBT_USERNAME='...'
    export CBT_AUTHKEY='...'
    export MDC_GCLOUD_SERVICE_ACCOUNT_KEY_FILE_PATH='...'
    
    npm run screenshot:build
    npm run screenshot:test
    ```
    
    ### What it does
    
    * Changes the layout of test pages
        - There are now only 2 columns of buttons
        - This makes it easier to compare desktop vs. mobile rendering
    * Gives each "cell" a specific height/width
        - If a button's size or margin changes, it will no longer affect the positioning of other buttons
        - This makes diffs less noisy and easier to review
    * Adds a background color of `#333333` to the `<html>` element
        - The `<main>` element still has a background of `#ffffff`
        - This makes it easier to automatically crop the image
    * Replaces [Jimp's unreliable `autocrop()`](https://github.com/oliver-moran/jimp#basic-methods) with custom auto-cropping logic
        - The top and bottom are cropped first, followed by left and right
        - Starting from the outer edges of the image, rows and columns in which at least 95% of the pixels are the page background color (`#333333`) get removed
        - The right side is cropped more loosely (67% instead of 95%) to remove [Edge's promotional notification popovers](https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/05/08/20_40_45_142/c6cc25f87/mdc-button/classes/baseline.html.win10e17_edge17.png)
    * Changes the directory structure and URL format of files stored in GCS
        - Instead of two separate directories for `assets` (HTML/CSS/JS) and `screenshots` (PNGs), captured images have the same path as the HTML file they were captured from, but with a suffix appended
        - Before:
            * [`.../<commit>/assets/mdc-button/classes/baseline.html`](https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/05/07/22_52_10_643/1de2aaf96/assets/mdc-button/classes/baseline.html)
            * [`.../<commit>/screenshots/mdc-button/classes/baseline.html/win10e15_chrome66x64.png`](https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/05/07/22_52_10_643/1de2aaf96/screenshots/mdc-button/classes/baseline.html/win10e15_chrome66x64.png)
        - After:
            * [`.../<commit>/mdc-button/classes/baseline.html`](https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/05/08/23_53_42_405/72d9999e9/mdc-button/classes/baseline.html)
            * [`.../<commit>/mdc-button/classes/baseline.html.win10e15_chrome66x64.png`](https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/05/08/23_53_42_405/72d9999e9/mdc-button/classes/baseline.html.win10e15_chrome66x64.png)
        - This makes it easier to view the page that a screenshot was generated from (you simply remove everything after `.html` from the URL)
    * Reorders the buttons with font icons and SVG icons
        - This makes it easier to compare the positioning of font vs. SVG icons
    
    ### Example output
    
    #### Before
    
    ![image](https://user-images.githubusercontent.com/409245/39830956-c782881e-5377-11e8-93a2-bc2c08951d88.png)
    
    #### After
    
    ![image](https://user-images.githubusercontent.com/409245/39830964-ce84476a-5377-11e8-89c4-8bb872c3fa9c.png)

--- test/screenshot/lib/controller.js
+++ test/screenshot/lib/controller.js
@@ -103,7 +103,7 @@ class Controller {
    */
   async uploadOneAsset_(assetFileRelativePath, testCases) {
     const assetFile = new UploadableFile({
-      destinationParentDirectory: `${this.baseUploadDir_}/assets`,
+      destinationParentDirectory: this.baseUploadDir_,
       destinationRelativeFilePath: assetFileRelativePath,
       fileContent: await readFileAsync(`${this.sourceDir_}/${assetFileRelativePath}`),
     });
@@ -213,8 +213,8 @@ class Controller {
     const imageName = `${osApiName}_${browserApiName}.png`.toLowerCase().replace(/[^\w.]+/g, '');
     const imageData = await this.downloadImage_(cbtResult.images.chromeless);
     const imageFile = new UploadableFile({
-      destinationParentDirectory: `${this.baseUploadDir_}/screenshots`,
-      destinationRelativeFilePath: `${testCase.htmlFile.destinationRelativeFilePath}/${imageName}`,
+      destinationParentDirectory: this.baseUploadDir_,
+      destinationRelativeFilePath: `${testCase.htmlFile.destinationRelativeFilePath}.${imageName}`,
       fileContent: imageData,
       userAgent: await CbtUserAgent.fetchBrowserByApiName(osApiName, browserApiName),
     });
@@ -252,15 +252,120 @@ class Controller {
       .then(
         (image) => {
           return new Promise((resolve, reject) => {
+            const {rows, cols} = this.getCropMatches_(image);
+            const cropRect = this.getCropRect_({rows, cols});
+            const {x, y, w, h} = cropRect;
+
             image
-              .autocrop()
+              .crop(x, y, w, h)
               .getBuffer(jimp.MIME_PNG, (err, buffer) => {
                 return err ? reject(err) : resolve(buffer);
-              });
+              })
+            ;
           });
         },
         (err) => Promise.reject(err)
       );
+  }
+
+  /**
+   * @param {!Jimp} jimpImage
+   * @return {{rows: !Array<!Array<boolean>, cols: !Array<!Array<boolean>}}
+   * @private
+   */
+  getCropMatches_(jimpImage) {
+    const trimColors = [0x333333FF];
+    const rows = [];
+    const cols = [];
+
+    jimpImage.scan(0, 0, jimpImage.bitmap.width, jimpImage.bitmap.height, (x, y) => {
+      if (!rows[y]) {
+        rows[y] = [];
+      }
+      if (!cols[x]) {
+        cols[x] = [];
+      }
+      const isMatch = trimColors.includes(jimpImage.getPixelColor(x, y));
+      rows[y][x] = isMatch;
+      cols[x][y] = isMatch;
+    });
+
+    return {rows, cols};
+  }
+
+  /**
+   * @param {!Array<!Array<boolean>>} rows
+   * @param {!Array<!Array<boolean>>} cols
+   * @return {{x: number, y: number, w: number, h: number}}
+   * @private
+   */
+  getCropRect_({rows, cols}) {
+    const HIGH_MATCH_PERCENTAGE = 0.95;
+    const LOW_MATCH_PERCENTAGE = 0.67;
+
+    const amounts = {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    };
+
+    for (const row of rows) {
+      if (matchPercentage(row) > HIGH_MATCH_PERCENTAGE) {
+        amounts.top++;
+      } else {
+        break;
+      }
+    }
+
+    // `reverse()` mutates the array in-place, so we call `concat()` first to create a copy of the array.
+    for (const row of rows.concat().reverse()) {
+      if (matchPercentage(row) > HIGH_MATCH_PERCENTAGE) {
+        amounts.bottom++;
+      } else {
+        break;
+      }
+    }
+
+
+    for (const col of cols) {
+      if (matchPercentage(skipCroppedRows(col)) > HIGH_MATCH_PERCENTAGE) {
+        amounts.left++;
+      } else {
+        break;
+      }
+    }
+
+    /* eslint-disable max-len */
+    // `reverse()` mutates the array in-place, so we call `concat()` first to create a copy of the array.
+    for (const col of cols.concat().reverse()) {
+      // Use a lower match percentage on the right side of the image in order to crop Edge popovers:
+      // https://storage.googleapis.com/mdc-web-screenshot-tests/advorak/2018/05/08/20_40_45_142/c6cc25f87/mdc-button/classes/baseline.html.win10e17_edge17.png
+      if (matchPercentage(skipCroppedRows(col)) > LOW_MATCH_PERCENTAGE) {
+        amounts.right++;
+      } else {
+        break;
+      }
+    }
+    /* eslint-enable max-len */
+
+    return {
+      x: amounts.left,
+      y: amounts.top,
+      w: cols.length - amounts.right - amounts.left,
+      h: rows.length - amounts.bottom - amounts.top,
+    };
+
+    function skipCroppedRows(col) {
+      const startRowIndex = amounts.top;
+      const endRowIndex = rows.length - amounts.bottom;
+      return col.slice(startRowIndex, endRowIndex);
+    }
+
+    function matchPercentage(matchList) {
+      const numMatchingPixelsInRow = matchList.filter((isMatch) => isMatch).length;
+      return numMatchingPixelsInRow / matchList.length;
+    }
   }
 
   /**
