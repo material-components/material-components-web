496fa997b84404de88cf6a9cc94a68cda836effa
Rachel Friedman <rlfriedman@google.com>
2018-05-31 11:06:50 -0700

    WIP: Actually add switch JS

---
+++ packages/mdc-switch/foundation.js
@@ -0,0 +1,115 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import MDCFoundation from '@material/base/foundation';
+/* eslint-disable no-unused-vars */
+import {MDCSelectionControlState} from '@material/selection-control/index';
+import MDCSwitchAdapter from './adapter';
+/* eslint-enable no-unused-vars */
+import {cssClasses, strings} from './constants';
+
+/**
+ * @extends {MDCFoundation<!MDCSwitchAdapter>}
+ */
+class MDCSwitchFoundation extends MDCFoundation {
+  /** @return enum {string} */
+  static get strings() {
+    return strings;
+  }
+
+  /** @return enum {string} */
+  static get cssClasses() {
+    return cssClasses;
+  }
+
+  /** @return {!MDCSwitchAdapter} */
+  static get defaultAdapter() {
+    return /** @type {!MDCSwitchAdapter} */ ({
+      addClass: (/* className: string */) => {},
+      removeClass: (/* className: string */) => {},
+      registerChangeHandler: (/* handler: EventListener */) => {},
+      deregisterChangeHandler: (/* handler: EventListener */) => {},
+      getNativeControl: () => /* !MDCSelectionControlState */ {},
+
+    });
+  }
+  
+  constructor(adapter) {
+    super(Object.assign(MDCSwitchFoundation.defaultAdapter, adapter));
+
+    this.changeHandler_ = /** @private {!EventListener} */ (
+      () => this.handleChange());
+  }
+
+  init() {
+    this.adapter_.registerChangeHandler(this.changeHandler_);
+  }
+
+  destroy() {
+    this.adapter_.deregisterChangeHandler(this.changeHandler_);
+  }
+
+  /** @return {boolean} */
+  isChecked() {
+    return this.getNativeControl_().checked;
+  }
+
+  /** @param {boolean} checked */
+  setChecked(checked) {
+    this.getNativeControl_().checked = checked;
+    this.updateCheckedStyling_();
+  }
+
+  /** @return {boolean} */
+  isDisabled() {
+    return this.getNativeControl_().disabled;
+  }
+
+  /** @param {boolean} disabled */
+  setDisabled(disabled) {
+    this.getNativeControl_().disabled = disabled;
+    if (disabled) {
+      this.adapter_.addClass(cssClasses.DISABLED);
+    } else {
+      this.adapter_.removeClass(cssClasses.DISABLED);
+    }
+  }
+
+  /**
+   * Handles the change event for the switch.
+   */
+  handleChange() {
+    this.updateCheckedStyling_();
+  }
+
+  /**
+   * @return {!MDCSelectionControlState}
+   * @private
+   */
+  getNativeControl_() {
+    return this.adapter_.getNativeControl() || {
+      checked: false,
+      disabled: false,
+    };
+  }
+
+  updateCheckedStyling_() {
+    this.isChecked() ? this.adapter_.addClass(cssClasses.TOGGLED_ON) : this.adapter_.removeClass(cssClasses.TOGGLED_ON);
+  }
+}
+
+export default MDCSwitchFoundation;
---
+++ packages/mdc-switch/index.js
@@ -0,0 +1,121 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import MDCComponent from '@material/base/component';
+/* eslint-disable no-unused-vars */
+import {MDCSelectionControlState, MDCSelectionControl} from '@material/selection-control/index';
+/* eslint-enable no-unused-vars */
+import MDCSwitchFoundation from './foundation';
+import {MDCRipple, MDCRippleFoundation} from '@material/ripple/index';
+import {getMatchesProperty} from '@material/ripple/util';
+
+/**
+ * @extends MDCComponent<!MDCSwitchFoundation>
+ * @implements {MDCSelectionControl}
+ */
+class MDCSwitch extends MDCComponent {
+  static attachTo(root) {
+    return new MDCSwitch(root);
+  }
+
+  constructor(...args) {
+    super(...args);
+
+    /** @private {!MDCRipple} */
+    this.ripple_ = this.initRipple_();
+
+    this.foundation_.setChecked(true);
+  }
+
+  /**
+   * Returns the state of the native control element, or null if the native control element is not present.
+   * @return {?MDCSelectionControlState}
+   * @private
+   */
+  get nativeControl_() {
+    const {NATIVE_CONTROL_SELECTOR} = MDCSwitchFoundation.strings;
+    const el = /** @type {?MDCSelectionControlState} */ (
+      this.root_.querySelector(NATIVE_CONTROL_SELECTOR));
+    return el;
+  }
+
+  /**
+   * @return {!MDCRipple}
+   * @private
+   */
+  initRipple_() {
+    const {RIPLE_SURFACE_SELECTOR} = MDCSwitchFoundation.strings;
+    const rippleSurface = /** @type {!Element} */ (this.root_.querySelector(RIPLE_SURFACE_SELECTOR));
+
+    const MATCHES = getMatchesProperty(HTMLElement.prototype);
+    const adapter = Object.assign(MDCRipple.createAdapter(this), {
+      isUnbounded: () => true,
+      isSurfaceActive: () => this.nativeControl_[MATCHES](':active'),
+      addClass: (className) => rippleSurface.classList.add(className),
+      removeClass: (className) => rippleSurface.classList.remove(className),
+      registerInteractionHandler: (type, handler) => this.nativeControl_.addEventListener(type, handler),
+      deregisterInteractionHandler: (type, handler) => this.nativeControl_.removeEventListener(type, handler),
+      updateCssVariable: (varName, value) => rippleSurface.style.setProperty(varName, value),
+      computeBoundingRect: () => rippleSurface.getBoundingClientRect(),
+    });
+    const foundation = new MDCRippleFoundation(adapter);
+    return new MDCRipple(this.root_, foundation);
+  }
+
+  /** @return {!MDCSwitchFoundation} */
+  getDefaultFoundation() {
+    return new MDCSwitchFoundation({
+      addClass: (className) => this.root_.classList.add(className),
+      removeClass: (className) => this.root_.classList.remove(className),
+      registerChangeHandler: (handler) => this.nativeControl_.addEventListener('change', handler),
+      deregisterChangeHandler: (handler) => this.nativeControl_.removeEventListener('change', handler),
+      getNativeControl: () => this.nativeControl_,
+    });
+  }
+
+  /** @return {!MDCRipple} */
+  get ripple() {
+    return this.ripple_;
+  }
+
+  /** @return {boolean} */
+  get checked() {
+    return this.foundation_.isChecked();
+  }
+
+  /** @param {boolean} checked */
+  set checked(checked) {
+    this.foundation_.setChecked(checked);
+  }
+
+  /** @return {boolean} */
+  get disabled() {
+    return this.foundation_.isDisabled();
+  }
+
+  /** @param {boolean} disabled */
+  set disabled(disabled) {
+    this.foundation_.setDisabled(disabled);
+  }
+
+  destroy() {
+    this.ripple_.destroy();
+    super.destroy();
+  }
+}
+
+export {MDCSwitchFoundation, MDCSwitch};
