ab4cba1a9433ca03c53a1656990223ada28ec140
Matt Goo <mattgoo@google.com>
2018-06-15 16:19:40 -0400

    WIP: drawer

---
+++ packages/mdc-drawer/dismissible/foundation.js
@@ -0,0 +1,73 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import MDCDrawerAdapter from '../adapter';
+import MDCFoundation from '@material/base/foundation';
+import {cssClasses} from './constants';
+
+/**
+ * @extends {MDCFoundation<!MDCDrawerAdapter>}
+ */
+class MDCDismissibleDrawerFoundation extends MDCFoundation {
+
+  /** @return enum {string} */
+  static get cssClasses() {
+    return cssClasses;
+  }
+
+  static get defaultAdapter() {
+    return /** @type {!MDCDrawerAdapter} */ ({
+      addClass: (/* className: string */) => {},
+      removeClass: (/* className: string */) => {},
+    });
+  }
+  /**
+   * @param {!MDCDrawerAdapter} adapter
+   */
+  constructor(adapter) {
+    super(adapter);
+
+    /**
+     * isOpen_ is used to indicate if the drawer is open.
+     * @private {boolean}
+     */
+    this.isOpen_ = false;
+  }
+
+  /**
+   * Function to open the drawer from closed state.
+   */
+  open() {
+    this.adapter_.addClass(cssClasses.OPEN);
+    this.isOpen_ = true;
+  }
+
+  /**
+   * Function to close the drawer from the open state.
+   */
+  close() {
+    this.adapter_.removeClass(cssClasses.OPEN);
+    this.isOpen_ = false;
+  }
+
+  isOpen() {
+    return this.isOpen_;
+  }
+
+}
+
+export default MDCDismissibleDrawerFoundation;
---
+++ packages/mdc-drawer/index.js
@@ -0,0 +1,62 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {MDCComponent} from '@material/base/index';
+import MDCDismissibleDrawerFoundation from './dismissible/foundation';
+import {cssClasses} from './constants';
+
+/**
+ * @extends {MDCComponent<!MDCDismissibleDrawerFoundation>}
+ * @final
+ */
+export class MDCDrawer extends MDCComponent {
+  /**
+   * @param {!Element} root
+   * @return {!MDCDrawer}
+   */
+  static attachTo(root) {
+    return new MDCDrawer(root);
+  }
+
+  get open() {
+    return this.foundation_.isOpen();
+  }
+
+  set open(value) {
+    if (value) {
+      this.foundation_.open();
+    } else {
+      this.foundation_.close();
+    }
+  }
+
+  // Return the drawer element inside the component.
+  get drawer() {
+    return this.root_.querySelector(MDCPersistentDrawerFoundation.strings.DRAWER_SELECTOR);
+  }
+
+  getDefaultFoundation() {
+    /** @type {!MDCDrawerAdapter} */
+    const adapter = /** @type {!MDCDrawerAdapter} */ (Object.assign({
+      addClass: (className) => this.root_.classList.add(className),
+      removeClass: (className) => this.root_.classList.remove(className),
+    });
+
+    if (this.root_.classList.contains(cssClasses.DISMISSIBLE_CLASS)) {
+      return new MDCDismissibleDrawerFoundation(adapter);
+    }
+  }
+}
