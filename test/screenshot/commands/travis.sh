dfe0d360e923f62f2b26039cf6eac8eb5dde0dca
Andy Dvorak <advorak@google.com>
2018-07-14 04:57:26 -0700

    WIP: Move encrypted GCS credential file

--- test/screenshot/commands/travis.sh
+++ test/screenshot/commands/travis.sh
@@ -11,12 +11,10 @@ if [ "$TEST_SUITE" == 'screenshot' ]; then
   export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 
   openssl aes-256-cbc -K $encrypted_2fd1810d2cb7_key -iv $encrypted_2fd1810d2cb7_iv \
-    -in travis-gcs.tar.gz.enc -out travis-gcs.tar.gz -d
-  tar -xzf travis-gcs.tar.gz
-  mv travis-gcs.json /tmp/mdc-travis-gcs.json
-  rm travis-gcs.tar.gz
+    -in test/screenshot/auth/travis-gcs.tar.gz.enc -out /tmp/travis-gcs.tar.gz -d
+  tar -xzf /tmp/travis-gcs.tar.gz -C /tmp/
 
-  gcloud auth activate-service-account --key-file /tmp/mdc-travis-gcs.json
+  gcloud auth activate-service-account --key-file /tmp/travis-gcs.json
   gcloud config set project material-components-web
   gcloud components install gsutil
 fi
