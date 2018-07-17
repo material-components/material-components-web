b1408ea826919c1702b144f03e85ccf14efe59b3
Esteban Gonzalez <esgonzalez@google.com>
2018-07-16 18:50:59 -0700

    feat(slider): Create new slider component (#3003)
    New Slider feature with foundation/adapter for basic click & drag functionality, getters and setters for max/min/value, accessibility: aria labels and tab index, README and tests.

---
+++ packages/mdc-slider/foundation.js
@@ -0,0 +1,253 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {strings} from './constants';
+import MDCSliderAdapter from './adapter';
+
+import MDCFoundation from '@material/base/foundation';
+
+/** @enum {string} */
+const MOVE_EVENT_MAP = {
+  'mousedown': 'mousemove',
+  'touchstart': 'touchmove',
+  'pointerdown': 'pointermove',
+};
+
+const DOWN_EVENTS = ['mousedown', 'pointerdown', 'touchstart'];
+const UP_EVENTS = ['mouseup', 'pointerup', 'touchend'];
+
+/**
+ * @extends {MDCFoundation<!MDCSliderAdapter>}
+ */
+class MDCSliderFoundation extends MDCFoundation {
+  /** @return enum {string} */
+  static get strings() {
+    return strings;
+  }
+
+  /** @return {!MDCSliderAdapter} */
+  static get defaultAdapter() {
+    return /** @type {!MDCSliderAdapter} */ ({
+      hasClass: () => {},
+      addClass: () => {},
+      removeClass: () => {},
+      getAttribute: () => {},
+      setAttribute: () => {},
+      computeBoundingRect: () => {},
+      registerEventHandler: () => {},
+      deregisterEventHandler: () => {},
+      registerBodyEventHandler: () => {},
+      deregisterBodyEventHandler: () => {},
+      registerWindowResizeHandler: () => {},
+      deregisterWindowResizeHandler: () => {},
+      notifyInput: () => {},
+      notifyChange: () => {},
+      setThumbStyleProperty: () => {},
+      setTrackFillStyleProperty: () => {},
+    });
+  }
+
+  /**
+   * Creates a new instance of MDCSliderFoundation
+   * @param {?MDCSliderAdapter} adapter
+   */
+  constructor(adapter) {
+    super(Object.assign(MDCSliderFoundation.defaultAdapter, adapter));
+    /** @private {?ClientRect} */
+    this.rect_ = null;
+    /** @private {number} */
+    this.min_ = 0;
+    /** @private {number} */
+    this.max_ = 100;
+    /** @private {number} */
+    this.value_ = 0;
+    /** @private {function(!Event): undefined} */
+    this.interactionStartHandler_ = (evt) => this.handleInteractionStart(evt);
+    /** @private {function(!Event): undefined} */
+    this.interactionMoveHandler_ = (evt) => this.handleInteractionMove(evt);
+    /** @private {function(): undefined} */
+    this.interactionEndHandler_ = () => this.handleInteractionEnd();
+    /** @private {function(): undefined} */
+    this.windowResizeHandler_ = () => this.layout();
+  }
+
+  init() {
+    DOWN_EVENTS.forEach((evtName) => this.adapter_.registerEventHandler(evtName, this.interactionStartHandler_));
+    this.adapter_.registerWindowResizeHandler(this.windowResizeHandler_);
+    this.layout();
+  }
+
+  destroy() {
+    DOWN_EVENTS.forEach((evtName) => {
+      this.adapter_.deregisterEventHandler(evtName, this.interactionStartHandler_);
+    });
+    this.adapter_.deregisterWindowResizeHandler(this.windowResizeHandler_);
+  }
+
+  layout() {
+    this.rect_ = this.adapter_.computeBoundingRect();
+    this.updateUIForCurrentValue_();
+  }
+
+  /** @return {number} */
+  getValue() {
+    return this.value_;
+  }
+
+  /** @param {number} value */
+  setValue(value) {
+    this.setValue_(value);
+  }
+
+  /** @return {number} */
+  getMax() {
+    return this.max_;
+  }
+
+  /** @param {number} max */
+  setMax(max) {
+    if (max < this.min_) {
+      return;
+    }
+    this.max_ = max;
+    this.setValue_(this.value_);
+    this.adapter_.setAttribute(strings.ARIA_VALUEMAX, String(this.max_));
+  }
+
+  /** @return {number} */
+  getMin() {
+    return this.min_;
+  }
+
+  /** @param {number} min */
+  setMin(min) {
+    if (min > this.max_) {
+      return;
+    }
+    this.min_ = min;
+    this.setValue_(this.value_);
+    this.adapter_.setAttribute(strings.ARIA_VALUEMIN, String(this.min_));
+  }
+
+  /**
+   * Called when the user starts interacting with the slider
+   * @param {!Event} evt
+   */
+  handleInteractionStart(evt) {
+    const moveHandler = (evt) => {
+      this.interactionMoveHandler_(evt);
+    };
+
+    // Note: endHandler is [de]registered on ALL potential pointer-related release event types, since some browsers
+    // do not always fire these consistently in pairs.
+    // (See https://github.com/material-components/material-components-web/issues/1192)
+    const endHandler = () => {
+      this.interactionEndHandler_();
+      this.adapter_.deregisterBodyEventHandler(MOVE_EVENT_MAP[evt.type], moveHandler);
+      UP_EVENTS.forEach((evtName) => this.adapter_.deregisterBodyEventHandler(evtName, endHandler));
+    };
+
+    this.adapter_.registerBodyEventHandler(MOVE_EVENT_MAP[evt.type], moveHandler);
+    UP_EVENTS.forEach((evtName) => this.adapter_.registerBodyEventHandler(evtName, endHandler));
+    this.setValueFromEvt_(evt);
+  }
+
+  /**
+   * Called when the user moves the slider
+   * @param {!Event} evt
+   */
+  handleInteractionMove(evt) {
+    evt.preventDefault();
+    this.setValueFromEvt_(evt);
+  }
+
+  /**
+   * Called when the user's interaction with the slider ends
+   */
+  handleInteractionEnd() {
+    this.adapter_.notifyChange();
+  }
+
+  /**
+   * Returns the pageX of the event
+   * @param {!Event} evt
+   * @return {number}
+   * @private
+   */
+  getPageX_(evt) {
+    if (evt.targetTouches && evt.targetTouches.length > 0) {
+      return evt.targetTouches[0].pageX;
+    }
+    return evt.pageX;
+  }
+
+  /**
+   * Sets the slider value from an event
+   * @param {!Event} evt
+   * @private
+   */
+  setValueFromEvt_(evt) {
+    const pageX = this.getPageX_(evt);
+    const value = this.computeValueFromPageX_(pageX);
+    this.setValue_(value);
+  }
+
+  /**
+   * Computes the new value from the pageX position
+   * @param {number} pageX
+   * @return {number}
+   */
+  computeValueFromPageX_(pageX) {
+    const xPos = pageX - this.rect_.left;
+    const pctComplete = xPos / this.rect_.width;
+
+    // Fit the percentage complete between the range [min,max]
+    // by remapping from [0, 1] to [min, min+(max-min)].
+    return this.min_ + pctComplete * (this.max_ - this.min_);
+  }
+
+  /**
+   * Sets the value of the slider
+   * @param {number} value
+   */
+  setValue_(value) {
+    if (value < this.min_) {
+      value = this.min_;
+    } else if (value > this.max_) {
+      value = this.max_;
+    }
+    this.value_ = value;
+    this.adapter_.setAttribute(strings.ARIA_VALUENOW, String(this.value_));
+    this.updateUIForCurrentValue_();
+    this.adapter_.notifyInput();
+  }
+
+  /**
+   * Updates the track-fill and thumb style properties to reflect current value
+   */
+  updateUIForCurrentValue_() {
+    const pctComplete = (this.value_ - this.min_) / (this.max_ - this.min_);
+    const translatePx = pctComplete * this.rect_.width;
+
+    requestAnimationFrame(() => {
+      this.adapter_.setThumbStyleProperty('transform', `translateX(${translatePx}px) translateX(-50%)`);
+      this.adapter_.setTrackFillStyleProperty('transform', `scaleX(${translatePx})`);
+    });
+  }
+}
+
+export default MDCSliderFoundation;
