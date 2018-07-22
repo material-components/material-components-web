ed0983c7af029e62a5fe84124cf4aa1b617021d2
Andy Dvorak <advorak@google.com>
2018-07-22 15:24:18 -0700

    WIP: Reword alt text

--- test/screenshot/infra/commands/test.js
+++ test/screenshot/infra/commands/test.js
@@ -304,9 +304,9 @@ ${listMarkdown}
       const memes = {
         'Chow Yun Fat Approves':
           'https://user-images.githubusercontent.com/409245/43050443-c4a34bc6-8dbd-11e8-8386-a330df5e29be.gif',
-        "Bill Murray: You're Awesome":
+        "You're Awesome - Bill Murray":
           'https://user-images.githubusercontent.com/409245/43050464-122754fa-8dbe-11e8-9ef8-6bd806f1fff8.jpg',
-        'Futurama: Bender Applause':
+        'Bender Wants Applause - Futurama':
           'https://user-images.githubusercontent.com/409245/43050501-7ba8e150-8dbe-11e8-930c-412976867686.jpg',
         'High Five Handshake':
           'https://user-images.githubusercontent.com/409245/43050618-dbd7f06e-8dc0-11e8-8a7c-4fe1b0a63f49.gif',
