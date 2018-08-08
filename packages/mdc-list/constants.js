c8ded0a3103efd0880157df6aef88a3a25b6f823
Will Ernest <34519388+williamernest@users.noreply.github.com>
2018-08-08 09:38:06 -0700

    fix(list): Update clickable elements selector (#3312)

--- packages/mdc-list/constants.js
+++ packages/mdc-list/constants.js
@@ -24,9 +24,9 @@ const cssClasses = {
 /** @enum {string} */
 const strings = {
   ARIA_ORIENTATION: 'aria-orientation',
-  ARIA_ORIENTATION_VERTICAL: 'vertical',
+  ARIA_ORIENTATION_HORIZONTAL: 'horizontal',
   ARIA_SELECTED: 'aria-selected',
-  FOCUSABLE_CHILD_ELEMENTS: 'button:not(:disabled), a',
+  FOCUSABLE_CHILD_ELEMENTS: `.${cssClasses.LIST_ITEM_CLASS} button:not(:disabled), .${cssClasses.LIST_ITEM_CLASS} a`,
   ENABLED_ITEMS_SELECTOR: '.mdc-list-item:not(.mdc-list-item--disabled)',
 };
 
