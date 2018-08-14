6316558e800f7d22e1e15a8abadf093667f4c093
Andrew C. Dvorak <acdvorak@gmail.com>
2018-08-14 12:25:58 -0700

    chore(infrastructure): Add "enable animation" checkbox (#3350)
    ### What it does
    
    - Adds an "Enable animation" checkbox to `linear-progress` pages:
        - For screenshot test pages to replace demo pages, we need to be able to manually verify animations. However, we also need to ensure consistent rendering for screenshot tests.
        - The checkbox is positioned _outside_ the green cropping border, so it does not appear in screenshot images.
        - Checkbox state is synchronized with a URL param to enable persistence between reloads and linking to a specific state (e.g., [animation enabled](https://storage.googleapis.com/mdc-web-screenshot-tests/travis/2018/08/14/18_05_33_820/spec/mdc-linear-progress/classes/baseline.html?enable_animation=true) vs. [animation disabled](https://storage.googleapis.com/mdc-web-screenshot-tests/travis/2018/08/14/18_05_33_820/spec/mdc-linear-progress/classes/baseline.html?enable_animation=false))
        - This is a proof-of-concept for adding a "show redlines" checkbox to `mdc-textfield` pages.
    - Adds `flex-direction: column` to `.test-container`
        - Causes subpixel rendering to change slightly for Drawer screenshots in Edge.
    - Simplifies `test-animation--paused`:
        - Moves `test-animation--paused` from multiple subelements to `<body>`
        - Adds `::before`/`::after` and descendant `*` selectors to `.test-animation--paused` rule
        - One top-level CSS class now disables all animations on the page, and can be toggled on/off
    
    ### Example output
    
    ![image](https://user-images.githubusercontent.com/409245/44109382-bdb70928-9fb1-11e8-913a-15a04a3c9b48.png)

--- test/screenshot/spec/mdc-linear-progress/fixture.js
+++ test/screenshot/spec/mdc-linear-progress/fixture.js
@@ -14,20 +14,51 @@
  * limitations under the License.
  */
 
-mdc.testFixture.fontsLoaded.then(() => {
-  [].forEach.call(document.querySelectorAll('.mdc-linear-progress'), (progressEl) => {
-    const linearProgress = mdc.linearProgress.MDCLinearProgress.attachTo(progressEl);
+import 'url-search-params-polyfill';
 
-    // TODO(acdvorak): Implement this in MDCLinearProgress initialSyncWithDOM()
-    const progressValue = parseFloat(progressEl.getAttribute('data-test-linear-progress-value'));
-    const bufferValue = parseFloat(progressEl.getAttribute('data-test-linear-progress-buffer'));
+[].forEach.call(document.querySelectorAll('.mdc-linear-progress'), (progressEl) => {
+  const linearProgress = mdc.linearProgress.MDCLinearProgress.attachTo(progressEl);
 
-    if (progressValue > 0) {
-      linearProgress.progress = progressValue;
-    }
+  // TODO(acdvorak): Implement this in MDCLinearProgress initialSyncWithDOM()
+  const progressValue = parseFloat(progressEl.getAttribute('data-test-linear-progress-value'));
+  const bufferValue = parseFloat(progressEl.getAttribute('data-test-linear-progress-buffer'));
 
-    if (bufferValue > 0) {
-      linearProgress.buffer = bufferValue;
-    }
-  });
+  if (progressValue > 0) {
+    linearProgress.progress = progressValue;
+  }
+
+  if (bufferValue > 0) {
+    linearProgress.buffer = bufferValue;
+  }
 });
+
+const enableAnimationCheckboxEl = document.querySelector('#test-linear-progress-enable-animation');
+
+function checkUrlParams() {
+  const params = new URLSearchParams(window.location.search);
+  if (params.has('enable_animation') && params.get('enable_animation') !== 'false') {
+    document.body.classList.remove('test-animation--paused');
+    enableAnimationCheckboxEl.checked = true;
+  } else {
+    document.body.classList.add('test-animation--paused');
+    enableAnimationCheckboxEl.checked = false;
+  }
+}
+
+enableAnimationCheckboxEl.addEventListener('change', (evt) => {
+  const params = new URLSearchParams(window.location.search);
+  if (evt.target.checked) {
+    params.set('enable_animation', 'true');
+  } else {
+    params.delete('enable_animation');
+  }
+  const path = window.location.pathname;
+  const qs = String(params) ? '?' + String(params) : '';
+  const hash = window.location.hash;
+  window.history.replaceState({}, 'title', `${path}${qs}${hash}`);
+  setTimeout(checkUrlParams, 100);
+});
+
+window.addEventListener('popstate', checkUrlParams);
+
+checkUrlParams();
