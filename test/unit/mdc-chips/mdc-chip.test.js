410b1c783cce17de6232eb3efa1b9909b63bb7e1
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2018-08-20 15:02:14 -0400

    chore(chips): Improve test coverage (#3385)

--- test/unit/mdc-chips/mdc-chip.test.js
+++ test/unit/mdc-chips/mdc-chip.test.js
@@ -16,6 +16,7 @@
 
 import bel from 'bel';
 import {assert} from 'chai';
+import domEvents from 'dom-events';
 import td from 'testdouble';
 
 import {MDCRipple} from '../../../packages/mdc-ripple';
@@ -27,9 +28,55 @@ const getFixture = () => bel`
   </div>
 `;
 
-const getLeadingIcon = () => bel`
-  <i class="material-icons mdc-chip__icon mdc-chip__icon--leading">face</i>
+const getFixtureWithCheckmark = () => bel`
+  <div class="mdc-chip">
+    <div class="mdc-chip__checkmark" >
+      <svg class="mdc-chip__checkmark-svg" viewBox="-2 -3 30 30">
+        <path class="mdc-chip__checkmark-path" fill="none" stroke="black"
+              d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+      </svg>
+    </div>
+    <div class="mdc-chip__text">Chip content</div>
+  </div>
 `;
+
+const addLeadingIcon = (root) => {
+  const icon = bel`<i class="material-icons mdc-chip__icon mdc-chip__icon--leading">face</i>`;
+  root.insertBefore(icon, root.firstChild);
+  return icon;
+};
+
+const addTrailingIcon = (root) => {
+  const icon = bel`<i class="material-icons mdc-chip__icon mdc-chip__icon--trailing">cancel</i>`;
+  root.appendChild(icon);
+  return icon;
+};
+
+class FakeRipple {
+  constructor(root) {
+    this.root = root;
+    this.destroy = td.func('.destroy');
+  }
+}
+
+function setupTest() {
+  const root = getFixture();
+  const component = new MDCChip(root);
+  return {root, component};
+}
+
+function setupMockRippleTest() {
+  const root = getFixture();
+  const component = new MDCChip(root, undefined, () => new FakeRipple());
+  return {root, component};
+}
+
+function setupMockFoundationTest(root = getFixture()) {
+  const MockFoundationConstructor = td.constructor(MDCChipFoundation);
+  const mockFoundation = new MockFoundationConstructor();
+  const component = new MDCChip(root, mockFoundation);
+  return {root, component, mockFoundation};
+}
 
 suite('MDCChip');
 
@@ -37,11 +84,51 @@ test('attachTo returns an MDCChip instance', () => {
   assert.isTrue(MDCChip.attachTo(getFixture()) instanceof MDCChip);
 });
 
-function setupTest() {
+test('#initialSyncWithDOM sets up event handlers', () => {
+  const {root, mockFoundation} = setupMockFoundationTest();
+
+  domEvents.emit(root, 'click');
+  td.verify(mockFoundation.handleInteraction(td.matchers.anything()), {times: 1});
+
+  domEvents.emit(root, 'transitionend');
+  td.verify(mockFoundation.handleTransitionEnd(td.matchers.anything()), {times: 1});
+});
+
+test('#initialSyncWithDOM sets up interaction event handler on trailing icon if present', () => {
   const root = getFixture();
-  const component = new MDCChip(root);
-  return {root, component};
-}
+  const icon = addTrailingIcon(root);
+  const {mockFoundation} = setupMockFoundationTest(root);
+
+  domEvents.emit(icon, 'click');
+  td.verify(mockFoundation.handleTrailingIconInteraction(td.matchers.anything()), {times: 1});
+});
+
+test('#destroy removes event handlers', () => {
+  const {root, component, mockFoundation} = setupMockFoundationTest();
+  component.destroy();
+
+  domEvents.emit(root, 'click');
+  td.verify(mockFoundation.handleInteraction(td.matchers.anything()), {times: 0});
+
+  domEvents.emit(root, 'transitionend');
+  td.verify(mockFoundation.handleTransitionEnd(td.matchers.anything()), {times: 0});
+});
+
+test('#destroy removes interaction event handler on trailing icon if present', () => {
+  const root = getFixture();
+  const icon = addTrailingIcon(root);
+  const {component, mockFoundation} = setupMockFoundationTest(root);
+
+  component.destroy();
+  domEvents.emit(icon, 'click');
+  td.verify(mockFoundation.handleTrailingIconInteraction(td.matchers.anything()), {times: 0});
+});
+
+test('#destroy destroys ripple', () => {
+  const {component} = setupMockRippleTest();
+  component.destroy();
+  td.verify(component.ripple.destroy());
+});
 
 test('get ripple returns MDCRipple instance', () => {
   const {component} = setupTest();
@@ -58,44 +145,42 @@ test('sets id on chip if attribute exists', () => {
   assert.equal(component.id, 'hello-chip');
 });
 
-test('#adapter.hasClass returns true if class is set on chip set element', () => {
+test('adapter#hasClass returns true if class is set on chip set element', () => {
   const {root, component} = setupTest();
   root.classList.add('foo');
   assert.isTrue(component.getDefaultFoundation().adapter_.hasClass('foo'));
 });
 
-test('#adapter.addClass adds a class to the root element', () => {
+test('adapter#addClass adds a class to the root element', () => {
   const {root, component} = setupTest();
   component.getDefaultFoundation().adapter_.addClass('foo');
   assert.isTrue(root.classList.contains('foo'));
 });
 
-test('#adapter.removeClass removes a class from the root element', () => {
+test('adapter#removeClass removes a class from the root element', () => {
   const {root, component} = setupTest();
   root.classList.add('foo');
   component.getDefaultFoundation().adapter_.removeClass('foo');
   assert.isFalse(root.classList.contains('foo'));
 });
 
-test('#adapter.addClassToLeadingIcon adds a class to the leading icon element', () => {
-  const root = getFixture();
-  const leadingIcon = getLeadingIcon();
-  root.appendChild(leadingIcon);
+test('adapter#addClassToLeadingIcon adds a class to the leading icon element', () => {
+  const root = getFixtureWithCheckmark();
+  const leadingIcon = addLeadingIcon(root);
   const component = new MDCChip(root);
 
   component.getDefaultFoundation().adapter_.addClassToLeadingIcon('foo');
   assert.isTrue(leadingIcon.classList.contains('foo'));
 });
 
-test('#adapter.addClassToLeadingIcon does nothing if no leading icon element is present', () => {
+test('adapter#addClassToLeadingIcon does nothing if no leading icon element is present', () => {
   const {component} = setupTest();
   assert.doesNotThrow(() => component.getDefaultFoundation().adapter_.addClassToLeadingIcon('foo'));
 });
 
-test('#adapter.removeClassFromLeadingIcon removes a class from the leading icon element', () => {
-  const root = getFixture();
-  const leadingIcon = getLeadingIcon();
-  root.appendChild(leadingIcon);
+test('adapter#removeClassFromLeadingIcon removes a class from the leading icon element', () => {
+  const root = getFixtureWithCheckmark();
+  const leadingIcon = addLeadingIcon(root);
   const component = new MDCChip(root);
 
   leadingIcon.classList.add('foo');
@@ -103,7 +188,7 @@ test('#adapter.removeClassFromLeadingIcon removes a class from the leading icon 
   assert.isFalse(leadingIcon.classList.contains('foo'));
 });
 
-test('#adapter.removeClassFromLeadingIcon does nothing if no leading icon element is present', () => {
+test('adapter#removeClassFromLeadingIcon does nothing if no leading icon element is present', () => {
   const {component} = setupTest();
   assert.doesNotThrow(() => component.getDefaultFoundation().adapter_.removeClassFromLeadingIcon('foo'));
 });
@@ -115,7 +200,7 @@ test('adapter#eventTargetHasClass returns true if given element has class', () =
   assert.isTrue(component.getDefaultFoundation().adapter_.eventTargetHasClass(mockEventTarget, 'foo'));
 });
 
-test('#adapter.notifyInteraction emits ' + MDCChipFoundation.strings.INTERACTION_EVENT, () => {
+test('adapter#notifyInteraction emits ' + MDCChipFoundation.strings.INTERACTION_EVENT, () => {
   const {component} = setupTest();
   const handler = td.func('interaction handler');
 
@@ -126,7 +211,7 @@ test('#adapter.notifyInteraction emits ' + MDCChipFoundation.strings.INTERACTION
   td.verify(handler(td.matchers.anything()));
 });
 
-test('#adapter.notifyTrailingIconInteraction emits ' +
+test('adapter#notifyTrailingIconInteraction emits ' +
   MDCChipFoundation.strings.TRAILING_ICON_INTERACTION_EVENT, () => {
   const {component} = setupTest();
   const handler = td.func('interaction handler');
@@ -138,7 +223,7 @@ test('#adapter.notifyTrailingIconInteraction emits ' +
   td.verify(handler(td.matchers.anything()));
 });
 
-test('#adapter.notifyRemoval emits ' + MDCChipFoundation.strings.REMOVAL_EVENT, () => {
+test('adapter#notifyRemoval emits ' + MDCChipFoundation.strings.REMOVAL_EVENT, () => {
   const {component} = setupTest();
   const handler = td.func('interaction handler');
 
@@ -149,12 +234,19 @@ test('#adapter.notifyRemoval emits ' + MDCChipFoundation.strings.REMOVAL_EVENT, 
   td.verify(handler(td.matchers.anything()));
 });
 
-function setupMockFoundationTest(root = getFixture()) {
-  const MockFoundationConstructor = td.constructor(MDCChipFoundation);
-  const mockFoundation = new MockFoundationConstructor();
-  const component = new MDCChip(root, mockFoundation);
-  return {root, component, mockFoundation};
-}
+test('adapter#getComputedStyleValue returns property value from root element styles', () => {
+  const {root, component} = setupTest();
+  assert.equal(
+    component.getDefaultFoundation().adapter_.getComputedStyleValue('color'),
+    window.getComputedStyle(root).getPropertyValue('color'));
+});
+
+test('adapter#setStyleProperty sets a style property on the root element', () => {
+  const {root, component} = setupTest();
+  const color = 'blue';
+  component.getDefaultFoundation().adapter_.setStyleProperty('color', color);
+  assert.equal(root.style.getPropertyValue('color'), color);
+});
 
 test('#get selected proxies to foundation', () => {
   const {component, mockFoundation} = setupMockFoundationTest();
