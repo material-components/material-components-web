e815a6f704dd681ef1fd19d3bb0557a5a784fded
Andy Dvorak <advorak@google.com>
2018-08-29 05:48:35 -0700

    chore(infrastructure): Add URL params for Google Analytics
    ![image](https://user-images.githubusercontent.com/409245/44788390-ca062800-ab4e-11e8-8393-50642bdc2634.png)

--- test/screenshot/infra/lib/github-api.js
+++ test/screenshot/infra/lib/github-api.js
@@ -25,11 +25,13 @@ const VError = require('verror');
 const debounce = require('debounce');
 const octokit = require('@octokit/rest');
 
+const Analytics = require('./analytics');
 const GitRepo = require('./git-repo');
 const getStackTrace = require('./stacktrace')('GitHubApi');
 
 class GitHubApi {
   constructor() {
+    this.analytics_ = new Analytics();
     this.gitRepo_ = new GitRepo();
     this.octokit_ = octokit();
     this.isTravis_ = process.env.TRAVIS === 'true';
@@ -146,7 +148,7 @@ class GitHubApi {
         description = `${numChanged.toLocaleString()} screenshots differ from PR's golden.json`;
       }
 
-      targetUrl = meta.report_html_file.public_url;
+      targetUrl = reportFileUrl;
     } else {
       const runnableScreenshots = screenshots.runnable_screenshot_list;
       const numTotal = runnableScreenshots.length;
@@ -196,7 +198,7 @@ class GitHubApi {
         repo: 'material-components-web',
         sha,
         state,
-        target_url: targetUrl,
+        target_url: this.analytics_.getUrl({url: targetUrl, source: 'github', type: 'pr_commit_status'}),
         description,
         context: 'screenshot-test/butter-bot',
       });
