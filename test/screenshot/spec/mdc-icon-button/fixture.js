344062f275cc33b15a19b3ad902c18df963ff398
Kenneth G. Franqueiro <kfranqueiro@google.com>
2018-09-05 16:57:03 -0400

    WIP Wait for fontsLoaded

--- test/screenshot/spec/mdc-icon-button/fixture.js
+++ test/screenshot/spec/mdc-icon-button/fixture.js
@@ -1,5 +1,7 @@
-const firstToggleButton = document.querySelector('.mdc-icon-button[aria-pressed]');
-if (firstToggleButton) {
-  mdc.iconButton.MDCIconButtonToggle.attachTo(firstToggleButton);
-  firstToggleButton.focus();
-}
+mdc.testFixture.fontsLoaded.then(() => {
+  const firstToggleButton = document.querySelector('.mdc-icon-button[aria-pressed]');
+  if (firstToggleButton) {
+    mdc.iconButton.MDCIconButtonToggle.attachTo(firstToggleButton);
+    firstToggleButton.focus();
+  }
+});
