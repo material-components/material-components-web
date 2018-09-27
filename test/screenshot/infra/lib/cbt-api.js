bb9eb4ad4bd4919ab0650e712b693a5808c3ee7c
Andy Dvorak <advorak@google.com>
2018-09-27 02:23:23 -0700

    WIP: Refactor

--- test/screenshot/infra/lib/cbt-api.js
+++ test/screenshot/infra/lib/cbt-api.js
@@ -44,7 +44,7 @@ const MDC_CBT_USERNAME = process.env.MDC_CBT_USERNAME;
 const MDC_CBT_AUTHKEY = process.env.MDC_CBT_AUTHKEY;
 const REST_API_BASE_URL = 'https://crossbrowsertesting.com/api/v3';
 const SELENIUM_SERVER_URL = `http://${MDC_CBT_USERNAME}:${MDC_CBT_AUTHKEY}@hub.crossbrowsertesting.com:80/wd/hub`;
-const {ExitCode, SELENIUM_ZOMBIE_SESSION_DURATION_MS} = require('./constants');
+const {ExitCode, CBT_HTTP_MAX_RETRIES, SELENIUM_ZOMBIE_SESSION_DURATION_MS} = require('./constants');
 
 /** @type {?Promise<!Array<!cbt.proto.CbtDevice>>} */
 let allDevicesPromise;
@@ -495,18 +495,32 @@ https://crossbrowsertesting.com/account
         json: true, // Automatically stringify the request body and parse the response body as JSON
       });
     } catch (err) {
-      const MAX_RETRIES = 5;
-      if (retryCount < MAX_RETRIES) {
-        console.error('');
-        console.error(`CBT API request failed: ${method} ${uri}:\n${stackTrace}`);
-        console.error(err);
-        console.error('');
-        console.error(`Retrying request (attempt ${retryCount + 1} of ${MAX_RETRIES})...`);
-        console.error('');
-        return this.sendRequest_(stackTrace, method, endpoint, body, retryCount + 1);
+      if (++retryCount <= CBT_HTTP_MAX_RETRIES) {
+        this.logRetry_(method, uri, err, stackTrace, retryCount);
+        return this.sendRequest_(stackTrace, method, endpoint, body, retryCount);
       }
       throw new VError(err, `CBT API request failed: ${method} ${uri}:\n${stackTrace}`);
     }
+  }
+
+  logRetry_(method, uri, err, stackTrace, retryCount) {
+    const colorLabel = CliColor.magenta('CBT API request failed:');
+    const colorMethod = CliColor.bold(method);
+    const colorUri = CliColor.underline(uri);
+    console.error('');
+    console.error('');
+    console.error(`${colorLabel} ${colorMethod} ${colorUri}`);
+    console.error('');
+    console.error(err);
+    console.error('');
+    console.error(stackTrace);
+    console.error('');
+
+    const colorRetrying = CliColor.magenta('Retrying request');
+    const colorAttemptCount = CliColor.bold.magenta(retryCount);
+    const colorMaxRetries = CliColor.bold.magenta(CBT_HTTP_MAX_RETRIES);
+    console.error(`${colorRetrying} (attempt ${colorAttemptCount} of ${colorMaxRetries})...`);
+    console.error('');
   }
 }
 
