c60107f3b8f693cc8c2bc6225ae3816da15dfd0d
Andy Dvorak <advorak@google.com>
2018-09-26 19:54:34 -0700

    WIP: Remove unnecessary comments and update JSDoc

--- test/screenshot/shield/index.js
+++ test/screenshot/shield/index.js
@@ -41,14 +41,11 @@ class ScreenshotShieldServer {
 
   /** @private */
   allowCORS_(req, res) {
-    // Set CORS headers
-    // e.g. allow GETs from any origin with the Content-Type header
-    // and cache preflight response for an 3600s
     res.set('Access-Control-Allow-Origin', '*.github.com');
-    // res.set('Access-Control-Allow-Origin', '*'); // TODO(acdvorak)
     res.set('Access-Control-Allow-Methods', 'GET');
     res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.set('Access-Control-Max-Age', '3600');
+
     // Send response to OPTIONS requests and terminate the function execution
     if (req.method === 'OPTIONS') {
       res.status(204).send('');
@@ -56,6 +53,7 @@ class ScreenshotShieldServer {
   }
 
   /**
+   * @param {{url: string}} req
    * @return {!Promise<!GitHubStatusInfo>}
    * @private
    */
