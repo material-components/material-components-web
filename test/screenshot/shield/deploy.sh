40650fdfb18c02c9b501046720f9455ec48e677e
Andy Dvorak <advorak@google.com>
2018-09-26 23:00:44 -0700

    WIP: Add `--local` CLI flag to `deploy.sh`

--- test/screenshot/shield/deploy.sh
+++ test/screenshot/shield/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Dev
-#functions start
-#functions deploy screenshot-shield-svg --entry-point=screenshotShieldSvg --trigger-http
-#functions deploy screenshot-shield-url --entry-point=screenshotShieldUrl --trigger-http
-
-# Prod
-gcloud functions deploy screenshot-shield-svg --entry-point=screenshotShieldSvg --runtime nodejs8 --trigger-http
-gcloud functions deploy screenshot-shield-url --entry-point=screenshotShieldUrl --runtime nodejs8 --trigger-http
+if [[ "$1" == '--local' ]]; then
+  functions start
+  functions deploy screenshot-shield-svg --entry-point=screenshotShieldSvg --trigger-http
+  functions deploy screenshot-shield-url --entry-point=screenshotShieldUrl --trigger-http
+else
+  gcloud functions deploy screenshot-shield-svg --entry-point=screenshotShieldSvg --runtime nodejs8 --trigger-http
+  gcloud functions deploy screenshot-shield-url --entry-point=screenshotShieldUrl --runtime nodejs8 --trigger-http
+fi
