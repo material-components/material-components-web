4afa8112f8fe3f32d0469867ca25998973d7d6bc
Andy Dvorak <advorak@google.com>
2018-09-26 22:28:15 -0700

    WIP: Add local commands to deploy.sh

--- test/screenshot/shield/deploy.sh
+++ test/screenshot/shield/deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# Dev
+#functions start
+#functions deploy screenshot-shield-svg --entry-point=screenshotShieldSvg --trigger-http
+#functions deploy screenshot-shield-url --entry-point=screenshotShieldUrl --trigger-http
+
+# Prod
 gcloud functions deploy screenshot-shield-svg --entry-point=screenshotShieldSvg --runtime nodejs8 --trigger-http
 gcloud functions deploy screenshot-shield-url --entry-point=screenshotShieldUrl --runtime nodejs8 --trigger-http
