46e186d29affaac1b20da904d8b447bf02a1cd88
Andy Dvorak <advorak@google.com>
2018-10-04 03:18:54 -0700

    chore(infrastructure): Fix zombie detection
    Fixes `if` condition so that tests are only killed if they haven't received any commands.

--- test/screenshot/infra/lib/cbt-api.js
+++ test/screenshot/infra/lib/cbt-api.js
@@ -407,7 +407,7 @@ https://crossbrowsertesting.com/account
       'GET', '/selenium?active=true&num=100'
     );
 
-    const activeSeleniumTestIds = listResponse.selenium.map((test) => test.selenium_test_id);
+    const activeSeleniumTestIds = listResponse.selenium.map((test) => test.selenium_session_id);
 
     /** @type {!Array<!CbtSeleniumInfoResponse>} */
     const infoResponses = await Promise.all(activeSeleniumTestIds.map((seleniumTestId) => {
@@ -415,27 +415,26 @@ https://crossbrowsertesting.com/account
       return this.sendRequest_(infoStackTrace, 'GET', `/selenium/${seleniumTestId}`);
     }));
 
-    const stalledSeleniumTestIds = [];
+    const stalledSeleniumSessionIds = [];
 
     for (const infoResponse of infoResponses) {
       const lastCommand = infoResponse.commands[infoResponse.commands.length - 1];
+      let timestampMs = null;
 
-      // At least one Selenium command was received
       if (lastCommand) {
-        const commandTimestampMs = new Date(lastCommand.date_issued).getTime();
-        if (Duration.hasElapsed(SELENIUM_ZOMBIE_SESSION_DURATION_MS, commandTimestampMs)) {
-          stalledSeleniumTestIds.push(infoResponse.selenium_test_id);
-        }
+        // At least one Selenium command was received.
+        timestampMs = new Date(lastCommand.date_issued).getTime();
+      } else {
+        // No Selenium commands have been received yet.
+        timestampMs = new Date(infoResponse.start_date || infoResponse.startup_finish_date).getTime();
       }
 
-      // No Selenium commands have been received
-      const startTimestampMs = new Date(infoResponse.start_date || infoResponse.startup_finish_date).getTime();
-      if (Duration.hasElapsed(SELENIUM_ZOMBIE_SESSION_DURATION_MS, startTimestampMs)) {
-        stalledSeleniumTestIds.push(infoResponse.selenium_test_id);
+      if (timestampMs > 0 && Duration.hasElapsed(SELENIUM_ZOMBIE_SESSION_DURATION_MS, timestampMs)) {
+        stalledSeleniumSessionIds.push(infoResponse.selenium_session_id);
       }
     }
 
-    await this.killSeleniumTests(stalledSeleniumTestIds);
+    await this.killSeleniumTests(stalledSeleniumSessionIds);
   }
 
   /**
