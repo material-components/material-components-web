2b43ee1ebaff2f7b838103a842fc6289f447e36e
Andy Dvorak <advorak@google.com>
2018-10-03 19:19:30 -0700

    WIP: Add comments

--- test/screenshot/infra/lib/cloud-datastore.js
+++ test/screenshot/infra/lib/cloud-datastore.js
@@ -190,6 +190,9 @@ class CloudDatastore {
    * @private
    */
   isAuthenticated_() {
+    // By default, the client will authenticate using the service account file specified by the
+    // GOOGLE_APPLICATION_CREDENTIALS environment variable.
+    // See https://googlecloudplatform.github.io/google-cloud-node/#/docs/datastore/latest/guides/authentication
     return Boolean(process.env.GOOGLE_APPLICATION_CREDENTIALS);
   }
 }
