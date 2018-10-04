8579f88d7d208098e0caf78020833b50dd426cfb
Andy Dvorak <advorak@google.com>
2018-10-04 08:45:36 -0700

    WIP: Disable user prompt when updating Datastore indexes

--- test/screenshot/infra/deploy.sh
+++ test/screenshot/infra/deploy.sh
@@ -8,7 +8,7 @@ if [[ "$1" == '--local' ]]; then
   functions deploy screenshot-shield-svg --entry-point=screenshotShieldSvg --trigger-http
   functions deploy screenshot-shield-url --entry-point=screenshotShieldUrl --trigger-http
 elif [[ "$1" == '--prod' ]]; then
-  gcloud datastore indexes create index.yaml
+  gcloud datastore indexes create --quiet index.yaml
   gcloud functions deploy screenshot-shield-svg --entry-point=screenshotShieldSvg --runtime nodejs8 --trigger-http
   gcloud functions deploy screenshot-shield-url --entry-point=screenshotShieldUrl --runtime nodejs8 --trigger-http
 else
