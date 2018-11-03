fe6e2a906c834240bae540e5bc4bdf71fb5d725d
Andy Dvorak <advorak@google.com>
2018-11-03 14:40:08 -0700

    WIP: Formatting

--- packages/mdc-snackbar/constants.js
+++ packages/mdc-snackbar/constants.js
@@ -20,6 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 export const cssClasses = {
   ROOT: 'mdc-snackbar',
   TEXT: 'mdc-snackbar__text',
