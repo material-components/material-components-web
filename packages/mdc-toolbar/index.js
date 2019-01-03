fd7074a9e25bc6dda817555fb89d3469eb5dbcc4
Abhinay Omkar <abhiomkar@gmail.com>
2019-01-03 21:55:46 +0530

    chore: remove destructuring on import

--- packages/mdc-toolbar/index.js
+++ packages/mdc-toolbar/index.js
@@ -21,7 +21,7 @@
  * THE SOFTWARE.
  */
 
-import {MDCComponent} from '@material/base/component';
+import MDCComponent from '@material/base/component';
 import {MDCRipple} from '@material/ripple/index';
 
 import MDCToolbarFoundation from './foundation';
