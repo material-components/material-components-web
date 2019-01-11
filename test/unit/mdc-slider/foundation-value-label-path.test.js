55e2222f6170ac0d1e464daefc021fbe8b5a8d38
Bonnie Zhou <bonniez@google.com>
2019-01-11 13:23:02 -0800

    finish tests

--- test/unit/mdc-slider/foundation-value-label-path.test.js
+++ test/unit/mdc-slider/foundation-value-label-path.test.js
@@ -21,7 +21,7 @@ import {setupFoundationTest} from '../helpers/setup';
 
 import MDCSliderFoundation from '../../../packages/mdc-slider/foundation';
 
-suite.only('MDCSliderFoundation - Value Label Path');
+suite('MDCSliderFoundation - Value Label Path');
 
 const setupTest = () => setupFoundationTest(MDCSliderFoundation);
 
@@ -32,28 +32,84 @@ const TOP_LOBE_RADIUS = 16;
 const TOP_NECK_RADIUS = 14;
 const MAX_TOP_NECK_WIDTH = 15;
 
-test('total horizontal distance added to the top-lobe', () => {
+test('path calculated correctly with label at beginning of slider', () => {
   const {foundation, mockAdapter} = setupTest();
   td.when(mockAdapter.getDigitWidth()).thenReturn(DIGIT_WIDTH);
   td.when(mockAdapter.getCommaWidth()).thenReturn(COMMA_WIDTH);
-  foundation.rect_ = 1000;
+  foundation.rect_ = {width: 1000};
 
   foundation.value_ = 10000000000; // 10,000,000,000
   const numCommas = 3;
   const labelHorizontalWidth = ('10000000000'.length - 2) * DIGIT_WIDTH + numCommas * COMMA_WIDTH;
   const topLobeHorizontal = Math.min(labelHorizontalWidth, MAX_TOP_LOBE_HORIZONTAL);
+  const extraHorizontalWidth = Math.max(labelHorizontalWidth - MAX_TOP_LOBE_HORIZONTAL, 0);
   const topNeckTheta = Math.acos((MAX_TOP_NECK_WIDTH - topLobeHorizontal/2)/(TOP_LOBE_RADIUS + TOP_NECK_RADIUS));
   const topNeckCenterYPos = Math.sqrt(Math.pow(TOP_LOBE_RADIUS + TOP_NECK_RADIUS, 2) -
       Math.pow(MAX_TOP_NECK_WIDTH - topLobeHorizontal/2, 2));
   
-  const leftTopNeckTheta = Math.acos(MAX_TOP_NECK_WIDTH/(TOP_LOBE_RADIUS + TOP_NECK_RADIUS));
+  const translatePx = 0;
+  const leftTopNeckTheta = Math.acos((MAX_TOP_NECK_WIDTH - translatePx)/(TOP_LOBE_RADIUS + TOP_NECK_RADIUS));
   const leftTopNeckCenterYPos = Math.sqrt(Math.pow(TOP_LOBE_RADIUS + TOP_NECK_RADIUS, 2) -
-    Math.pow(MAX_TOP_NECK_WIDTH, 2));
+    Math.pow(MAX_TOP_NECK_WIDTH - translatePx, 2));
+  foundation.calcPath_(translatePx);
 
-  foundation.calcPath_(0);
-  assert.equal(foundation.pathTotalHorizontalDistance_, 90);
+  assert.equal(foundation.pathTotalHorizontalDistance_, topLobeHorizontal + extraHorizontalWidth);
   assert.equal(foundation.pathLeftTopNeckTheta_, leftTopNeckTheta);
   assert.equal(foundation.pathLeftTopNeckCenterYPos_, leftTopNeckCenterYPos);
   assert.equal(foundation.pathRightTopNeckTheta_, topNeckTheta);
   assert.equal(foundation.pathRightTopNeckCenterYPos_, topNeckCenterYPos);  
+});
+
+test('path calculated correctly with label in middle of slider', () => {
+  const {foundation, mockAdapter} = setupTest();
+  td.when(mockAdapter.getDigitWidth()).thenReturn(DIGIT_WIDTH);
+  td.when(mockAdapter.getCommaWidth()).thenReturn(COMMA_WIDTH);
+  foundation.rect_ = {width: 1000};
+
+  foundation.value_ = 10000000000; // 10,000,000,000
+  const numCommas = 3;
+  const labelHorizontalWidth = ('10000000000'.length - 2) * DIGIT_WIDTH + numCommas * COMMA_WIDTH;
+  const topLobeHorizontal = Math.min(labelHorizontalWidth, MAX_TOP_LOBE_HORIZONTAL);
+  const extraHorizontalWidth = Math.max(labelHorizontalWidth - MAX_TOP_LOBE_HORIZONTAL, 0);
+  const topNeckTheta = Math.acos((MAX_TOP_NECK_WIDTH - topLobeHorizontal/2)/(TOP_LOBE_RADIUS + TOP_NECK_RADIUS));
+  const topNeckCenterYPos = Math.sqrt(Math.pow(TOP_LOBE_RADIUS + TOP_NECK_RADIUS, 2) -
+      Math.pow(MAX_TOP_NECK_WIDTH - topLobeHorizontal/2, 2));
+  
+  const translatePx = 500;
+  foundation.calcPath_(translatePx);
+
+  assert.equal(foundation.pathTotalHorizontalDistance_, topLobeHorizontal + extraHorizontalWidth);
+  assert.equal(foundation.pathLeftTopNeckTheta_, topNeckTheta);
+  assert.equal(foundation.pathLeftTopNeckCenterYPos_, topNeckCenterYPos);
+  assert.equal(foundation.pathRightTopNeckTheta_, topNeckTheta);
+  assert.equal(foundation.pathRightTopNeckCenterYPos_, topNeckCenterYPos);  
+});
+
+test('path calculated correctly with label at end of slider', () => {
+  const {foundation, mockAdapter} = setupTest();
+  td.when(mockAdapter.getDigitWidth()).thenReturn(DIGIT_WIDTH);
+  td.when(mockAdapter.getCommaWidth()).thenReturn(COMMA_WIDTH);
+  foundation.rect_ = {width: 1000};
+
+  foundation.value_ = 10000000000; // 10,000,000,000
+  const numCommas = 3;
+  const labelHorizontalWidth = ('10000000000'.length - 2) * DIGIT_WIDTH + numCommas * COMMA_WIDTH;
+  const topLobeHorizontal = Math.min(labelHorizontalWidth, MAX_TOP_LOBE_HORIZONTAL);
+  const extraHorizontalWidth = Math.max(labelHorizontalWidth - MAX_TOP_LOBE_HORIZONTAL, 0);
+  const topNeckTheta = Math.acos((MAX_TOP_NECK_WIDTH - topLobeHorizontal/2)/(TOP_LOBE_RADIUS + TOP_NECK_RADIUS));
+  const topNeckCenterYPos = Math.sqrt(Math.pow(TOP_LOBE_RADIUS + TOP_NECK_RADIUS, 2) -
+      Math.pow(MAX_TOP_NECK_WIDTH - topLobeHorizontal/2, 2));
+  
+  const translatePx = 1000;
+  const rightTopNeckTheta =
+    Math.acos((MAX_TOP_NECK_WIDTH - (foundation.rect_.width - translatePx))/(TOP_LOBE_RADIUS + TOP_NECK_RADIUS));
+  const rightTopNeckCenterYPos = Math.sqrt(Math.pow(TOP_LOBE_RADIUS + TOP_NECK_RADIUS, 2) -
+    Math.pow(MAX_TOP_NECK_WIDTH - (foundation.rect_.width - translatePx), 2));
+  foundation.calcPath_(translatePx);
+
+  assert.equal(foundation.pathTotalHorizontalDistance_, topLobeHorizontal + extraHorizontalWidth);
+  assert.equal(foundation.pathLeftTopNeckTheta_, topNeckTheta);
+  assert.equal(foundation.pathLeftTopNeckCenterYPos_, topNeckCenterYPos);
+  assert.equal(foundation.pathRightTopNeckTheta_, rightTopNeckTheta);
+  assert.equal(foundation.pathRightTopNeckCenterYPos_, rightTopNeckCenterYPos);  
 });
