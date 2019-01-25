218ca51bb180600b85a72ab7d63120b380e97da7
Matt Goo <mattgoo@google.com>
2019-01-25 13:53:45 -0800

    feat: ripple ts conversion

--- packages/mdc-dom/ponyfill.ts
+++ packages/mdc-dom/ponyfill.ts
@@ -48,4 +48,4 @@ function matches(element: Element, selector: string): boolean {
   return nativeMatches.call(element, selector);
 }
 
-export {closest, matches};
+export {closest, matches, MsElement};
