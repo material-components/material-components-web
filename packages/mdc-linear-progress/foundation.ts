17bee8eb6451809dd68af09fdd632cf21563c5e6
Andy Dvorak <advorak@google.com>
2019-01-31 13:09:20 -0800

    Revert "WIP: Shorten import path"
    This reverts commit 8990add6ed29a7af4f62b551bd6320dd80d723b5.

--- packages/mdc-linear-progress/foundation.ts
+++ packages/mdc-linear-progress/foundation.ts
@@ -21,7 +21,7 @@
  * THE SOFTWARE.
  */
 
-import {getCorrectPropertyName} from '@material/animation';
+import {getCorrectPropertyName} from '@material/animation/index';
 import MDCFoundation from '@material/base/foundation';
 import {MDCLinearProgressAdapter} from './adapter';
 import {cssClasses, strings} from './constants';
