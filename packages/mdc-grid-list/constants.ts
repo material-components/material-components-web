ed7f8d792e001ca5c8b7c916e5a8232add63bcf7
Matt Goo <mattgoo@google.com>
2019-02-06 18:11:06 -0800

    feat(grid-list): ts conversion (#4337)
    related #4225

--- packages/mdc-grid-list/constants.ts
+++ packages/mdc-grid-list/constants.ts
@@ -20,6 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 export const strings = {
   TILES_SELECTOR: '.mdc-grid-list__tiles',
   TILE_SELECTOR: '.mdc-grid-tile',
