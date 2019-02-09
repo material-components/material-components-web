eadf01b6ca8c552c7bb010b0498a1de4bf407911
Andy Dvorak <advorak@google.com>
2019-02-09 02:39:11 -0800

    feat(package): Convert JS to TypeScript
    Refs #4225
    
    Removes deprecated packages `mdc-icon-toggle` and `mdc-toolbar` from the all-in-one JS bundle.

--- packages/material-components-web/index.ts
+++ packages/material-components-web/index.ts
@@ -22,36 +22,34 @@
  */
 
 import autoInit from '@material/auto-init/index';
-import * as base from '@material/base/index.ts';
+import * as base from '@material/base/index';
 import * as checkbox from '@material/checkbox/index';
 import * as chips from '@material/chips/index';
 import * as dialog from '@material/dialog/index';
-import * as dom from '@material/dom/index.ts';
+import * as dom from '@material/dom/index';
 import * as drawer from '@material/drawer/index';
 import * as floatingLabel from '@material/floating-label/index';
-import * as formField from '@material/form-field/index.ts';
+import * as formField from '@material/form-field/index';
 import * as gridList from '@material/grid-list/index';
-import * as iconButton from '@material/icon-button/index.ts';
-import * as iconToggle from '@material/icon-toggle/index';
-import * as linearProgress from '@material/linear-progress/index.ts';
+import * as iconButton from '@material/icon-button/index';
 import * as lineRipple from '@material/line-ripple/index';
+import * as linearProgress from '@material/linear-progress/index';
 import * as list from '@material/list/index';
-import * as menu from '@material/menu/index.ts';
-import * as menuSurface from '@material/menu-surface/index.ts';
+import * as menuSurface from '@material/menu-surface/index';
+import * as menu from '@material/menu/index';
 import * as notchedOutline from '@material/notched-outline/index';
 import * as radio from '@material/radio/index';
-import * as ripple from '@material/ripple/index.ts';
+import * as ripple from '@material/ripple/index';
 import * as select from '@material/select/index';
-import * as selectionControl from '@material/selection-control/index.ts';
+import * as selectionControl from '@material/selection-control/index';
 import * as slider from '@material/slider/index';
 import * as snackbar from '@material/snackbar/index';
-import * as switchControl from '@material/switch/index.ts';
-import * as tab from '@material/tab/index';
+import * as switchControl from '@material/switch/index';
 import * as tabBar from '@material/tab-bar/index';
 import * as tabIndicator from '@material/tab-indicator/index';
 import * as tabScroller from '@material/tab-scroller/index';
+import * as tab from '@material/tab/index';
 import * as textField from '@material/textfield/index';
-import * as toolbar from '@material/toolbar/index';
 import * as topAppBar from '@material/top-app-bar/index';
 
 // Register all components
@@ -62,24 +60,22 @@ autoInit.register('MDCDialog', dialog.MDCDialog);
 autoInit.register('MDCDrawer', drawer.MDCDrawer);
 autoInit.register('MDCFloatingLabel', floatingLabel.MDCFloatingLabel);
 autoInit.register('MDCFormField', formField.MDCFormField);
-autoInit.register('MDCRipple', ripple.MDCRipple);
 autoInit.register('MDCGridList', gridList.MDCGridList);
 autoInit.register('MDCIconButtonToggle', iconButton.MDCIconButtonToggle);
-autoInit.register('MDCIconToggle', iconToggle.MDCIconToggle);
 autoInit.register('MDCLineRipple', lineRipple.MDCLineRipple);
 autoInit.register('MDCLinearProgress', linearProgress.MDCLinearProgress);
 autoInit.register('MDCList', list.MDCList);
-autoInit.register('MDCNotchedOutline', notchedOutline.MDCNotchedOutline);
-autoInit.register('MDCRadio', radio.MDCRadio);
-autoInit.register('MDCSnackbar', snackbar.MDCSnackbar);
-autoInit.register('MDCTabBar', tabBar.MDCTabBar);
-autoInit.register('MDCTextField', textField.MDCTextField);
 autoInit.register('MDCMenu', menu.MDCMenu);
 autoInit.register('MDCMenuSurface', menuSurface.MDCMenuSurface);
+autoInit.register('MDCNotchedOutline', notchedOutline.MDCNotchedOutline);
+autoInit.register('MDCRadio', radio.MDCRadio);
+autoInit.register('MDCRipple', ripple.MDCRipple);
 autoInit.register('MDCSelect', select.MDCSelect);
 autoInit.register('MDCSlider', slider.MDCSlider);
+autoInit.register('MDCSnackbar', snackbar.MDCSnackbar);
 autoInit.register('MDCSwitch', switchControl.MDCSwitch);
-autoInit.register('MDCToolbar', toolbar.MDCToolbar);
+autoInit.register('MDCTabBar', tabBar.MDCTabBar);
+autoInit.register('MDCTextField', textField.MDCTextField);
 autoInit.register('MDCTopAppBar', topAppBar.MDCTopAppBar);
 
 // Export all components.
@@ -95,7 +91,6 @@ export {
   formField,
   gridList,
   iconButton,
-  iconToggle,
   lineRipple,
   linearProgress,
   list,
@@ -107,13 +102,12 @@ export {
   select,
   selectionControl,
   slider,
-  switchControl,
   snackbar,
+  switchControl,
   tab,
   tabBar,
   tabIndicator,
   tabScroller,
   textField,
-  toolbar,
   topAppBar,
 };
