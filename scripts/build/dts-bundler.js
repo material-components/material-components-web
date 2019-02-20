442d1bdd1a59a031ed1165c920c0632c30accdfd
Matt Goo <mattgoo@google.com>
2019-02-20 10:11:05 -0800

    fix: packagejson types, ordering, dtsBundler method

--- scripts/build/dts-bundler.js
+++ scripts/build/dts-bundler.js
@@ -5,13 +5,12 @@ const path = require('path');
 const dTsDirectory = path.resolve(__dirname, '../../build/packages');
 
 function dtsBundler() {
-  fs.readdir(dTsDirectory, (err, packageDirectories) => {
-    packageDirectories.forEach((packageDirectory) => {
-      const main = path.resolve(dTsDirectory, packageDirectory, './index.d.ts');
-      dts.bundle({
-        name: packageDirectory,
-        main,
-      });
+  const packageDirectories = fs.readdirSync(dTsDirectory)
+  packageDirectories.forEach((packageDirectory) => {
+    const main = path.resolve(dTsDirectory, packageDirectory, './index.d.ts');
+    dts.bundle({
+      name: packageDirectory,
+      main,
     });
   });
 }
