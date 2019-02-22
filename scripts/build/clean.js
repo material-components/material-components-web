ebf8fcceaebf26a0047bb07d7623407e787ee516
Kenneth G. Franqueiro <kfranqueiro@google.com>
2019-02-22 16:55:00 -0500

    WIP Clean all the things

--- scripts/build/clean.js
+++ scripts/build/clean.js
@@ -1,3 +1,7 @@
+/**
+ * Cleans top-level build directories (for OSS and internal processes),
+ * dist subfolders under packages, and .js[.map] files in package directories.
+ */
 
 const {exec} = require('child_process');
 const fs = require('fs');
@@ -7,6 +11,7 @@ const {sync: globSync} = require('glob');
 function main() {
   removeDirectory('build');
   removeDirectory('.rewrite-tmp');
+  removeFilesOfType('css');
   removeFilesOfType('js');
   removeFilesOfType('d.ts');
   removeFilesOfType('map');
@@ -23,8 +28,8 @@ function removeDirectory(directory) {
 }
 
 function removeFilesOfType(type) {
-  const fileGlob = `packages/*!(node_modules|dist)/*.${type}`;
-  const nestedFileGlob = `packages/*/!(node_modules|dist)/*.${type}`;
+  const fileGlob = `packages/*/*.${type}`;
+  const nestedFileGlob = `packages/*/!(node_modules)/**/*.${type}`;
   const filePaths = globSync(fileGlob);
   const nestedFilePaths = globSync(nestedFileGlob);
   filePaths.forEach((filePath) => {
