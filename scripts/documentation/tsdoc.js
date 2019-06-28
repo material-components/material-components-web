eb36283d642017dec2c854ae7fdeca2d51da48e0
Matt Goo <mattgoo@google.com>
2019-06-27 17:24:17 -0700

    WIP

--- scripts/documentation/tsdoc.js
+++ scripts/documentation/tsdoc.js
@@ -1,21 +1,50 @@
 "use strict";
 exports.__esModule = true;
-var tsdoc_1 = require("@microsoft/tsdoc");
-var fs_1 = require("fs");
-// import {sync as glob} from 'glob';
-function buildTsDocs() {
-    console.log('Building JS Docs'); //tslint:disable-line
-    // const tsFiles = glob('packages/**/*.ts', {
-    //   ignore: ['**/node_modules/**'],
-    // });
-    var tsFile = 'packages/mdc-textfield/adapter.ts';
-    // tsFiles.forEach((tsFile) => {
-    console.log("\n parsing " + tsFile); //tslint:disable-line
-    var inputBuffer = fs_1.readFileSync(tsFile).toString();
-    var tsdocParser = new tsdoc_1.TSDocParser();
-    var parserContext = tsdocParser.parseString(inputBuffer);
-    var t = parserContext.docComment.params;
-    console.log(t); //tslint:disable-line
-    // });
-}
-buildTsDocs();
+// import {ParserContext, TSDocParser} from '@microsoft/tsdoc';
+var fs = require("fs");
+// // import {sync as glob} from 'glob';
+// function buildTsDocs() {
+//   console.log('Building JS Docs'); //tslint:disable-line
+//   // const tsFiles = glob('packages/**/*.ts', {
+//   //   ignore: ['**/node_modules/**'],
+//   // });
+//   const tsFile = 'packages/mdc-textfield/adapter.ts';
+//   // tsFiles.forEach((tsFile) => {
+//   console.log(`\n parsing ${tsFile}`); //tslint:disable-line
+//   const inputBuffer: string = readFileSync(tsFile).toString();
+//   const tsdocParser: TSDocParser = new TSDocParser();
+//   const parserContext: ParserContext = tsdocParser.parseString(inputBuffer);
+//   const t = parserContext.docComment.params;
+//   console.log(t); //tslint:disable-line
+//   // });
+// }
+// buildTsDocs();
+var jsDocJson = require("../../jsDoc.json");
+jsDocJson.children.forEach(function (file) {
+    var filepath = file.name.replace(/\"/g, '');
+    var componentPath = filepath.split('/')[0];
+    var layers = file.children;
+    var markdownString = '';
+    layers.forEach(function (layer) {
+        // only do foundation, adapter, component methods
+        if (!filepath.endsWith('/adapter')) {
+            return;
+        }
+        // create title
+        var title = "### " + layer.name;
+        markdownString += title;
+        // add table layout header
+        markdownString += 'Method Signature | Description \n --- | --- \n';
+        // create function table
+        var functions = layer.children;
+        functions.forEach(function (func) {
+            markdownString += func.name + " | " + func.signatures[0].comment.shortText + " \n";
+        });
+        console.log(componentPath, '\n'); //tslint:disable-line
+        fs.writeFile("./packages/" + componentPath + "/markdown.md", markdownString, function (err) {
+            console.log('error ', err); //tslint:disable-line
+        });
+    });
+    // console.log(filepath, '\n'); //tslint:disable-line
+    // console.log(file.children); //tslint:disable-line
+});
--- scripts/documentation/tsdoc.ts
+++ scripts/documentation/tsdoc.ts
@@ -1,24 +1,56 @@
-import {ParserContext, TSDocParser} from '@microsoft/tsdoc';
-import {readFileSync} from 'fs';
+// import {ParserContext, TSDocParser} from '@microsoft/tsdoc';
+import * as fs from 'fs';
 
-// import {sync as glob} from 'glob';
+// // import {sync as glob} from 'glob';
 
-function buildTsDocs() {
-  console.log('Building JS Docs'); //tslint:disable-line
-  // const tsFiles = glob('packages/**/*.ts', {
-  //   ignore: ['**/node_modules/**'],
-  // });
+// function buildTsDocs() {
+//   console.log('Building JS Docs'); //tslint:disable-line
+//   // const tsFiles = glob('packages/**/*.ts', {
+//   //   ignore: ['**/node_modules/**'],
+//   // });
 
-  const tsFile = 'packages/mdc-textfield/adapter.ts';
+//   const tsFile = 'packages/mdc-textfield/adapter.ts';
 
-  // tsFiles.forEach((tsFile) => {
-  console.log(`\n parsing ${tsFile}`); //tslint:disable-line
-  const inputBuffer: string = readFileSync(tsFile).toString();
-  const tsdocParser: TSDocParser = new TSDocParser();
-  const parserContext: ParserContext = tsdocParser.parseString(inputBuffer);
-  const t = parserContext.docComment.params;
-  console.log(t); //tslint:disable-line
-  // });
-}
+//   // tsFiles.forEach((tsFile) => {
+//   console.log(`\n parsing ${tsFile}`); //tslint:disable-line
+//   const inputBuffer: string = readFileSync(tsFile).toString();
+//   const tsdocParser: TSDocParser = new TSDocParser();
+//   const parserContext: ParserContext = tsdocParser.parseString(inputBuffer);
+//   const t = parserContext.docComment.params;
+//   console.log(t); //tslint:disable-line
+//   // });
+// }
 
-buildTsDocs();
+// buildTsDocs();
+
+import * as jsDocJson from '../../jsDoc.json';
+
+jsDocJson.children.forEach((file) => {
+  const filepath = file.name.replace(/\"/g, '');
+  const componentPath = filepath.split('/')[0];
+  const layers = file.children;
+  let markdownString = '';
+  layers.forEach((layer) => {
+    // only do foundation, adapter, component methods
+    if (!filepath.endsWith('/adapter')) {
+      return;
+    }
+    // create title
+    const title = `### ${layer.name}`;
+    markdownString += title;
+    // add table layout header
+    markdownString += 'Method Signature | Description \n --- | --- \n';
+    // create function table
+    const functions = layer.children;
+    functions.forEach((func) => {
+      markdownString += `${func.name} | ${func.signatures[0].comment.shortText} \n`;
+    });
+
+    console.log(componentPath, '\n'); //tslint:disable-line
+    fs.writeFile(`./packages/${componentPath}/markdown.md`, markdownString, (err) => {
+      console.log('error ', err); //tslint:disable-line
+    });
+  });
+  // console.log(filepath, '\n'); //tslint:disable-line
+  // console.log(file.children); //tslint:disable-line
+});
