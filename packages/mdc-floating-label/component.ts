301764fec41d010cdc37a63c547b825858e57fc8
Material Web Team <material-web-copybara@google.com>
2020-01-13 13:06:22 -0500

    Project import generated by Copybara.
    PiperOrigin-RevId: 289465443

--- packages/mdc-floating-label/component.ts
+++ packages/mdc-floating-label/component.ts
@@ -22,7 +22,6 @@
  */
 
 import {MDCComponent} from '@material/base/component';
-import {estimateScrollWidth} from '@material/dom/ponyfill';
 import {MDCFloatingLabelAdapter} from './adapter';
 import {MDCFloatingLabelFoundation} from './foundation';
 
@@ -60,7 +59,7 @@ export class MDCFloatingLabel extends MDCComponent<MDCFloatingLabelFoundation> {
     const adapter: MDCFloatingLabelAdapter = {
       addClass: (className) => this.root_.classList.add(className),
       removeClass: (className) => this.root_.classList.remove(className),
-      getWidth: () => estimateScrollWidth(this.root_),
+      getWidth: () => this.root_.scrollWidth,
       registerInteractionHandler: (evtType, handler) => this.listen(evtType, handler),
       deregisterInteractionHandler: (evtType, handler) => this.unlisten(evtType, handler),
     };
