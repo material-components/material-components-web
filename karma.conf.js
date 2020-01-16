071a6ab7009c0a1461b1598f23c3700468663260
Allan Chen <allanchen@google.com>
2020-01-16 10:17:51 -0800

    test(animation): Add Jasmine component unit tests (#5473)

--- karma.conf.js
+++ karma.conf.js
@@ -66,6 +66,7 @@ const istanbulInstrumenterLoader = {
   exclude: [
     /node_modules/,
     /adapter.[jt]s$/,
+    /animation\/.*$/,
     /base\/.*$/,
     /checkbox\/.*$/,
     /chips\/.*$/,
@@ -156,6 +157,7 @@ const jasmineConfig = {
             'adapter.ts',
             'constants.ts',
             'testing/**/*.ts',
+            'packages/!(mdc-animation)/**/*',
             'packages/!(mdc-base)/**/*',
             'packages/!(mdc-checkbox)/**/*',
             'packages/!(mdc-chips)/**/*',
---
+++ packages/mdc-animation/test/component.test.ts
@@ -0,0 +1,110 @@
+/**
+ * @license
+ * Copyright 2020 Google Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+import {getCorrectEventName, getCorrectPropertyName,} from '../../mdc-animation/index';
+
+// Has no properties without a prefix
+const legacyWindowObj = {
+  document: {
+    createElement: () => ({
+      style: {
+        'webkitTransform': 'nah',
+      },
+    })
+  },
+} as any;
+
+describe('MDCAnimation', () => {
+  it('#getCorrectEventName does not prefix events when not necessary', () => {
+    const windowObj = {
+      document: {
+        createElement: () => ({
+          style: {
+            animation: 'none',
+          },
+        })
+      },
+    } as any;
+
+    expect(getCorrectEventName(windowObj, 'animationstart'))
+        .toEqual('animationstart');
+  });
+
+  it('#getCorrectPropertyName does not prefix events when not necessary',
+     () => {
+       const windowObj = {
+         document: {
+           createElement: () => ({
+             style: {
+               animation: 'none',
+             },
+           })
+         },
+       } as any;
+
+       expect(getCorrectPropertyName(windowObj, 'animation'))
+           .toEqual('animation');
+     });
+
+  it('#getCorrectEventName does not prefix events if window does not contain a DOM node',
+     () => {
+       const windowObj = {} as any;
+
+       expect(getCorrectEventName(windowObj, 'animationstart'))
+           .toEqual('animationstart');
+     });
+
+  it('#getCorrectPropertyName does not prefix events if window does not contain a DOM node',
+     () => {
+       const windowObj = {} as any;
+
+       expect(getCorrectPropertyName(windowObj, 'transition'))
+           .toEqual('transition');
+     });
+
+  it('#getCorrectPropertyName prefixes css properties when required', () => {
+    expect(getCorrectPropertyName(legacyWindowObj, 'animation'))
+        .toEqual('-webkit-animation');
+
+    expect(getCorrectPropertyName(legacyWindowObj, 'transform'))
+        .toEqual('-webkit-transform');
+
+    expect(getCorrectPropertyName(legacyWindowObj, 'transition'))
+        .toEqual('-webkit-transition');
+  });
+
+  it('#getCorrectEventName prefixes javascript events when required', () => {
+    expect(getCorrectEventName(legacyWindowObj, 'animationstart'))
+        .toEqual('webkitAnimationStart');
+
+    expect(getCorrectEventName(legacyWindowObj, 'animationend'))
+        .toEqual('webkitAnimationEnd');
+
+    expect(getCorrectEventName(legacyWindowObj, 'animationiteration'))
+        .toEqual('webkitAnimationIteration');
+
+    expect(getCorrectEventName(legacyWindowObj, 'transitionend'))
+        .toEqual('webkitTransitionEnd');
+  });
+});
