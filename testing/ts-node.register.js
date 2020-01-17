dbd38d6e7b17cb8ef34cc5d2f82cbad3a9a20975
Abhinay Omkar <abhiomkar@gmail.com>
2020-01-17 13:14:52 -0500

    test(feature-targeting): Move common feature targeting test cod… (#5485)

--- testing/ts-node.register.js
+++ testing/ts-node.register.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const tsconfigPath = path.join(__dirname, '../../tsconfig-node.json');
+const tsconfigPath = path.join(__dirname, '../tsconfig-node.json');
+
 require('ts-node').register({
   project: tsconfigPath,
 });
