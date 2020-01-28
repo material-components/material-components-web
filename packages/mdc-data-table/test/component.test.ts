d08036658f1b09a8ecf5830e53016dbf21641c67
jony666666 <59974447+jony666666@users.noreply.github.com>
2020-01-28 15:15:55 +0800

    Update component.test.ts

--- packages/mdc-data-table/test/component.test.ts
+++ packages/mdc-data-table/test/component.test.ts
@@ -48,7 +48,7 @@ const mdcCheckboxTemplate = (props: Partial<CheckboxTemplateProps>): string => {
       <input type="checkbox" class="mdc-checkbox__native-control" ${
       props.isChecked ? 'checked' : ''}></input>
       <div class="mdc-checkbox__background">
-        <svg class="mdc-checkbox__checkmark" viewbox="0 0 24 24">
+        <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
           <path class="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59"></path>
         </svg>
         <div class="mdc-checkbox__mixedmark"></div>
