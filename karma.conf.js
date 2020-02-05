30937b0c4d18b2a08ab28a697aa237b823c4dc4e
Joy Zhong <joytzhong@gmail.com>
2020-02-05 11:22:07 -0500

    Remove remaining Mocha test infra.

--- karma.conf.js
+++ karma.conf.js
@@ -21,13 +21,8 @@
  * THE SOFTWARE.
  */
 
-const path = require('path');
-const webpackConfig = require('./webpack.config')[1];
-
 const USE_SAUCE = Boolean(process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY);
-// If true, runs new suite of Jasmine foundation unit tests.
-// Otherwise, runs old Mocha unit tests.
-const USE_JASMINE = Boolean(process.env.USE_JASMINE);
+const PROGRESS = USE_SAUCE ? 'dots' : 'progress';
 
 // Files to include in Jasmine tests.
 const FILES_TO_USE = [
@@ -44,224 +39,76 @@ const EXCLUDE_FILES = [
   'scripts/**/*.ts',
 ];
 
-const HEADLESS_LAUNCHERS = {
-  'ChromeHeadlessNoSandbox': {
-    base: 'ChromeHeadless',
-    flags: ['--no-sandbox'],
-  },
-  'FirefoxHeadless': {
-    base: 'Firefox',
-    flags: ['-headless'],
-  },
-};
-
-const SAUCE_LAUNCHERS = {
-  'sl-ie': {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    version: '11',
-    platform: 'Windows 10',
-  },
-};
-
-const PROGRESS = USE_SAUCE ? 'dots' : 'progress';
-
-const customLaunchers = Object.assign({}, USE_SAUCE ? SAUCE_LAUNCHERS : {}, HEADLESS_LAUNCHERS);
-const browsers = USE_SAUCE ? Object.keys(customLaunchers) : ['Chrome'];
-const istanbulInstrumenterLoader = {
-  use: [{
-    loader: 'istanbul-instrumenter-loader',
-    options: {esModules: true},
-  }],
-  exclude: [
-    /node_modules/,
-    /adapter.[jt]s$/,
-    /animation\/.*$/,
-    /auto-init\/.*$/,
-    /base\/.*$/,
-    /checkbox\/.*$/,
-    /chips\/.*$/,
-    /constants.[jt]s$/,
-    /data-table\/.*$/,
-    /dialog\/.*$/,
-    /dom\/.*$/,
-    /drawer\/.*$/,
-    /floating-label\/.*$/,
-    /form-field\/.*$/,
-    /icon-button\/.*$/,
-    /line-ripple\/.*$/,
-    /linear-progress\/.*$/,
-    /list\/.*$/,
-    /menu\/.*$/,
-    /menu-surface\/.*$/,
-    /notched-outline\/.*$/,
-    /radio\/.*$/,
-    /ripple\/.*$/,
-    /select\/.*$/,
-    /slider\/.*$/,
-    /snackbar\/.*$/,
-    /switch\/.*$/,
-    /tab-bar\/.*$/,
-    /tab-indicator\/.*$/,
-    /tab-scroller\/.*$/,
-    /tab\/.*$/,
-    /textfield\/.*$/,
-    /top-app-bar\/.*$/,
-  ],
-  include: path.resolve('./packages'),
-};
-
-const mochaConfig = {
-  basePath: '',
-  // Refer https://github.com/karma-runner/karma-mocha
-  frameworks: ['mocha'],
-  files: [
-    // Refer https://github.com/babel/karma-babel-preprocessor
-    'node_modules/@babel/polyfill/dist/polyfill.js',
-    'test/unit/index.js',
-  ],
-  preprocessors: {
-    'test/unit/index.js': ['webpack', 'sourcemap'],
-  },
-  reporters: [PROGRESS, 'coverage-istanbul'],
-
-  coverageIstanbulReporter: {
-    'dir': 'coverage',
-    'reports': ['html', 'lcovonly', 'json'],
-    'report-config': {
-      lcovonly: {subdir: '.'},
-      json: {subdir: '.', file: 'coverage.json'},
-    },
-    // Set 'emitWarning = true' to NOT fail tests if the thresholds are not met
-    'emitWarning': false,
-    'thresholds': {
-      statements: 95,
-      branches: 92,
-      lines: 95,
-      functions: 95,
-    },
-  },
-
-  client: {
-    mocha: {
-      reporter: 'html',
-      ui: 'qunit',
-
-      // Number of milliseconds to wait for an individual `test(...)` function to complete.
-      // The default is 2000.
-      timeout: 10000,
-    },
-  },
-
-  webpackMiddleware: {
-    noInfo: true,
-    stats: 'minimal',
-  },
-};
-
-const jasmineConfig = {
-  basePath: '',
-  files: FILES_TO_USE,
-  exclude: EXCLUDE_FILES,
-  frameworks: ['jasmine', 'karma-typescript'],
-  karmaTypescriptConfig: {
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    files: FILES_TO_USE,
     exclude: EXCLUDE_FILES,
-    coverageOptions: {
-      threshold: {
-        global: {
-          // TODO: Raise threshold to at least 90% after more tests have been migrated.
-          statements: 80,
-          branches: 70,
-          functions: 50,
-          lines: 80,
-          excludes: [
-            'adapter.ts',
-            'constants.ts',
-            'testing/**/*.ts',
-            'packages/!(mdc-animation)/**/*',
-            'packages/!(mdc-auto-init)/**/*',
-            'packages/!(mdc-base)/**/*',
-            'packages/!(mdc-checkbox)/**/*',
-            'packages/!(mdc-chips)/**/*',
-            'packages/!(mdc-data-table)/**/*',
-            'packages/!(mdc-dialog)/**/*',
-            'packages/!(mdc-dom)/**/*',
-            'packages/!(mdc-drawer)/**/*',
-            'packages/!(mdc-floating-label)/**/*',
-            'packages/!(mdc-form-field)/**/*',
-            'packages/!(mdc-icon-button)/**/*',
-            'packages/!(mdc-line-ripple)/**/*',
-            'packages/!(mdc-linear-progress)/**/*',
-            'packages/!(mdc-list)/**/*',
-            'packages/!(mdc-menu)/**/*',
-            'packages/!(mdc-menu-surface)/**/*',
-            'packages/!(mdc-notched-outline)/**/*',
-            'packages/!(mdc-radio)/**/*',
-            'packages/!(mdc-ripple)/**/*',
-            'packages/!(mdc-select)/**/*',
-            'packages/!(mdc-slider)/**/*',
-            'packages/!(mdc-snackbar)/**/*',
-            'packages/!(mdc-switch)/**/*',
-            'packages/!(mdc-tab-bar)/**/*',
-            'packages/!(mdc-tab-indicator)/**/*',
-            'packages/!(mdc-tab-scroller)/**/*',
-            'packages/!(mdc-tab)/**/*',
-            'packages/!(mdc-textfield)/**/*',
-            'packages/!(mdc-top-app-bar)/**/*',
-          ],
+    frameworks: ['jasmine', 'karma-typescript'],
+    karmaTypescriptConfig: {
+      exclude: EXCLUDE_FILES,
+      coverageOptions: {
+        threshold: {
+          global: {
+            // TODO: Raise threshold to at least 90% after more tests have been migrated.
+            statements: 80,
+            branches: 70,
+            functions: 50,
+            lines: 80,
+            excludes: [
+              'adapter.ts',
+              'constants.ts',
+              'testing/**/*.ts',
+              'packages/!(mdc-animation)/**/*',
+              'packages/!(mdc-auto-init)/**/*',
+              'packages/!(mdc-base)/**/*',
+              'packages/!(mdc-checkbox)/**/*',
+              'packages/!(mdc-chips)/**/*',
+              'packages/!(mdc-data-table)/**/*',
+              'packages/!(mdc-dialog)/**/*',
+              'packages/!(mdc-dom)/**/*',
+              'packages/!(mdc-drawer)/**/*',
+              'packages/!(mdc-floating-label)/**/*',
+              'packages/!(mdc-form-field)/**/*',
+              'packages/!(mdc-icon-button)/**/*',
+              'packages/!(mdc-line-ripple)/**/*',
+              'packages/!(mdc-linear-progress)/**/*',
+              'packages/!(mdc-list)/**/*',
+              'packages/!(mdc-menu)/**/*',
+              'packages/!(mdc-menu-surface)/**/*',
+              'packages/!(mdc-notched-outline)/**/*',
+              'packages/!(mdc-radio)/**/*',
+              'packages/!(mdc-ripple)/**/*',
+              'packages/!(mdc-select)/**/*',
+              'packages/!(mdc-slider)/**/*',
+              'packages/!(mdc-snackbar)/**/*',
+              'packages/!(mdc-switch)/**/*',
+              'packages/!(mdc-tab-bar)/**/*',
+              'packages/!(mdc-tab-indicator)/**/*',
+              'packages/!(mdc-tab-scroller)/**/*',
+              'packages/!(mdc-tab)/**/*',
+              'packages/!(mdc-textfield)/**/*',
+              'packages/!(mdc-top-app-bar)/**/*',
+            ],
+          },
         },
       },
-    },
-    reports: {
-      html: 'coverage',
-      lcovonly: 'coverage',
-      json: {
-        directory: 'coverage',
-        filename: 'coverage.json',
+      reports: {
+        html: 'coverage',
+        lcovonly: 'coverage',
+        json: {
+          directory: 'coverage',
+          filename: 'coverage.json',
+        },
       },
+      tsconfig: './tsconfig-base.json',
     },
-    tsconfig: './tsconfig-base.json',
-  },
-  preprocessors: FILES_TO_USE.reduce((obj, file) => {
-    obj[file] = 'karma-typescript';
-    return obj;
-  }, {}),
-  reporters: [PROGRESS, 'karma-typescript'],
-};
-
-module.exports = function(config) {
-  config.set(Object.assign(USE_JASMINE ? jasmineConfig : mochaConfig, {
-    logLevel: config.LOG_INFO,
-    port: 9876,
-    colors: true,
-    browsers: browsers,
-    browserDisconnectTimeout: 40000,
-    browserNoActivityTimeout: 120000,
-    captureTimeout: 240000,
-    concurrency: USE_SAUCE ? 10 : Infinity,
-    customLaunchers: customLaunchers,
-  }));
-
-  if (!USE_JASMINE) {
-    // Need to set webpack here rather than in `mochaConfig` to read `config.singleRun`.
-    config.set({
-      // Refer https://github.com/webpack-contrib/karma-webpack
-      webpack: Object.assign({}, webpackConfig, {
-        plugins: [], // Exclude UglifyJs plugin from test build.
-        mode: 'development',
-        module: Object.assign({}, webpackConfig.module, {
-          // Cover source files when not debugging tests. Otherwise, omit coverage instrumenting to get
-          // uncluttered source maps.
-          rules: webpackConfig.module.rules.concat(config.singleRun ? [Object.assign({
-            enforce: 'post',
-            test: /\.ts$/,
-          }, istanbulInstrumenterLoader), Object.assign({
-            test: /\.js$/,
-          }, istanbulInstrumenterLoader)] : []),
-        }),
-      }),
-    });
-  }
+    preprocessors: FILES_TO_USE.reduce((obj, file) => {
+      obj[file] = 'karma-typescript';
+      return obj;
+    }, {}),
+    reporters: [PROGRESS, 'karma-typescript'],
+  });
 
   if (USE_SAUCE) {
     const sauceLabsConfig = {
