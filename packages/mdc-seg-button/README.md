5fad0d856670723fe7162a895078132b8cc1ac7d
gschrag <gschrag@google.com>
2020-06-12 12:42:15 -0600

    refactor: create new component repository for mdc-seg-button
    move current code to component repository and begin to structure as per authoring-components.md

---
+++ packages/mdc-seg-button/README.md
@@ -0,0 +1,2 @@
+# Instructions on running demo
+1. From material-components-web/ run "npm i & npx parcel packages/mdc-seg-button/demo/index.html"
--- packages/mdc-seg-button/index.ts
+++ packages/mdc-seg-button/index.ts
@@ -1,8 +1,31 @@
+/**
+ * @license
+ * Copyright 2020 Google Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 import './index.scss';
 
 
 //TODO: have list of currently active buttons
-class MDCSegButton {
+export class MDCSegButton {
     private root: HTMLElement;
     private mdcSegButtonElements: MDCSegButtonElement[] = [];
 
@@ -24,7 +47,7 @@ class MDCSegButton {
 
 }
 
-class MDCSegButtonSingle {
+export class MDCSegButtonSingle {
     private root: HTMLElement;
     private mdcSegButtonElements: MDCSegButtonElement[] = [];
 
@@ -64,7 +87,7 @@ class MDCSegButtonSingle {
 }
 
 
-class MDCSegButtonElement {
+export class MDCSegButtonElement {
     private root: HTMLElement;
     private id: number;
     private parentElement: HTMLElement;
@@ -118,22 +141,4 @@ class MDCSegButtonElement {
         return this.root;
     }
 
-}
-
-
-/*
-    The below section is what instantiates the buttons groups for demo
-*/
-
-
-const mdcSegButtonVanillaEls = document.querySelectorAll<HTMLElement>('.mdc-seg-button-vanilla');
-
-if (mdcSegButtonVanillaEls) {
-    mdcSegButtonVanillaEls.forEach((mdcSegButtonEl) => {
-        if (mdcSegButtonEl.classList.contains('mdc-seg-button--single')) {
-            new MDCSegButtonSingle(mdcSegButtonEl);
-        } else {
-            new MDCSegButton(mdcSegButtonEl);
-        }
-    });
 }
