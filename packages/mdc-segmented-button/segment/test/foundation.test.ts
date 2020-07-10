ae01397dfeb56f482c6ec5baff87a5cdfc90fb96
gschrag <gschrag@google.com>
2020-07-10 17:41:36 -0600

    test: add segment foundation unit tests
    test new segment foundation business logic

---
+++ packages/mdc-segmented-button/segment/test/foundation.test.ts
@@ -0,0 +1,156 @@
+/**
+ * @license
+ * Copyright 2020 Google Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import {MDCSegmentedButtonSegmentFoundation} from '../foundation';
+import {verifyDefaultAdapter} from '../../../../testing/helpers/foundation';
+import {setUpFoundationTest} from '../../../../testing/helpers/setup';
+
+describe('MDCSegmentedButtonSegmentFoundation', () => {
+  it('defaultAdapter returns a complete adapter implementation', () => {
+    verifyDefaultAdapter(MDCSegmentedButtonSegmentFoundation, [
+      'isSingleSelect',
+      'getAttr',
+      'setAttr',
+      'addClass',
+      'removeClass',
+      'hasClass',
+      'notifySelectedChange'
+    ]);
+  });
+
+  // This value is arbitrary
+  const SEGMENT_ID = 'segment0';
+  const SELECTED_CLASS = 'mdc-segmented-button__segment--selected';
+
+  const setupTest = (selected: boolean, singleSelect: boolean) => {
+    const {foundation, mockAdapter} = setUpFoundationTest(MDCSegmentedButtonSegmentFoundation);
+    mockAdapter.hasClass.withArgs(SELECTED_CLASS).and.returnValue(selected);
+    mockAdapter.isSingleSelect.and.returnValue(singleSelect);
+
+    // Need calls to mocked methods to change mocked state
+    mockAdapter.addClass.and.callFake((cssClass: string) => {
+      mockAdapter.hasClass.withArgs(cssClass).and.returnValue(true);
+    });
+    mockAdapter.removeClass.and.callFake((cssClass: string) => {
+      mockAdapter.hasClass.withArgs(cssClass).and.returnValue(false);
+    });
+
+    return {foundation, mockAdapter};
+  };
+
+  const setupSelectedTest = (singleSelect: boolean = false) => {
+    return setupTest(true, singleSelect);
+  };
+
+  const setupUnselectedTest = (singleSelect: boolean = false) => {
+    return setupTest(false, singleSelect);
+  };
+
+  it('#isSelected returns true if segment is selected', () => {
+    const {foundation} = setupSelectedTest();
+    expect(foundation.isSelected()).toBeTruthy();
+  });
+
+  it('#isSelected returns false if segment is unselected', () => {
+    const {foundation} = setupUnselectedTest();
+    expect(foundation.isSelected()).toBeFalsy();
+  });
+
+  it('#getSegmentId returns segment id', () => {
+    const {foundation, mockAdapter} = setupSelectedTest();
+    mockAdapter.getAttr.withArgs('data-segment-id').and.returnValue(SEGMENT_ID);
+    expect(foundation.getSegmentId()).toEqual(SEGMENT_ID);
+  });
+
+  describe('Selection toggling', () => {
+    it('#setSelected adds the `selected` css class and if singleSelect then sets aria-checked to true', () => {
+      const {foundation, mockAdapter} = setupUnselectedTest(true);
+      foundation.setSelected();
+
+      expect(mockAdapter.addClass).toHaveBeenCalledWith(SELECTED_CLASS);
+      expect(mockAdapter.setAttr).toHaveBeenCalledWith('aria-checked', 'true');
+    });
+
+    it('#setSelected adds the `selected` css class and if not singleSelect then sets aria-pressed to true', () => {
+      const {foundation, mockAdapter} = setupUnselectedTest();
+      foundation.setSelected();
+
+      expect(mockAdapter.addClass).toHaveBeenCalledWith(SELECTED_CLASS);
+      expect(mockAdapter.setAttr).toHaveBeenCalledWith('aria-pressed', 'true');
+    });
+
+    it('#setUnselected removes the `selected` css class and if singleSelect then sets aria-checked to false', () => {
+      const {foundation, mockAdapter} = setupSelectedTest(true);
+      foundation.setUnselected();
+
+      expect(mockAdapter.removeClass).toHaveBeenCalledWith(SELECTED_CLASS);
+      expect(mockAdapter.setAttr).toHaveBeenCalledWith('aria-checked', 'false');
+    });
+
+    it('#setUnselected removes the `selected` css class and if not singleSelect then sets aria-pressed to false', () => {
+      const {foundation, mockAdapter} = setupSelectedTest();
+      foundation.setUnselected();
+
+      expect(mockAdapter.removeClass).toHaveBeenCalledWith(SELECTED_CLASS);
+      expect(mockAdapter.setAttr).toHaveBeenCalledWith('aria-pressed', 'false');
+    });
+  });
+
+  describe('Click handling', () => {
+    it('#handleClick if singleSelect and previously unselected: selects segment, sets aria-checked to true, and notifies parent', () => {
+      const {foundation, mockAdapter} = setupUnselectedTest(true);
+      foundation.handleClick();
+
+      expect(mockAdapter.addClass).toHaveBeenCalledWith(SELECTED_CLASS);
+      expect(mockAdapter.setAttr).toHaveBeenCalledWith('aria-checked', 'true');
+      expect(mockAdapter.notifySelectedChange).toHaveBeenCalledWith(true);
+    });
+
+    it('#handleClick if singleSelect and previously selected: changes nothing and notifies parent', () => {
+      const {foundation, mockAdapter} = setupSelectedTest(true);
+      foundation.handleClick();
+
+      expect(mockAdapter.removeClass).toHaveBeenCalledTimes(0);
+      expect(mockAdapter.setAttr).toHaveBeenCalledTimes(0);
+      expect(mockAdapter.notifySelectedChange).toHaveBeenCalledWith(true);
+    });
+
+    it('#handleClick if not singleSelect and previously unselected: selects segment, sets aria-pressed to true, and notifies parent', () => {
+      const {foundation, mockAdapter} = setupUnselectedTest();
+      foundation.handleClick();
+
+      expect(mockAdapter.addClass).toHaveBeenCalledWith(SELECTED_CLASS);
+      expect(mockAdapter.setAttr).toHaveBeenCalledWith('aria-pressed', 'true');
+      expect(mockAdapter.notifySelectedChange).toHaveBeenCalledWith(true);
+    });
+
+    it('#handleClick if not singleSelect and previously selected: unselects segment, sets aria-pressed to false, and notifies parent', () => {
+      const {foundation, mockAdapter} = setupSelectedTest();
+      foundation.handleClick();
+
+      expect(mockAdapter.removeClass).toHaveBeenCalledWith(SELECTED_CLASS);
+      expect(mockAdapter.setAttr).toHaveBeenCalledWith('aria-pressed', 'false');
+      expect(mockAdapter.notifySelectedChange).toHaveBeenCalledWith(false);
+    });
+  });
+});
