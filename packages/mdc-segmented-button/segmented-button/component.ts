0119e6143efb5b2d35522b65b2de52d482309619
gschrag <gschrag@google.com>
2020-07-14 15:10:00 -0600

    feat: add component outlines for segment and segmented-button
    add method outlines for segment and segmented-button components
    
    related to issue#6069

---
+++ packages/mdc-segmented-button/segmented-button/component.ts
@@ -0,0 +1,65 @@
+/**
+ * @license
+ * Copyright 2020 Google Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import {MDCComponent} from '@material/base/component';
+import {MDCSegmentedButtonSegmentFactory} from '../segment/component';
+import {SegmentDetail} from '../types';
+import {MDCSegmentedButtonFoundation} from './foundation';
+
+export class MDCSegmentedButton extends MDCComponent<MDCSegmentedButtonFoundation> {
+  static attachTo(root: Element): MDCSegmentedButton {
+    return new MDCSegmentedButton(root);
+  }
+
+  initialize(_segmentFactory: MDCSegmentedButtonSegmentFactory) {
+    return;
+  }
+
+  initialSyncWithDOM() {
+    return;
+  }
+
+  destroy() {
+    super.destroy();
+  }
+
+  getDefaultFoundation(): MDCSegmentedButtonFoundation {
+    return new MDCSegmentedButtonFoundation();
+  }
+
+  getSelectedSegments(): readonly SegmentDetail[] {
+    return [];
+  }
+
+  selectSegment(_indexOrSegmentId: number | string) {
+    return;
+  }
+
+  unselectSegment(_indexOrSegmentId: number | string) {
+    return;
+  }
+
+  isSegmentSelected(_indexOrSegmentId: number | string): boolean {
+    return false;
+  }
+}
