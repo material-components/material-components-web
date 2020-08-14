97a0bf20a20e869cfdcb9fdd6f31f1c28a16e060
gschrag <gschrag@google.com>
2020-08-14 17:48:46 -0600

    feat: change default behavior of singleSelect to throw error
    throw error rather than modify the DOM if singleSelect

--- packages/mdc-segmented-button/segmented-button/component.ts
+++ packages/mdc-segmented-button/segmented-button/component.ts
@@ -71,11 +71,10 @@ export class MDCSegmentedButton extends MDCComponent<MDCSegmentedButtonFoundatio
     });
 
     const selectedSegments = this.segments_.filter((segment) => segment.isSelected());
-    for (let i = 1; i < selectedSegments.length; i++) {
-      selectedSegments[i].setUnselected();
-    }
     if (isSingleSelect && selectedSegments.length == 0 && this.segments_.length > 0) {
-      this.segments_[0].setSelected();
+      throw Error('No segment selected in singleSelect mdc-segmented-button');
+    } else if (isSingleSelect && selectedSegments.length > 1) {
+      throw Error('Multiple segments selected in singleSelect mdc-segmented-button');
     }
   }
 
