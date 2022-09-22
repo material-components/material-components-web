42510b3778b17b69b9b72cd6a33f39e3d7debaea
patrickrodee <prodee@google.com>
2022-09-22 22:29:42 +0000

    Attempting to unblock #7719

--- karma.conf.js
+++ karma.conf.js
@@ -70,6 +70,11 @@ module.exports = function(config) {
     karmaTypescriptConfig: {
       exclude: EXCLUDE_FILES,
       tsconfig: './tsconfig-testing.json',
+      bundlerOptions: {
+        transforms: [
+          require("karma-typescript-es6-transform")()
+        ]
+      }
     },
     preprocessors: FILES_TO_USE.reduce((obj, file) => {
       obj[file] = 'karma-typescript';
