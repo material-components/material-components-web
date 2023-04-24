ef754381c4f79b3ae2f0a88a13abf8687e1fc610
Cody Coljee-Gray <codr@google.com>
2023-04-24 13:44:03 -0700

    refactor: migration some component tests to Sass modules
    PiperOrigin-RevId: 526743123

--- packages/mdc-typography/test/mdc-typography.scss.test.ts
+++ packages/mdc-typography/test/mdc-typography.scss.test.ts
@@ -23,7 +23,6 @@
 
 import 'jasmine';
 
-import * as fs from 'fs';
 import * as path from 'path';
 
 import {expectStylesWithNoFeaturesToBeEmpty} from '../../../testing/featuretargeting';
@@ -31,18 +30,4 @@ import {expectStylesWithNoFeaturesToBeEmpty} from '../../../testing/featuretarge
 describe('mdc-typography.scss', () => {
   expectStylesWithNoFeaturesToBeEmpty(
       path.join(__dirname, 'feature-targeting-any.test.css'));
-
-  it('should allow global variable overrides with @import', () => {
-    const css = fs.readFileSync(
-                      path.join(__dirname, 'global-variables.test.css'), 'utf8')
-                    .trim();
-    const headline1Start = css.indexOf('.mdc-typography--headline1 {');
-    const headline1End = css.indexOf('}', headline1Start);
-    const headline1Css = css.substring(headline1Start, headline1End);
-    expect(headline1Css.includes('font-family: Arial'))
-        .toBe(true, '$mdc-typography-font-family should override');
-    expect(headline1Css.includes('font-size: 1rem'))
-        .toBe(
-            true, '$mdc-typography-styles-headline1-font-size should override');
-  });
 });
